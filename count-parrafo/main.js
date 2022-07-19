d854415c3fa531a7304d730bb6bd7c700cd009d8
Eri Vera <eriveraec@gmail.com>
2022-07-19 12:05:40 -0500

    pub

---
+++ count-parrafo/main.js
@@ -0,0 +1,13 @@
+function pubCenter1() {
+  var rel_center1 = jQuery(".adcontainer-portlet.publicidad-center1").html();
+  rel_center1 =
+    "<div class='pub-center1-js c_002_001'>" + rel_center1 + "</div>";
+  jQuery(".ly-detail .adcontainer-portlet.publicidad-center1").empty();
+
+  var content = $(".DETAIL_TXT .text").find("p");
+  var midLength = parseInt(content.length / 2);
+  content.eq(midLength).after(rel_center1);
+  console.log("pubCenter1-activo-after");
+}
+
+pubCenter1();
