686ce081c9c56c9b9cb6ef59175b10eff6d5f56f
Eri Vera <eriveraec@gmail.com>
2022-07-19 12:45:07 -0500

    new code pud

--- count-parrafo/main.js
+++ count-parrafo/main.js
@@ -1,8 +1,9 @@
 function pubCenter1() {
-  var rel_center1 = jQuery(".adcontainer-portlet.publicidad-center1").html();
+  // var rel_center1 = jQuery(".adcontainer-portlet.publicidad-center1").html();
   rel_center1 =
-    "<div id='' class='portlet-boundary portlet-static-end adcontainer-portlet pub-active'>" + rel_center1 + "</div>";
-  jQuery(".ly-detail .adcontainer-portlet.publicidad-center1").empty();
+    "<div align='center' data-ad='top-generico' id='center1' ><script type='text/javascript'>googletag.cmd.push(function() { googletag.display('center1'); });console.log('center1');</script></div>";
+
+  // jQuery(".ly-detail .adcontainer-portlet.publicidad-center1").empty();
 
   var content = $(".DETAIL_TXT .text").find("p");
   var midLength = parseInt(content.length / 2);
