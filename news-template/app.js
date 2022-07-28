e1e37f96dfa1e37a591816e7ea653165fbce3b92
Eri Vera <eriveraec@gmail.com>
2022-07-28 13:06:08 -0500

    new class general js

--- news-template/app.js
+++ news-template/app.js
@@ -1,7 +1,7 @@
-$(".newsletters_options .newsletters_check input").addClass("btn-check");
-$(".newsletter_email .newsletter_email_div input").addClass("form-control");
-$(".newsletter_email_div #newsletter_email_ctrl").attr("placeholder", "Email");
-$(".boton-suscrib .btn-trans").after(`<img src="https://ecuavisadev.netlify.app/news-template/src-img-btn.png" alt="recurso">`);
+$(".newsletter-styles-form .newsletters_options .newsletters_check input").addClass("btn-check");
+$(".newsletter-styles-form .newsletter_email .newsletter_email_div input").addClass("form-control");
+$(".newsletter-styles-form .newsletter_email_div #newsletter_email_ctrl").attr("placeholder", "Email");
+$(".newsletter-styles-form .boton-suscrib .btn-trans").after(`<img src="https://ecuavisadev.netlify.app/news-template/src-img-btn.png" alt="recurso">`);
 
 function ClearFields() {
   document.getElementById("newsletter_email_ctrl").value = "";
