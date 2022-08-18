fceaa7adef5f8ecdfb744646756d7548e7ee2c25
Eri Vera <eriveraec@gmail.com>
2022-08-18 12:11:14 -0500

    Update swiper-estilo.js

--- swiper-dev/swiper-estilo.js
+++ swiper-dev/swiper-estilo.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
   var dpagination = document.createElement("div");
   dpagination.classList = "swiper-pagination";
-  document.querySelector(`.lista-bloque-estilo`).insertBefore(dpagination);
+  document.querySelector(`.lista-bloque-estilo`).appendChild(dpagination);
 
   var swiperEstilo = new Swiper(".lista-bloque-estilo", {
     slidesPerView: 2,
