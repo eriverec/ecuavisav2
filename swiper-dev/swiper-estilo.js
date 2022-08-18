32d869ec3373f11a8d0aff1c1c7bc2f38f169df9
Eri Vera <eriveraec@gmail.com>
2022-08-18 09:55:22 -0500

    Update swiper-estilo.js

---
+++ swiper-dev/swiper-estilo.js
@@ -0,0 +1,52 @@
+/*
+1.- swipper mySwiper
+2.- swiper-wrapper
+3.- swiper-slide
+
+swiper-pagination
+swiper-button-prev
+swiper-button-next
+swiper-scrollbar
+*/
+
+
+$(`.lista-bloque-estilo`).addClass('swiper');
+$('.lista-bloque-estilo .noticias').addClass('swiper-wrapper');
+$('.lista-bloque-estilo .noticias .article').addClass('swiper-slide');
+
+var dpagination = document.createElement("div");
+dpagination.classList = "swiper-pagination";
+document.querySelector(`.lista-bloque-estilo`).appendChild(dpagination);
+
+
+var swiperEstilo = new Swiper(".lista-bloque-estilo", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    360: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    390: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    600: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
