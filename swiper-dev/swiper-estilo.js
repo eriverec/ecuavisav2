c496de43de588087985149824a6c0ae8b0b501ac
Eri Vera <eriveraec@gmail.com>
2022-08-18 09:57:11 -0500

    Update swiper-estilo.js

--- swiper-dev/swiper-estilo.js
+++ swiper-dev/swiper-estilo.js
@@ -8,45 +8,44 @@ swiper-button-prev
 swiper-button-next
 swiper-scrollbar
 */
+$(document).ready(function () {
+  $(`.lista-bloque-estilo`).addClass("swiper");
+  $(".lista-bloque-estilo .noticias").addClass("swiper-wrapper");
+  $(".lista-bloque-estilo .noticias .article").addClass("swiper-slide");
 
+  var dpagination = document.createElement("div");
+  dpagination.classList = "swiper-pagination";
+  document.querySelector(`.lista-bloque-estilo`).appendChild(dpagination);
 
-$(`.lista-bloque-estilo`).addClass('swiper');
-$('.lista-bloque-estilo .noticias').addClass('swiper-wrapper');
-$('.lista-bloque-estilo .noticias .article').addClass('swiper-slide');
-
-var dpagination = document.createElement("div");
-dpagination.classList = "swiper-pagination";
-document.querySelector(`.lista-bloque-estilo`).appendChild(dpagination);
-
-
-var swiperEstilo = new Swiper(".lista-bloque-estilo", {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-
-  breakpoints: {
-    360: {
-      slidesPerView: 2.5,
-      spaceBetween: 20,
+  var swiperEstilo = new Swiper(".lista-bloque-estilo", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    390: {
-      slidesPerView: 1.5,
-      spaceBetween: 20,
+
+    breakpoints: {
+      360: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      390: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+      600: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
     },
-    600: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 2,
-      spaceBetween: 30,
-    },
-  },
+  });
 });
