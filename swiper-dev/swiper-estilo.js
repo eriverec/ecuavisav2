e3fdc461f30b9e225abf55cbb1fa23c8c8f892c3
Eri Vera <eriveraec@gmail.com>
2022-08-18 12:30:51 -0500

    swiper y css

--- swiper-dev/swiper-estilo.js
+++ swiper-dev/swiper-estilo.js
@@ -15,9 +15,8 @@ $(document).ready(function () {
 
   var dpagination = document.createElement("div");
   dpagination.classList = "swiper-pagination";
-  // document.querySelector(`.lista-bloque-estilo`).insertBefore(dpagination);
-
   $(".lista-bloque-estilo").prepend(dpagination);
+  // document.querySelector(`.lista-bloque-estilo`).insertBefore(dpagination);
 
   var swiperEstilo = new Swiper(".lista-bloque-estilo", {
     slidesPerView: 2,
