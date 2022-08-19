18561ba6b4bce8300761b1fe0776e3b66884cfc1
Eri Vera <eriveraec@gmail.com>
2022-08-19 12:36:55 -0500

    remove jqueryiter

--- swiper-dev/swiper-futuro.js
+++ swiper-dev/swiper-futuro.js
@@ -9,37 +9,35 @@ swiper-button-next
 swiper-scrollbar
 */
 $(document).ready(function () {
-  jQryIter(document).bind("widgetCompleteLoad", function (e, widgetElement) {
-    $(`.lista-bloque-futuro`).addClass("swiper");
-    $(".lista-bloque-futuro .noticias").addClass("swiper-wrapper");
-    $(".lista-bloque-futuro .noticias .article").addClass("swiper-slide");
+  $(`.lista-bloque-futuro`).addClass("swiper");
+  $(".lista-bloque-futuro .noticias").addClass("swiper-wrapper");
+  $(".lista-bloque-futuro .noticias .article").addClass("swiper-slide");
 
-    // var dpagination = document.createElement("div");
-    // dpagination.classList = "swiper-pagination";
-    // $(".lista-bloque-futuro").prepend(dpagination);
+  // var dpagination = document.createElement("div");
+  // dpagination.classList = "swiper-pagination";
+  // $(".lista-bloque-futuro").prepend(dpagination);
 
-    var swiperfuturo = new Swiper(".lista-bloque-futuro", {
-      slidesPerView: 2.5,
-      spaceBetween: 30,
-      // pagination: {
-      //   el: ".swiper-pagination",
-      //   dynamicBullets: true,
-      //   clickable: true,
-      // },
-      breakpoints: {
-        600: {
-          slidesPerView: 2.5,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 30,
-        },
+  var swiperfuturo = new Swiper(".lista-bloque-futuro", {
+    slidesPerView: 2.5,
+    spaceBetween: 30,
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   dynamicBullets: true,
+    //   clickable: true,
+    // },
+    breakpoints: {
+      600: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
       },
-    });
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
   });
 });
