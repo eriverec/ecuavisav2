914b4c8c8ddb5853482fcf6818b94d54f18154ec
Eri Vera <eriveraec@gmail.com>
2022-08-23 13:43:31 -0500

    Update swiper-prog-web.js

--- swiper-dev/swiper-prog-web.js
+++ swiper-dev/swiper-prog-web.js
@@ -8,42 +8,8 @@ swiper-button-prev
 swiper-button-next
 swiper-scrollbar
 */
-$(document).ready(function () {
-  $(".lista-programacion-bloque").addClass("swiper");
-  $(".lista-programacion-bloque .noticias").addClass("swiper-wrapper");
-  $(".lista-programacion-bloque .noticias .article").addClass("swiper-slide");
+// $(document).ready(function () {
 
-  var dpagination = document.createElement("div");
-  dpagination.classList = "swiper-scrollbar";
-  $(".lista-programacion-bloque").append(dpagination);
 
-  var getNItem = localStorage.getItem('nItem');
-  // console.log("numero desde swiper prog: ",getNItem);
-  var numSlider = getNItem - 1;
-  // console.log(numSlider);
-    
-  var swiperProgramacionWeb = new Swiper(".lista-programacion-bloque", {
-    slidesPerView: 1.5,
-    spaceBetween: 10,
-    initialSlide: numSlider,
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
-    },
-    breakpoints: {
-      600: {
-        slidesPerView: 1.5,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 2.5,
-        spaceBetween: 10,
-      },
-      1024: {
-        slidesPerView: 3.5,
-        spaceBetween: 10,
-      },
-    },
-  });
 
-});
+// });
