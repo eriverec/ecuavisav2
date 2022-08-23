4f6ccbdd45b4d9930cff62f93796f094ac00d4c2
Eri Vera <eriveraec@gmail.com>
2022-08-23 14:43:55 -0500

    swiper prog

--- swiper-dev/swiper-prog-web.js
+++ swiper-dev/swiper-prog-web.js
@@ -8,8 +8,185 @@ swiper-button-prev
 swiper-button-next
 swiper-scrollbar
 */
-// $(document).ready(function () {
+
+$(document).ready(function () {
+  var d = new Date();
+  var day = d.getDay();
+  var hour = d.getHours();
+  var min = d.getMinutes();
+  var t = d.getTime();
+  var name = "";
+
+  // console.log(playerOptions.id);
+  $(".sect-81d0c9805a5bf069ec4be1a657699d8b").hide();
+  $(".HOME_MENU_RD .img-header-prog .mediaBlock .icon_player_prog").hide();
+  $(".HOME_MENU_RD .img-header-prog .mediaBlock .senal_active").hide();
+  $(
+    ".HOME_MENU_RD .img-header-prog .mediaBlock #rd_cont_botones .btn-gye"
+  ).hide();
+  $(
+    ".HOME_MENU_RD .img-header-prog .mediaBlock #rd_cont_botones .btn-quito"
+  ).hide();
+
+  console.log("dia " + day, "hora " + hour, "minuto " + min);
+
+  timeOfDay =
+    d.getHours().toString().padStart(2, "0") +
+    ":" +
+    d.getMinutes().toString().padStart(2, "0");
+  console.log(timeOfDay);
+
+  function showTelevistazoAmanecerComunidad() {
+    $(".HOME_MENU_RD .img-header-prog .mediaBlock #rd_cont_botones .btn-quito").show();
+  }
+
+ 
+  function activePlayer() {
+    $(".sect-81d0c9805a5bf069ec4be1a657699d8b").show();
+    $(".sect-81d0c9805a5bf069ec4be1a657699d8b").addClass("active_envivo");
+    $("#playerembed").removeClass("d-none");
+    console.log("player-activo");
+  }
+
+  function disabledPlayer() {
+    $("#playerembed").remove();
+    console.log("player-eliminado");
+  }
+
+  function menuEnvivo() {
+    $(".sect-81d0c9805a5bf069ec4be1a657699d8b").show();
+    $(".sect-81d0c9805a5bf069ec4be1a657699d8b").addClass("active_envivo");
+  }
+
+  function activeBlock(val) {
+    $(`.lista-programacion-bloque .noticias .article .BLOQUE_PROG_TIT_TXT.n${val} .text_block .text .text-wrapper`).addClass("activeBG");
+    var res = $(".text-wrapper.activeBG p:eq(1)").text();
+    $(".t_prog p").append(res);
+    $(".HOME_MENU_RD .img-header-prog .mediaBlock #rd_cont_botones .btn-gye").show();
+    $(".HOME_MENU_RD .img-header-prog .mediaBlock .senal_active").show();
+
+    localStorage.setItem("nItem", val);
+    console.log("no es nulo, valor:", val);
+    
+    $(".lista-programacion-bloque").addClass("swiper");
+    $(".lista-programacion-bloque .noticias").addClass("swiper-wrapper");
+    $(".lista-programacion-bloque .noticias .article").addClass("swiper-slide");
+  
+    var dpagination = document.createElement("div");
+    dpagination.classList = "swiper-scrollbar";
+    $(".lista-programacion-bloque").append(dpagination);
+  
+    var getNItem = localStorage.getItem("nItem");
+    // console.log("numero desde swiper prog: ",getNItem);
+    var numSlider = getNItem - 1;
+    // console.log(numSlider);
+  
+    var swiperProgramacionWeb = new Swiper(".lista-programacion-bloque", {
+      slidesPerView: 1.5,
+      spaceBetween: 10,
+      initialSlide: numSlider,
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+      breakpoints: {
+        600: {
+          slidesPerView: 1.5,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 3.5,
+          spaceBetween: 10,
+        },
+      },
+    });
+  }
 
 
+  // prueba si timeOfDay está dentro de un marco de tiempo determinado
+  if (day > 0 && day <= 5) {
+    //validacion de lunes a viernes
+    if ("05:55" <= timeOfDay && timeOfDay <= "06:55") {
+      activeBlock(1);
+      activePlayer();
+      showTelevistazoAmanecerComunidad();
+      $(".title_programa").append("Televistazo Al Amanecer");
+    } else if ("06:55" <= timeOfDay && timeOfDay <= "07:30") {
+      activeBlock(2);
+      activePlayer();
+      $(".title_programa").append("Contacto Directo");
+    } else if ("07:30" <= timeOfDay && timeOfDay <= "09:00") {
+      activeBlock(3);
+      activePlayer();
+      showTelevistazoAmanecerComunidad();
+      $(".title_programa").append("Televistazo en la comunidad");
+    } else if ("10:30" <= timeOfDay && timeOfDay <= "13:00") {
+      activeBlock(4);
+      activePlayer();
+      $(".title_programa").append("En Contacto");
+    } else if ("13:00" <= timeOfDay && timeOfDay <= "14:00") {
+      activeBlock(5);
+      activePlayer();
+      $(".title_programa").append("Televistazo 13h00");
+    } else if ("14:00" <= timeOfDay && timeOfDay <= "15:00") {
+      activeBlock(6);
+      menuEnvivo();
+      disabledPlayer();
+    } else if ("15:00" <= timeOfDay && timeOfDay <= "16:00") {
+      activeBlock(6);
+      menuEnvivo();
+      disabledPlayer();
+    } else if ("18:00" <= timeOfDay && timeOfDay <= "19:00") {
+      menuEnvivo();
+      disabledPlayer();
+    } else if ("19:00" <= timeOfDay && timeOfDay <= "19:59") {
+      activeBlock(7);
+      activePlayer();
+      $(".title_programa").append("Televistazo 19h00");
+    } else if ("20:00" <= timeOfDay && timeOfDay <= "21:00") {
+      activeBlock(8);
+      menuEnvivo();
+      disabledPlayer();
+    } else if ("21:00" <= timeOfDay && timeOfDay <= "22:00") {
+      activeBlock(8);
+      menuEnvivo();
+      disabledPlayer();
+    } else if ("22:00" <= timeOfDay && timeOfDay <= "23:00") {
+      activeBlock(8);
+      menuEnvivo();
+      disabledPlayer();
+    } else {
+      console.log("player no activo");
+      disabledPlayer();
+    }
+  } else if (day === 6) {
+    //validacion de sabado
+    console.log("es sabado");
 
-// });
+    if ("19:00" <= timeOfDay && timeOfDay <= "19:30") {
+      activePlayer();
+      $(".title_programa").append("Televistazo 19h00");
+    } else {
+      console.log("player no activo");
+      disabledPlayer();
+    }
+  } else {
+    //validacion de domingo
+    console.log("es domingo");
+
+    if ("10:30" <= timeOfDay && timeOfDay <= "11:30") {
+      activePlayer();
+      $(".title_programa").append("Políticamente Correcto");
+    } else if ("19:00" <= timeOfDay && timeOfDay <= "20:00") {
+      activePlayer();
+      $(".title_programa").append("Televistazo 19h00");
+    } else {
+      console.log("player no activo");
+      disabledPlayer();
+    }
+  }
+});
