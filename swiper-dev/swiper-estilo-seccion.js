38a81377d8b321e9bfd14892a90ca80f6a69e3af
Eri Vera <eriveraec@gmail.com>
2022-09-09 14:57:24 -0500

    Create swiper-estilo-seccion.js

---
+++ swiper-dev/swiper-estilo-seccion.js
@@ -0,0 +1,17 @@
+setTimeout(function () {
+  var noticiasCard = document.querySelector('.card_dest_estilo');
+  var noticiasEstilo = document.querySelector('.card_dest_estilo .noticias');
+  var articuloEstilo = document.querySelectorAll('.card_dest_estilo .noticias article');
+
+  noticiasCard.classList.add('swiper');
+  noticiasEstilo.classList.add('swiper-wrapper');
+
+  for (const art of articuloEstilo) {
+    art.classList.add('swiper-slide');
+  }
+
+  var swiperEsti = new Swiper(".card_dest_estilo", {
+    effect: "cards",
+    grabCursor: true,
+  });
+}, 500);
