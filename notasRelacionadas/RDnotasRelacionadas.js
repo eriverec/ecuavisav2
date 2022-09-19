b814ad595767d6eb994c4177fad915cea4f742e6
redy <granredy@gmail.com>
2022-09-19 18:07:56 -0500

    Update RDnotasRelacionadas.js

--- notasRelacionadas/RDnotasRelacionadas.js
+++ notasRelacionadas/RDnotasRelacionadas.js
@@ -16,3 +16,10 @@ function notasRelacionadas() {
 
 	}
 }
+		var rel = jQryIter(".ly-detail .related-viewer-portlet").html();
+		rel = "<div class='notasrelacionadas c_002_001'>" + rel + "</div>";	
+		jQryIter(".ly-detail .related-viewer-portlet").empty()
+
+		var content = $('.subheadline');  
+		// var midLength = parseInt(content.length/2);
+		content.after(rel);
