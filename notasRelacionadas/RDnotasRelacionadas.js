704ba7e330fdcb5f072cb773594619a9c8d12809
Eri Vera <eriveraec@gmail.com>
2022-09-19 15:48:38 -0500

    Update RDnotasRelacionadas.js

--- notasRelacionadas/RDnotasRelacionadas.js
+++ notasRelacionadas/RDnotasRelacionadas.js
@@ -1,1 +1,15 @@
 console.log ('cargado');
+
+function notasRelacionadas() {
+	if(jQryIter("body.signed-out").size() > 0) {
+
+		var rel = jQryIter(".ly-detail .related-viewer-portlet").html();
+		rel = "<div class='notasrelacionadas c_002_001'>" + rel + "</div>";	
+		jQryIter(".ly-detail .related-viewer-portlet").empty()
+
+		var content = $('.DETAIL_TXT .text').find('p');  
+		var midLength = parseInt(content.length/2);
+		content.eq(midLength).after(rel);
+
+	}
+}
