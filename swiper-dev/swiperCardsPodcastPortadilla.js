cb4bf9b6c27bbc9cde35afad5f82b8e2bf53745c
Eri Vera <eriveraec@gmail.com>
2022-09-26 09:04:43 -0500

    Update swiperCardsPodcastPortadilla.js

--- swiper-dev/swiperCardsPodcastPortadilla.js
+++ swiper-dev/swiperCardsPodcastPortadilla.js
@@ -1,15 +1,17 @@
-var classListaPodcastPor = document.querySelectorAll('.temas_destacados');
-var classListaPodcastPorNot = document.querySelectorAll('.temas_destacados .noticias');
-var classListaPodcastPorNotArt = document.querySelectorAll('.temas_destacados .noticias article');
+setTimeout(function () {
+  var classListaPodcastPor = document.querySelectorAll('.temas_destacados');
+  var classListaPodcastPorNot = document.querySelectorAll('.temas_destacados .noticias');
+  var classListaPodcastPorNotArt = document.querySelectorAll('.temas_destacados .noticias article');
 
-classListaPodcastPor.classList.add('swiper');
-classListaPodcastPorNot.classList.add('swiper-wrapper');
+  classListaPodcastPor.classList.add('swiper');
+  classListaPodcastPorNot.classList.add('swiper-wrapper');
 
-for (const swiperSlideClas of classListaPodcastPorNotArt) {
-  swiperSlideClas.classList.add('swiper-slide');
-}
+  for (const swiperSlideClas of classListaPodcastPorNotArt) {
+    swiperSlideClas.classList.add('swiper-slide');
+  }
 
-var swiperPodcastPor = new Swiper(".temas_destacados", {
-  effect: "cards",
-  grabCursor: true,
-});
+  var swiperPodcastPor = new Swiper(".temas_destacados", {
+    effect: "cards",
+    grabCursor: true,
+  });
+}, 500);
