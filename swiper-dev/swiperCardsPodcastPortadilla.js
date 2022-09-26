d6195cc5c89843f60e3aff6a4e9375a23b193d56
Eri Vera <eriveraec@gmail.com>
2022-09-26 09:02:37 -0500

    Create swiperCardsPodcastPortadilla.js

---
+++ swiper-dev/swiperCardsPodcastPortadilla.js
@@ -0,0 +1,15 @@
+var classListaPodcastPor = document.querySelectorAll('.temas_destacados');
+var classListaPodcastPorNot = document.querySelectorAll('.temas_destacados .noticias');
+var classListaPodcastPorNotArt = document.querySelectorAll('.temas_destacados .noticias article');
+
+classListaPodcastPor.classList.add('swiper');
+classListaPodcastPorNot.classList.add('swiper-wrapper');
+
+for (const swiperSlideClas of classListaPodcastPorNotArt) {
+  swiperSlideClas.classList.add('swiper-slide');
+}
+
+var swiperPodcastPor = new Swiper(".temas_destacados", {
+  effect: "cards",
+  grabCursor: true,
+});
