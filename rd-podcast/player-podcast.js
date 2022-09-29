17c6d0394f6f5488be389ec64724fdf298679fd1
Eri Vera <eriveraec@gmail.com>
2022-09-29 16:40:07 -0500

    Update player-podcast.js

--- rd-podcast/player-podcast.js
+++ rd-podcast/player-podcast.js
@@ -51,13 +51,16 @@ $('.IMG_TIT_TXT.mult').each(function (i, obj) {
   //obtenemos variables
   const mp3url = $(this).find('source').attr('src');
   const title_pd = $(this).find('h2').text();
-  
+
   // agregamos botones del player
   $(this).find('.media_block .pod_icon_play').append(`
   <svg class="play-stop-button" data-media="${mp3url}" data-title="${title_pd}" onclick="podcastPlay();" width="38"  height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
   <rect x="0.694336" y="0.975098" width="37" height="37" rx="3" fill="#23DCD1"  />
-  <path class="play-stop-button__stop" d="M11.889 12.5249H24.7223V24.5249H11.889V12.5249ZM11.8612 10.7749C11.3874 10.7749 10.93 10.9512 10.5903 11.2697C10.2501 11.5886 10.0557 12.0248 10.0557 12.4832V24.5666C10.0557 25.5246 10.8799 26.2749 11.8612 26.2749H24.7501C25.224 26.2749 25.6814 26.0986 26.021 25.7801C26.3612 25.4612 26.5557 25.025 26.5557 24.5666V12.4832C26.5557 12.0248 26.3612 11.5886 26.021 11.2697C25.6814 10.9512 25.224 10.7749 24.7501 10.7749H11.8612Z" fill="#2927B9" stroke="#2927B9" stroke-width="0.5"/>
+
+  <path class="play-stop-button__stop" d="M12.8333 13.75H25.6666V25.75H12.8333V13.75ZM12.8055 12C12.3317 12 11.8743 12.1763 11.5346 12.4948C11.1944 12.8137 11 13.2499 11 13.7083V25.7917C11 26.7497 11.8242 27.5 12.8055 27.5H25.6944C26.1683 27.5 26.6257 27.3237 26.9653 27.0052C27.3055 26.6863 27.5 26.2501 27.5 25.7917V13.7083C27.5 13.2499 27.3055 12.8137 26.9653 12.4948C26.6257 12.1763 26.1683 12 25.6944 12H12.8055Z" fill="#2927B9" stroke="#2927B9" stroke-width="0.5"/>
+
   <path class="play-stop-button__play" d="M13.0273 23.1822V12.3681C13.0273 11.5599 13.9364 11.0856 14.5993 11.5479L24.7893 18.6537C25.3633 19.0539 25.3593 19.9047 24.7817 20.2995L13.0273 28.335" stroke="#2927B9" stroke-width="2" stroke-linecap="round" />
+
 </svg>`);
 });
 // Fin preparador de contenidos reproducibles
