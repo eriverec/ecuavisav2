73172183d8f7a8dcfcf6b46bd4c3e1291208e29c
Eri Vera <eriveraec@gmail.com>
2022-09-30 09:17:40 -0500

    rollback js podcast

--- rd-podcast/player-podcast.js
+++ rd-podcast/player-podcast.js
@@ -46,20 +46,22 @@ new MediaElementPlayer(document.querySelector(".mainplayer"), options);
 // ####################################################################
 
 // preparador de contenidos reproducibles
-jQryIter('.IMG_TIT_TXT.mult').each(function (i, obj) {
-  jQryIter(this).find('.Multimedia_Text').css('display', 'none'); // ocultamos players
+$('.IMG_TIT_TXT.mult').each(function (i, obj) {
+  $(this).find('.Multimedia_Text').css('display', 'none'); // ocultamos players
   //obtenemos variables
-  const mp3url = jQryIter(this).find('source').attr('src');
-  const title_pd = jQryIter(this).find('h2').text();
+  const mp3url = $(this).find('source').attr('src');
+  const title_pd = $(this).find('h2').text();
 
   // agregamos botones del player
-  jQryIter(this).find('.media_block .pod_icon_play').append(`
-    <svg class="play-stop-button" data-media="${mp3url}" data-title="${title_pd}" onclick="podcastPlay();" width="38"  height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect x="0.694336" y="0.975098" width="37" height="37" rx="3" fill="#23DCD1"  />
+  $(this).find('.media_block .pod_icon_play').append(`
+  <svg class="play-stop-button" data-media="${mp3url}" data-title="${title_pd}" onclick="podcastPlay();" width="38"  height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <rect x="0.694336" y="0.975098" width="37" height="37" rx="3" fill="#23DCD1"  />
 
-    <path class="play-stop-button__stop" d="M12.8333 13.75H25.6666V25.75H12.8333V13.75ZM12.8055 12C12.3317 12 11.8743 12.1763 11.5346 12.4948C11.1944 12.8137 11 13.2499 11 13.7083V25.7917C11 26.7497 11.8242 27.5 12.8055 27.5H25.6944C26.1683 27.5 26.6257 27.3237 26.9653 27.0052C27.3055 26.6863 27.5 26.2501 27.5 25.7917V13.7083C27.5 13.2499 27.3055 12.8137 26.9653 12.4948C26.6257 12.1763 26.1683 12 25.6944 12H12.8055Z" fill="#2927B9" stroke="#2927B9" stroke-width="0.5"/>
+  <path class="play-stop-button__stop" d="M12.8333 13.75H25.6666V25.75H12.8333V13.75ZM12.8055 12C12.3317 12 11.8743 12.1763 11.5346 12.4948C11.1944 12.8137 11 13.2499 11 13.7083V25.7917C11 26.7497 11.8242 27.5 12.8055 27.5H25.6944C26.1683 27.5 26.6257 27.3237 26.9653 27.0052C27.3055 26.6863 27.5 26.2501 27.5 25.7917V13.7083C27.5 13.2499 27.3055 12.8137 26.9653 12.4948C26.6257 12.1763 26.1683 12 25.6944 12H12.8055Z" fill="#2927B9" stroke="#2927B9" stroke-width="0.5"/>
 
-    <path class="play-stop-button__play" d="M13.0273 23.1822V12.3681C13.0273 11.5599 13.9364 11.0856 14.5993 11.5479L24.7893 18.6537C25.3633 19.0539 25.3593 19.9047 24.7817 20.2995L13.0273 28.335" stroke="#2927B9" stroke-width="2" stroke-linecap="round" /></svg>`);
+  <path class="play-stop-button__play" d="M13.0273 23.1822V12.3681C13.0273 11.5599 13.9364 11.0856 14.5993 11.5479L24.7893 18.6537C25.3633 19.0539 25.3593 19.9047 24.7817 20.2995L13.0273 28.335" stroke="#2927B9" stroke-width="2" stroke-linecap="round" />
+
+</svg>`);
 });
 // Fin preparador de contenidos reproducibles
 
@@ -67,32 +69,28 @@ jQryIter('.IMG_TIT_TXT.mult').each(function (i, obj) {
 
 // Funcion de reproducción
 function podcastPlay() {
-  jQryIter(document).ready(function () {
-    var theMp3 = (event.currentTarget.getAttribute('data-media'));
-    var theTitle = (event.currentTarget.getAttribute('data-title'));
-    // detectamos si esta reproduciendo para dar stop o si reproduce otro audio
-    if (event.currentTarget.classList.contains('play-stop-button--playing')) {
-      setTimeout(function () {
-        jQryIter('.mejs__playpause-button').click();
-      }, 100);
-    } else {
-      playStop();
-    }
-    event.currentTarget.classList.toggle('play-stop-button--playing');
+  var theMp3 = (event.currentTarget.getAttribute('data-media'));
+  var theTitle = (event.currentTarget.getAttribute('data-title'));
+  // detectamos si esta reproduciendo para dar stop o si reproduce otro audio
+  if (event.currentTarget.classList.contains('play-stop-button--playing')) {
+    setTimeout(function () {
+      $('.mejs__playpause-button').click();
+    }, 100);
+  } else {
+    playStop();
+  }
+  event.currentTarget.classList.toggle('play-stop-button--playing');
 
-    jQryIter('.mainplayer').attr('src', theMp3);
-    jQryIter('.podcast__episode_title').html(theTitle);
-    jQryIter('.mejs__playpause-button').click();
-    console.log('url:' + theMp3);
-  });
+  $('.mainplayer').attr('src', theMp3);
+  $('.podcast__episode_title').html(theTitle);
+  $('.mejs__playpause-button').click();
+  console.log('url:' + theMp3);
 };
 
 function playStop() {
-  jQryIter(document).ready(function () {
-    var elements = document.querySelectorAll(".play-stop-button");
-    elements.forEach(element => {
-      element.classList.remove('play-stop-button--playing');
-    });
+  var elements = document.querySelectorAll(".play-stop-button");
+  elements.forEach(element => {
+    element.classList.remove('play-stop-button--playing');
   });
 };
 // End Funcion de reproducción
