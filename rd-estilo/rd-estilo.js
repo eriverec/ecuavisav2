2321a77b3822030256389ab3eb5a83d6237ba5fa
Eri Vera <eriveraec@gmail.com>
2022-10-02 09:42:50 -0500

    note pierdas js y css

---
+++ rd-estilo/rd-estilo.js
@@ -0,0 +1,43 @@
+//No te pierdas
+function noTePierdas() {
+  $('.note-pierdas').addClass('swiper');
+  $('.note-pierdas .noticias').addClass('swiper-wrapper');
+  $('.note-pierdas .noticias .article').addClass('swiper-slide');
+
+  var dpagination = document.createElement("div");
+  dpagination.classList = "swiper-pagination";
+  document.querySelector(".note-pierdas").appendChild(dpagination);
+
+  var notepierdas = new Swiper(".note-pierdas", {
+    slidesPerView: 1,
+    slidesPerColumn: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      360: {
+        slidesPerView: 1.5,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
+    },
+
+  });
+}
+
+
+setTimeout(function () {
+
+  noTePierdas();
+
+
+}, 500);
