b066f6785c632a6275f59daa344b2aca2bf8af12
Eri Vera <eriveraec@gmail.com>
2022-10-04 18:14:54 -0500

    Update pautasDigitales.js

--- rd-pautas-digitales/pautasDigitales.js
+++ rd-pautas-digitales/pautasDigitales.js
@@ -6,19 +6,25 @@
 // slc.href = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css";
 // document.head.appendChild(slc);
 
+setTimeout(function () {
+  swiperPautas();
+}, 500);
 
-var classListaPauta = document.querySelector('.banner_fijos_rd');
-var classListaPautaNot = document.querySelector('.banner_fijos_rd .noticias');
-var classListaPautaNotArt = document.querySelectorAll('.banner_fijos_rd .noticias .article');
+function swiperPautas() {
+  var classListaPauta = document.querySelector('.banner_fijos_rd');
+  var classListaPautaNot = document.querySelector('.banner_fijos_rd .noticias');
+  var classListaPautaNotArt = document.querySelectorAll('.banner_fijos_rd .noticias .article');
 
-classListaPauta.classList.add('swiper');
-classListaPautaNot.classList.add('swiper-wrapper');
+  classListaPauta.classList.add('swiper');
+  classListaPautaNot.classList.add('swiper-wrapper');
 
-for (const SlideProg of classListaPautaNotArt) {
-  SlideProg.classList.add('swiper-slide');
+  for (const SlideProg of classListaPautaNotArt) {
+    SlideProg.classList.add('swiper-slide');
+  }
+
+  var swiperPautas = new Swiper(".banner_fijos_rd", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+  });
+
 }
-
-var swiperPautas = new Swiper(".banner_fijos_rd", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-});
