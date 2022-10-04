03631f8aa9be790f3a823515f5721d8ee55ae810
Eri Vera <eriveraec@gmail.com>
2022-10-04 13:01:16 -0500

    Update pautasDigitales.js

---
+++ rd-pautas-digitales/pautasDigitales.js
@@ -0,0 +1,9 @@
+// var swjs = document.createElement("script");
+// swjs.src = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js";
+// document.head.appendChild(swjs);
+// var slc = document.createElement("link");
+// slc.rel = "stylesheet";
+// slc.href = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css";
+// document.head.appendChild(slc);
+
+
