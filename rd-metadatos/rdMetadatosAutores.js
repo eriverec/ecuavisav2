a36449e04836e496902c2f7a5952751086f0c45f
Eri Vera <eriveraec@gmail.com>
2022-10-06 13:07:31 -0500

    Update rdMetadatosAutores.js

--- rd-metadatos/rdMetadatosAutores.js
+++ rd-metadatos/rdMetadatosAutores.js
@@ -6,5 +6,8 @@ function getNameTag() {
   var selec = document.querySelector('.categoryListItemLink span');
   var metaSelec = selec.textContent;
   var cintillo = document.querySelector('.cintillo-single h2');
+  var dataAuthor = document.querySelector('.author_card .text_block .name_autor');
+
+  dataAuthor.append(`${metaSelec}`);
   cintillo.append(`Recopilación de todo lo publicado en Ecuavisa sobre ${metaSelec}.`);
 }
