5313f01ffb963ba6298c5724b36fc7dcfc71cf01
Eri Vera <eriveraec@gmail.com>
2022-10-14 11:03:52 -0500

    swiper notificaciones

--- rd-mi-perfil/rdMiPerfil.js
+++ rd-mi-perfil/rdMiPerfil.js
@@ -17,19 +17,58 @@ function DatosPersonales() {
   const getFirstName = localStorage.getItem('wylexFirstName');
   const getLastName = localStorage.getItem('wylexLastName');
   const getEmail = localStorage.getItem('wylexEmail');
-  
+
   var firstNameUpperCase = getFirstName.charAt(0).toUpperCase() + getFirstName.slice(1);
   var lastNameUpperCase = getLastName.charAt(0).toUpperCase() + getLastName.slice(1);
-  
+
   var nombreApellido = `${firstNameUpperCase} ${lastNameUpperCase}`;
-  
+
   var selectValue = document.querySelector('.outsidetName .insideValue');
   selectValue.innerHTML = `${nombreApellido}`;
 
   var selectValueEmail = document.querySelector('.outsideEmail .insideValue');
   var emailcorreo = getEmail;
   selectValueEmail.innerHTML = `${emailcorreo}`;
+
+}
+
+function swiperNotificaciones() {
+  var classListaNotification = document.querySelector('.notificationBox');
+  var classListaNotificationNot = document.querySelector('.notificationBox .noticias');
+  var classListaNotificationNotArt = document.querySelectorAll('.notificationBox .noticias .article');
+
+  classListaNotification.classList.add('swiper');
+  classListaNotificationNot.classList.add('swiper-wrapper');
+
+  for (const swiperSlideNotification of classListaNotificationNotArt) {
+    swiperSlideNotification.classList.add('swiper-slide');
+  }
+
+  var swiperNoti = new Swiper(".notificationBox", {
+    slidesPerView: 1,
+    spaceBetween: 20,
   
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+
 }
 
 
