125ae1f2d87c8507543c59009829819e37ff797e
Eri Vera <eriveraec@gmail.com>
2022-10-17 16:52:59 -0500

    Update pautasDigitales.js

--- rd-pautas-digitales/pautasDigitales.js
+++ rd-pautas-digitales/pautasDigitales.js
@@ -1,10 +1,10 @@
-// var swjs = document.createElement("script");
-// swjs.src = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js";
-// document.head.appendChild(swjs);
-// var slc = document.createElement("link");
-// slc.rel = "stylesheet";
-// slc.href = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css";
-// document.head.appendChild(slc);
+var swjs = document.createElement("script");
+swjs.src = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js";
+document.head.appendChild(swjs);
+var slc = document.createElement("link");
+slc.rel = "stylesheet";
+slc.href = "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css";
+document.head.appendChild(slc);
 
 setTimeout(function () {
   swiperPautas();
