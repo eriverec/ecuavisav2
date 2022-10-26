0f405bc2231a3c3ad6fd56fa5a52c1463fa31537
Eri Vera <eriveraec@gmail.com>
2022-10-26 12:14:31 -0500

    Create swiperAgendaDesktop.js

---
+++ swiper-dev/swiperAgendaDesktop.js
@@ -0,0 +1,30 @@
+function SwiperAgendaDesktop() {
+  //SWIPER AGENDA DESKTOP
+  const agendaDeskSwiper = document.querySelector('.agDesktopSwiper');
+  const agendaDeskWraper = document.querySelector('.agDesktopSwiper .noticias');
+  const agendaDeskSlider = document.querySelectorAll('.agDesktopSwiper .noticias .article');
+
+  var divPagDesk = document.createElement('div');
+  divPagDesk.classList.add('swiper-pagination');
+  agendaDeskSwiper.appendChild(divPagDesk);
+
+  agendaDeskSwiper.classList.add('swiper');
+  agendaDeskWraper.classList.add('swiper-wrapper');
+
+  for (const agendaDeskSliders of agendaDeskSlider) {
+    agendaDeskSliders.classList.add('swiper-slide');
+  }
+
+  var dymanicDesk = new Swiper(agendaDeskSwiper, {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+
+setTimeout(function () {
+  SwiperAgendaDesktop();
+}, 300);
