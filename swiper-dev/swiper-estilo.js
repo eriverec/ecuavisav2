d6de115ac7a6153de6212f70dd90419b8e1af04e
Eri Vera <eriveraec@gmail.com>
2022-10-28 12:14:53 -0500

    home estilo swiper optimixer

--- swiper-dev/swiper-estilo.js
+++ swiper-dev/swiper-estilo.js
@@ -1,20 +1,36 @@
-var classListaEstilo = document.querySelectorAll('.lista-bloque-estilo');
-var classListaEstiloNot = document.querySelectorAll('.lista-bloque-estilo .noticias');
-var classListaEstiloNotArt = document.querySelectorAll('.lista-bloque-estilo .noticias article');
+function swiperHomeEstilo() {
+  var classListaEstilo = document.querySelectorAll('.lista-bloque-estilo');
+  var classListaEstiloNot = document.querySelectorAll('.lista-bloque-estilo .noticias');
+  var classListaEstiloNotArt = document.querySelectorAll('.lista-bloque-estilo .noticias article');
 
-for (const swiper of classListaEstilo) {
-  swiper.classList.add('swiper');
+  for (const swiper of classListaEstilo) {
+    swiper.classList.add('swiper');
+  }
+
+  for (const swiperWa of classListaEstiloNot) {
+    swiperWa.classList.add('swiper-wrapper');
+  }
+
+  for (const swiperSlideClas of classListaEstiloNotArt) {
+    swiperSlideClas.classList.add('swiper-slide');
+  }
+
+  var swiperEstilo = new Swiper(".lista-bloque-estilo", {
+    effect: "cards",
+    grabCursor: true,
+  });
 }
 
-for (const swiperWa of classListaEstiloNot) {
-  swiperWa.classList.add('swiper-wrapper');
+function sGoHEstilo() {
+  setTimeout(function () {
+    if (typeof Swiper === "undefined") {
+      sGoHEstilo();
+    } else {
+      swiperHomeEstilo();
+    }
+  }, 400);
 }
 
-for (const swiperSlideClas of classListaEstiloNotArt) {
-  swiperSlideClas.classList.add('swiper-slide');
-}
-
-var swiperEstilo = new Swiper(".lista-bloque-estilo", {
-  effect: "cards",
-  grabCursor: true,
+$(document).ready(function () {
+  sGoHEstilo();
 });
