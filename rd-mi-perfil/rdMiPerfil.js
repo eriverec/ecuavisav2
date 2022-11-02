3f2b6f11b6bd958ee28c968f9d886c25383a69b5
crojano1995 <carlosrojano_@hotmail.com>
2022-11-01 20:14:56 -0500

    Update rdMiPerfil.js

--- rd-mi-perfil/rdMiPerfil.js
+++ rd-mi-perfil/rdMiPerfil.js
@@ -95,9 +95,66 @@ function swiperNotificaciones() {
   });
 }
 
+function eyesPass(){
+  $("input[type='password'][data-eye]").each(function(i) {
+      var $this = $(this),
+        id = 'eye-password-' + i,
+        el = $('#' + id);
+
+      $this.wrap($("<div/>", {
+        style: 'position:relative',
+        id: id
+      }));
+
+      $this.css({
+        paddingRight: 60
+      });
+      $this.after($("<div/>", {
+        html: 'Show',
+        class: 'btn btn-primary btn-sm',
+        id: 'passeye-toggle-'+i,
+      }).css({
+          position: 'absolute',
+          right: 10,
+          top: ($this.outerHeight() / 2) - 35,
+          padding: '2px 7px',
+          fontSize: 12,
+          cursor: 'pointer',
+      }));
+
+      $this.after($("<input/>", {
+        type: 'hidden',
+        id: 'passeye-' + i
+      }));
+
+      var invalid_feedback = $this.parent().parent().find('.invalid-feedback');
+
+      if(invalid_feedback.length) {
+        $this.after(invalid_feedback.clone());
+      }
+
+      $this.on("keyup paste", function() {
+        $("#passeye-"+i).val($(this).val());
+      });
+      $("#passeye-toggle-"+i).on("click", function() {
+        if($this.hasClass("show")) {
+          $this.attr('type', 'password');
+          $this.removeClass("show");
+          $(this).removeClass("btn-outline-primary");
+        }else{
+          $this.attr('type', 'text');
+          $this.val($("#passeye-"+i).val());        
+          $this.addClass("show");
+          $(this).addClass("btn-outline-primary");
+        }
+      });
+    });
+}
+
 let perfil = {
   notificaciones:{
     listar:function(){
+      eyesPass();
       let $articles = '';
       let $idusuario = localStorage.getItem('wylexUserId') || 0;
       fetch("https://estadisticas.ecuavisa.com/sites/gestor/zonaPrivada/getNotifications/index.php").then(response => {
