0bbe5ae07e5de623e812cc02080a1249c38b9397
crojano1995 <carlosrojano_@hotmail.com>
2022-11-21 20:34:56 -0500

    Update renderdata.js

--- grilla/v5/renderdata.js
+++ grilla/v5/renderdata.js
@@ -154,7 +154,7 @@ function renderProgramas(){
                 var timeAc = parseInt(timeOfDay.replace(regExp, "$1$2$3"));
 
 
-                var currentTime= moment(timeOfDay, "HH:mm:ss");
+                var currentTime= moment("17:20:00", "HH:mm:ss");
                 var startTime = moment(programTimeIni, "HH:mm:ss");
                 var endTime = moment(programTimeEnd, "HH:mm:ss");
 
