ee07d8c03ce89871df850bd99a6642838fe10cdc
Eri Vera <eriveraec@gmail.com>
2022-12-07 17:10:11 -0500

    Update rdLoginRegistro.js

--- rd-login-registro/rdLoginRegistro.js
+++ rd-login-registro/rdLoginRegistro.js
@@ -26,43 +26,67 @@ window.handleCredentialResponse = (response) => {
 }
 
 
+
+//Facebook init
+// window.fbAsyncInit = function () {
+//   FB.init({
+//     appId: '7328523993885687',
+//     xfbml: true,
+//     version: 'v15.0'
+//   });
+//   FB.AppEvents.logPageView();
+// };
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
+
 //Login facebook
-function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+function statusChangeCallback(response) { // Called with the results from FB.getLoginStatus().
   console.log('statusChangeCallback');
-  console.log(response);                   // The current login status of the person.
-  if (response.status === 'connected') {   // Logged into your webpage and Facebook.
-    testAPI();  
-  } else {                                 // Not logged into your webpage or we are unable to tell.
+  console.log(response); // The current login status of the person.
+  if (response.status === 'connected') { // Logged into your webpage and Facebook.
+    testAPI();
+  } else { // Not logged into your webpage or we are unable to tell.
     document.getElementById('status').innerHTML = 'Please log ' +
       'into this webpage.';
   }
 }
 
 
-function checkLoginState() {               // Called when a person is finished with the Login Button.
-  FB.getLoginStatus(function(response) {   // See the onlogin handler
+function checkLoginState() { // Called when a person is finished with the Login Button.
+  FB.getLoginStatus(function (response) { // See the onlogin handler
     statusChangeCallback(response);
   });
 }
 
 
-window.fbAsyncInit = function() {
+window.fbAsyncInit = function () {
   FB.init({
-    appId      : '617025596888772',
-    cookie     : true,                     // Enable cookies to allow the server to access the session.
-    xfbml      : true,                     // Parse social plugins on this webpage.
-    version    : 'v15.0'           // Use this Graph API version for this call.
+    appId: '7328523993885687',
+    cookie: true, // Enable cookies to allow the server to access the session.
+    xfbml: true, // Parse social plugins on this webpage.
+    version: 'v15.0' // Use this Graph API version for this call.
   });
 
 
-  FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
-    statusChangeCallback(response);        // Returns the login status.
+  FB.getLoginStatus(function (response) { // Called after the JS SDK has been initialized.
+    statusChangeCallback(response); // Returns the login status.
   });
 };
 
-function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+function testAPI() { // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
   console.log('Welcome!  Fetching your information.... ');
-  FB.api('/me', function(response) {
+  FB.api('/me?fields=id,name,email', function (response) {
     console.log('Successful login for: ' + response.name);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
