6e53834e4cd160f191f50130944c1280fe39f1c7
Eri Vera <eriveraec@gmail.com>
2022-12-07 12:43:57 -0500

    Update rdLoginRegistro.js

--- rd-login-registro/rdLoginRegistro.js
+++ rd-login-registro/rdLoginRegistro.js
@@ -1,4 +1,3 @@
-
 //Animacion de cambio de tabs
 const toggleForm = () => {
   const container_lr = document.querySelector('.container-lr');
@@ -6,10 +5,14 @@ const toggleForm = () => {
 }
 
 //Google Platform
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// }
+
+window.handleCredentialResponse = (response) => {
+  console.log(response);
 }
