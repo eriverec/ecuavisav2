b51f16b746b59f83dd420e5b499514cd717dbad4
Eri Vera <eriveraec@gmail.com>
2022-12-07 16:23:40 -0500

    Update rdLoginRegistro.js

--- rd-login-registro/rdLoginRegistro.js
+++ rd-login-registro/rdLoginRegistro.js
@@ -15,12 +15,32 @@ const toggleForm = () => {
 
 window.handleCredentialResponse = (response) => {
   const sData = response.credential.split(".");
-  console.log('sData:',sData);
+  console.log('sData:', sData);
   const gData = JSON.parse(atob(sData[1]));
-  console.log('gData:',gData);
+  console.log('gData:', gData);
   console.log("Email: " + gData.email);
   console.log("Email-verified: " + gData.email_verified);
   console.log("Firstname: " + gData.given_name);
   console.log("Lastname: " + gData.family_name);
   console.log("IMG-url: " + gData.picture);
 }
+
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: '617025596888772',
+    xfbml: true,
+    version: 'v15.0'
+  });
+  FB.AppEvents.logPageView();
+};
+
+(function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
