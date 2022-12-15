06b1cd667eceb3edd2a9d463f905cd16daaf04cc
Fredy Cruz <cruzfreddy_5@hotmail.com>
2022-12-15 00:19:50 -0500

    Update rdMiPerfil.js

--- rd-mi-perfil/rdMiPerfil.js
+++ rd-mi-perfil/rdMiPerfil.js
@@ -6,39 +6,39 @@ if (!UserId){
     va a redireccionar al login */
     //window.location = URL_login_G;
 }
-
-
-$(document).ready(async function()  {
-  const queryString = window.location.search;
+const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   var myHeaders = new Headers();
   const tk = urlParams.get('tk')
-myHeaders.append("Authorization", "JWT "+tk);
+  myHeaders.append("Authorization", "JWT "+tk);
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  redirect: 'follow'
-};
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  await fetch("https://ecuavisa-register.onrender.com/tasks", requestOptions)
+    .then(response => response.json())
+    .then (async (result) => {console.log(result); 
+      localStorage.wylexUserId=result.userId;
+      localStorage.wylexFirstName=result.first_name;
+      localStorage.wylexLastName=result.last_name;
+      localStorage.wylexEmail=result.email;
+      if(result.wylexAvatar){
+        localStorage.wylexAvatar=result.avatar;
+      }
+  //     var UserId = localStorage.getItem('wylexUserId'); //variable de id de usuario
+  // if (!UserId){
+  //     /*Si no existe sesión lo 
+  //     va a redireccionar al login */
+  //     window.location = URL_login_G;
+  // }
+    })
+    .catch(error => {console.log('error', error); window.location = URL_login_G});
+$(document).ready(async function()  {
+  
 
-await fetch("https://ecuavisa-register.onrender.com/tasks", requestOptions)
-  .then(response => response.json())
-  .then (async (result) => {console.log(result); 
-    localStorage.wylexUserId=result.userId;
-    localStorage.wylexFirstName=result.first_name;
-    localStorage.wylexLastName=result.last_name;
-    localStorage.wylexEmail=result.email;
-    if(result.wylexAvatar){
-      localStorage.wylexAvatar=result.avatar;
-    }
-//     var UserId = localStorage.getItem('wylexUserId'); //variable de id de usuario
-// if (!UserId){
-//     /*Si no existe sesión lo 
-//     va a redireccionar al login */
-//     window.location = URL_login_G;
-// }
-  })
-  .catch(error => {console.log('error', error); window.location = URL_login_G});
 });
 /* $( document ).ready( function() {
   
