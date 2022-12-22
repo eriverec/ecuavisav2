c336fb253c527f8c896c4b646a6efd166a44c7ce
Eri Vera <eriveraec@gmail.com>
2022-12-22 16:26:29 -0500

    Create olvidasteContraseña.js

---
+++ rd-login-registro/olvidasteContraseña.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $(".modal-footer button").click(function () {
+    var email = $('#forgetEmail').val();
+
+    if (IsEmail(email) == false) {
+      $('#invalid_email').show();
+      return false;
+    } else {
+      console.log('correo: ', email);
+      $('#invalid_email').hide();
+      $('.modal-footer').hide();
+      $('.modal-body').html(`
+      <div class="alert alert-success" role="alert">
+       Correo enviado con éxito
+     </div>
+     `);
+    }
+
+  });
+
+  var checkInput = (e) => {
+    const content = $("#forgetEmail").val().trim();
+    $('#forgetButton').prop('disabled', content === '');
+  };
+
+  $(document).on('keyup', '#forgetEmail', checkInput);
+
+
+  function IsEmail(email) {
+    var regex =
+      /^([a-zA-Z0-9_.-+])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+    if (!regex.test(email)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+});
