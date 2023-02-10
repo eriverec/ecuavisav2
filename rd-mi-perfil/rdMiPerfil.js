72f162755f9a638b141e989579885a9ecad60d92
Carlos Rojano <carlosrojano_@hotmail.com>
2023-02-09 21:21:40 -0500

    Update rdMiPerfil.js

--- rd-mi-perfil/rdMiPerfil.js
+++ rd-mi-perfil/rdMiPerfil.js
@@ -91,14 +91,14 @@ async function Token(){
     })
     .catch(error => {
       console.log('error', error); 
-      window.location = URL_login_G // comentar para modificar el modelo web
+      //window.location = URL_login_G // comentar para modificar el modelo web
     });
 
     var UserId = localStorage.getItem('wylexUserId'); //variable de id de usuario
     if (!UserId){
         /*Si no existe sesión lo 
         va a redireccionar al login */
-        window.location = URL_login_G; // comentar para modificar el modelo web
+        //window.location = URL_login_G; // comentar para modificar el modelo web
     }
 
     function collapse() {
