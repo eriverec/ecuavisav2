921631a43426ae820e892780968e886de6399663
Eri Vera <eriveraec@gmail.com>
2023-02-13 11:35:54 -0500

    ajustes en swiper especial

--- rd-especiales-ecuavisa/rd-especiales.js
+++ rd-especiales-ecuavisa/rd-especiales.js
@@ -1,7 +1,18 @@
 function cardEpecialEcu(){
-  $('.cardsLista').addClass('swiper');
-  $('.cardsLista .noticias').addClass('swiper-wrapper');
-  $('.cardsLista .noticias .article').addClass('swiper-slide');
+  // $('.cardsLista').addClass('swiper');
+  // $('.cardsLista .noticias').addClass('swiper-wrapper');
+  // $('.cardsLista .noticias .article').addClass('swiper-slide');
+
+  var classListaEspecial = document.querySelector('.cardsLista');
+  var classListaEspecialNot = document.querySelector('.cardsLista .noticias');
+  var classListaEspecialNotArt = document.querySelectorAll('.cardsLista .noticias article');
+
+  classListaEspecial.classList.add('swiper');
+  classListaEspecialNot.classList.add('swiper-wrapper');
+
+  for (const SlideEspeS of classListaEspecialNotArt) {
+    SlideEspeS.classList.add('swiper-slide');
+  }
 
   var swiperEsti = new Swiper(".cardsLista", {
     effect: "cards",
