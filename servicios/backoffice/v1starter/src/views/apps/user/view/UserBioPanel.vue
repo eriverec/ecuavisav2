66619d7aafbb259676d80d2c6e004aaf9f147b1c
redy <granredy@gmail.com>
2023-02-14 16:05:30 -0500

    Update UserBioPanel.vue

--- servicios/backoffice/v1starter/src/views/apps/user/view/UserBioPanel.vue
+++ servicios/backoffice/v1starter/src/views/apps/user/view/UserBioPanel.vue
@@ -300,19 +300,19 @@ const resolveUserRoleVariant = role => {
 
         <!-- 👉 Edit and Suspend button -->
         <VCardText class="d-flex justify-center">
-          <VBtn
-            variant="elevated"
-            class="me-3"
-            @click="isUserInfoEditDialogVisible = true"
-          >
-            Editar
-          </VBtn>
-          <VBtn
-            variant="tonal"
-            color="error"
-          >
-            Suspender
-          </VBtn>
+          // <VBtn
+          //   variant="elevated"
+          //   class="me-3"
+          //   @click="isUserInfoEditDialogVisible = true"
+          // >
+          //   Editar
+          // </VBtn>
+          // <VBtn
+          //   variant="tonal"
+          //   color="error"
+          // >
+          //   Suspender
+          // </VBtn>
         </VCardText>
       </VCard>
     </VCol>
