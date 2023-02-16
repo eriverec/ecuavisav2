af19ed99d623e5be84067ecc3736285928807bc9
Carlos Rojano <granredy@gmail.com>
2023-02-15 20:34:33 -0500

    Ver comportamiento del token

--- rd-login-registro/rdLoginRegistro.js
+++ rd-login-registro/rdLoginRegistro.js
@@ -2,6 +2,7 @@ var URL_principal_G = "https://www.ecuavisa.com/";
 var URL_login_G = "https://www.ecuavisa.com/servicios/login";
 var URL_perfil_G = "https://www.ecuavisa.com/servicios/perfil";
 var UserId = localStorage.getItem("wylexUserId");
+
 window.onload = () => {
   document.getElementById("forgetButton").disabled = false;
   let btnLogGoogle = document.getElementById("aLogGoogle");
