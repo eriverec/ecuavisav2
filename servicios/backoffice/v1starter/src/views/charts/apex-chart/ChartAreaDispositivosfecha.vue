28b145c8cc37cb3e2347577cce0b36b2075de035
redy <granredy@gmail.com>
2023-03-11 19:08:17 -0500

    Format vuejs graphics

--- servicios/backoffice/v1starter/src/views/charts/apex-chart/ChartAreaDispositivosfecha.vue
+++ servicios/backoffice/v1starter/src/views/charts/apex-chart/ChartAreaDispositivosfecha.vue
@@ -45,9 +45,9 @@ const getAreaChartSplineConfig = (themeColors) => {
   return {
     chart: {
       id: "crejemplo",
-      type: "bar",
+      //type: "area",
       parentHeightOffset: 0,
-      zoom: { enabled: false },
+      zoom: { enabled: true },
       toolbar: { show: false },
     },
     tooltip: {
@@ -95,7 +95,7 @@ const getAreaChartSplineConfig = (themeColors) => {
       },
     },
     xaxis: {
-      axisBorder: { show: false },
+      /**/axisBorder: { show: false },
       axisTicks: { color: themeBorderColor },
       crosshairs: {
         stroke: { color: themeBorderColor },
@@ -129,25 +129,276 @@ const series = [
 flatpickr.setDefaults({
   disableMobile: true
 });
-console.log(getAreaChartSplineConfig(vuetifyTheme.current.value));
-function obtenerFecha(selectedDates, dateStr, instance){
-  // actualiza la serie de datos filtrados y vuelve a renderizar el gráfico
-  var filteredChartSeries = [{
-    name: "Mobile",
-    data: [{
-            x: '2022-02-01',
-            y: 121,
-          },
-          {
-            x: '2022-02-12',
-            y: 240,
-          },
-          {
-            x: '2022-02-15',
-            y: 21,
-        }]
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+    return [year, month, day].join('-');
+}
+
+function groupBy(collection, property) {
+    var i = 0, val, index,
+        values = [], result = [];
+    for (; i < collection.length; i++) {
+        val = collection[i][property];
+        index = values.indexOf(val);
+        if (index > -1)
+            result[index].push(collection[i]);
+        else {
+            values.push(val);
+            result.push([collection[i]]);
+        }
+    }
+    return result;
+}
+
+function getDataTrazabilidad(){
+  var data = [{
+    "ip": "157.100.91.13",
+    "os": "Windows",
+    "browser": "Chrome",
+    "url": "https://www.ecuavisa.com/deportes",
+    "timestamp": "10/3/2023, 22:41:44",
+    "navigationRecord": [
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        },
+        {
+          "id": 2,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        },
+        {
+          "id": 3,
+          "url": "https://www.ecuavisa.com/mundo",
+          "fecha": "10/3/2023",
+          "hora": "22:05:19"
+        },
+        {
+          "id": 4,
+          "url": "https://www.ecuavisa.com/noticias/ecuador",
+          "fecha": "10/3/2023",
+          "hora": "22:40:16"
+        },
+        {
+          "id": 5,
+          "url": "https://www.ecuavisa.com/deportes",
+          "fecha": "10/3/2023",
+          "hora": "22:41:44"
+      }
+    ]
+  },
+  {
+    "ip": "157.100.91.13",
+    "os": "Windows",
+    "browser": "Opera",
+    "url": "https://www.ecuavisa.com/deportes",
+    "timestamp": "10/4/2023, 22:41:44",
+    "navigationRecord": [
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        },
+        {
+          "id": 2,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        },
+        {
+          "id": 4,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        }
+    ]
+  },
+  {
+    "ip": "157.100.91.13",
+    "os": "Windows",
+    "browser": "Opera",
+    "url": "https://www.ecuavisa.com/deportes",
+    "timestamp": "10/3/2023, 22:41:44",
+    "navigationRecord": [
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        },
+        {
+          "id": 2,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        },
+        {
+          "id": 2,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        },
+        {
+          "id": 2,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        },
+        {
+          "id": 2,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "14:40:16"
+        }
+    ]
+  },
+  {
+    "ip": "157.100.91.13",
+    "os": "Windows",
+    "browser": "Safari",
+    "url": "https://www.ecuavisa.com/deportes",
+    "timestamp": "10/3/2023, 22:41:44",
+    "navigationRecord": [
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        },
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        },
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        },
+        {
+          "id": 1,
+          "url": "https://www.ecuavisa.com/",
+          "fecha": "10/3/2023",
+          "hora": "2:03:38"
+        }
+    ]
   }];
-  ApexCharts.exec("crejemplo", "updateSeries", filteredChartSeries);
+
+  var dataGroupBrowser = groupBy(data, "browser");
+  var serieTotal = [];
+  
+  var dataFormat = [];
+  for(var i in dataGroupBrowser){
+    var serieData = [];
+    var nameSerie = '';
+    var total = 0;
+    for(var j in dataGroupBrowser[i]){
+      var renderData = dataGroupBrowser[i][j];
+      nameSerie = renderData.browser;
+      var count = Math.floor(Math.random()*2000);
+      var fecha = formatDate(renderData.timestamp);
+      total += count;
+      serieTotal.push(fecha);
+      serieData.push({
+        x: fecha,
+        y: count//renderData.navigationRecord.length
+      });
+    }
+
+    dataFormat.push({
+      name:nameSerie,
+      data:serieData,
+      total:total
+    });
+  }
+
+  let seriTotalFiltrada = serieTotal.filter((item,index)=>{
+    return serieTotal.indexOf(item) === index;
+  });
+
+  for(var i in dataFormat){
+    if(dataFormat[i].data.length != seriTotalFiltrada.length){
+      for(var z in seriTotalFiltrada){
+        var existe = false;
+        for(var j in dataFormat[i].data){
+          if(seriTotalFiltrada[z] == dataFormat[i].data[j].x){
+            existe = true;
+          }
+        }
+        if(!existe){
+          dataFormat[i].data.push({
+            x: seriTotalFiltrada[z],
+            y:0
+          })
+        }
+      }
+    }
+  }
+  //console.log(dataFormat.sort((a, b) => (a.topSpeed - b.topSpeed)))
+  return dataFormat.sort((a, b) => ( b.total - a.total));
+}
+
+function obtenerFecha(selectedDates, dateStr, instance){
+  //console.log(formatDate(selectedDates[0]))
+  // actualiza la serie de datos filtrados y vuelve a renderizar el gráfico
+  var filteredChartSeries = [
+
+    {
+        "name": "Safari",
+        "data": [
+            {
+                "x": "2023-10-03",
+                "y": 907
+            },
+            {
+                "x": "2023-10-04",
+                "y": 907
+            }
+        ]
+    },
+    {
+        "name": "Opera",
+        "data": [
+            {
+                "x": "2023-10-03",
+                "y": 652
+            },
+            {
+                "x": "2023-10-04",
+                "y": 856
+            }
+        ]
+    },
+    {
+        "name": "Chrome",
+        "data": [
+            {
+                "x": "2023-10-03",
+                "y": 640
+            },
+            {
+                "x": "2023-10-04",
+                "y": 0
+            }
+        ]
+    }
+];
+ApexCharts.exec("crejemplo", "updateSeries", getDataTrazabilidad());
 }
 
 </script>
