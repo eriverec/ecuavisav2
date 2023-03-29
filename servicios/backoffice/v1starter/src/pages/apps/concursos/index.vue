b3933971989e7a79185157e53d3806e9aa666024
Eri Vera <eriveraec@gmail.com>
2023-03-29 15:40:39 -0500

    evento concurso registro v1

--- servicios/backoffice/v1starter/src/pages/apps/concursos/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/concursos/index.vue
@@ -4,26 +4,34 @@ import { useTheme } from 'vuetify'
 import { hexToRgb } from '@layouts/utils'
 
 const vuetifyTheme = useTheme()
-const currentTab = ref(0)
-const refVueApexChart = ref()
+const preguntas = ref([])
+const preguntasConCount = ref({})
+
+const currentTheme = vuetifyTheme.current.value.colors
+const variableTheme = vuetifyTheme.current.value.variables
+const labelPrimaryColor = `rgba(${hexToRgb(currentTheme.primary)},${variableTheme['dragged-opacity']})`
+const legendColor = `rgba(${hexToRgb(currentTheme['on-background'])},${variableTheme['high-emphasis-opacity']})`
+const borderColor = `rgba(${hexToRgb(String(variableTheme['border-color']))},${variableTheme['border-opacity']})`
+const labelColor = `rgba(${hexToRgb(currentTheme['on-surface'])},${variableTheme['disabled-opacity']})`
+
+const columnColors = {
+    series1: '#826af9',
+    series2: '#d2b0ff',
+    bg: '#f8d3ff',
+  }
+
+/*NUEVO CODE DE EVETOS ECUAVISA*/
+// https://showandevents-service.vercel.app/all
+
 
 const chartConfigs = computed(() => {
-  const currentTheme = vuetifyTheme.current.value.colors
-  const variableTheme = vuetifyTheme.current.value.variables
-  const labelPrimaryColor = `rgba(${ hexToRgb(currentTheme.primary) },${ variableTheme['dragged-opacity'] })`
-  const legendColor = `rgba(${ hexToRgb(currentTheme['on-background']) },${ variableTheme['high-emphasis-opacity'] })`
-  const borderColor = `rgba(${ hexToRgb(String(variableTheme['border-color'])) },${ variableTheme['border-opacity'] })`
-  const labelColor = `rgba(${ hexToRgb(currentTheme['on-surface']) },${ variableTheme['disabled-opacity'] })`
-  
-  return [
-    {
-      title: 'Orders',
-      icon: 'tabler-shopping-cart',
+  return preguntas.value.map(pregunta => {
+    return {
       chartOptions: {
         chart: {
           parentHeightOffset: 0,
           type: 'bar',
-          toolbar: { show: false },
+          toolbar: { show: false }
         },
         plotOptions: {
           bar: {
@@ -31,173 +39,102 @@ const chartConfigs = computed(() => {
             startingShape: 'rounded',
             borderRadius: 4,
             distributed: true,
-            dataLabels: { position: 'top' },
-          },
+            dataLabels: { position: 'center' }
+          }
         },
         grid: {
           show: false,
-          padding: {
-            top: 0,
-            bottom: 0,
-            left: -10,
-            right: -10,
-          },
+          padding: { top: 0, bottom: 0, left: -10, right: -10 }
         },
-        colors: [
-          labelPrimaryColor,
-          labelPrimaryColor,
-          currentTheme.primary,
-          labelPrimaryColor,
-          labelPrimaryColor,
-          labelPrimaryColor,
-          labelPrimaryColor,
-          labelPrimaryColor,
-          labelPrimaryColor,
-        ],
+        colors: [columnColors.series2,columnColors.series1],
         dataLabels: {
           enabled: true,
-          formatter(val) {
-            return `${ val }k`
-          },
-          offsetY: -25,
+          formatter(val) { return `${val}` },
+          offsetY: 0,
           style: {
             fontSize: '15px',
             colors: [legendColor],
-            fontWeight: '600',
-            fontFamily: 'Public Sans',
-          },
+            fontWeight: '600'
+          }
         },
-        legend: { show: false },
+        legend: {
+          show: true,
+          position: 'top',
+          horizontalAlign: 'left',
+          labels: { colors: labelColor },
+        },
         tooltip: { enabled: false },
         xaxis: {
-          categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-          ],
-          axisBorder: {
-            show: true,
-            color: borderColor,
-          },
+          categories: preguntasConCount.value[pregunta].map(respuesta => respuesta.respuesta),
+          axisBorder: { show: true, color: borderColor },
           axisTicks: { show: false },
           labels: {
-            style: {
-              colors: labelColor,
-              fontSize: '14px',
-              fontFamily: 'Public Sans',
-            },
-          },
+            style: { colors: labelColor, fontSize: '14px' }
+          }
         },
         yaxis: {
           labels: {
             offsetX: -15,
-            formatter(val) {
-              return `${ parseInt(val / 1) }k`
-            },
-            style: {
-              fontSize: '14px',
-              colors: labelColor,
-              fontFamily: 'Public Sans',
-            },
+            formatter(val) { return `${parseInt(val / 1)}` },
+            style: { fontSize: '14px', colors: labelColor },
             min: 0,
             max: 60000,
-            tickAmount: 6,
-          },
-        },
-        responsive: [
-          {
-            breakpoint: 1441,
-            options: { plotOptions: { bar: { columnWidth: '41%' } } },
-          },
-          {
-            breakpoint: 590,
-            options: {
-              plotOptions: { bar: { columnWidth: '61%' } },
-              yaxis: { labels: { show: false } },
-              grid: {
-                padding: {
-                  right: 0,
-                  left: -20,
-                },
-              },
-              dataLabels: {
-                style: {
-                  fontSize: '12px',
-                  fontWeight: '400',
-                },
-              },
-            },
-          },
-        ],
+            tickAmount: 6
+          }
+        }
       },
-      series: [{
-        data: [
-          28,
-          10,
-          45,
-          38,
-          15,
-          30,
-          35,
-          30,
-          8,
-        ],
-      }],
+      series: [{ data: preguntasConCount.value[pregunta].map(respuesta => respuesta.count ) }]
     }
-  ]
+  })
 })
+
+async function fetchData() {
+  const response = await fetch('https://showandevents-service.vercel.app/all')
+  const data = await response.json()
+  console.log(data)
+
+  data.data.forEach(trivia => {
+    trivia.data.forEach(datos => {
+      datos.trivia.forEach(pregunta => {
+        if (preguntas.value.includes(pregunta.pregunta)) {
+          const respuesta = preguntasConCount.value[pregunta.pregunta].find(
+            item => item.respuesta === pregunta.respuesta
+          )
+          if (respuesta !== undefined) respuesta.count++
+          else {
+            preguntasConCount.value[pregunta.pregunta].push({
+              respuesta: pregunta.respuesta,
+              count: 1
+            })
+          }
+        } else {
+          preguntas.value.push(pregunta.pregunta)
+          preguntasConCount.value[pregunta.pregunta] = [{
+            respuesta: pregunta.respuesta,
+            count: 1
+          }]
+        }
+      })
+    })
+  })
+}
+
+onMounted(() => {
+  fetchData()
+})
+
 </script>
 
 <template>
-  <VCard
-    title="Concursos y eventos"
-    subtitle="Yearly Earnings Overview"
-  >
-    <template #append>
-      <div class="mt-n4 me-n2">
-        <VBtn
-          icon
-          size="x-small"
-          variant="plain"
-          color="default"
-        >
-          <VIcon
-            size="22"
-            icon="tabler-dots-vertical"
-          />
-
-          <VMenu activator="parent">
-            <VList>
-              <VListItem
-                v-for="(item, index) in ['View More', 'Delete']"
-                :key="index"
-                :value="index"
-              >
-                <VListItemTitle>{{ item }}</VListItemTitle>
-              </VListItem>
-            </VList>
-          </VMenu>
-        </VBtn>
-      </div>
-    </template>
-
-    <VCardText>
-      
-
-      <VueApexCharts
-        ref="refVueApexChart"
-        :key="currentTab"
-        :options="chartConfigs[Number(currentTab)].chartOptions"
-        :series="chartConfigs[Number(currentTab)].series"
-        height="240"
-        class="mt-3"
-      />
-    </VCardText>
-  </VCard>
+  <div>
+    <h1>Tabla de Respuestas de Usuarios</h1>
+    <div v-for="(pregunta, index) in preguntas" :key="index">
+      <h3>{{ pregunta }}</h3>
+      <VueApexCharts 
+      :options="chartConfigs[index].chartOptions" 
+      :series="chartConfigs[index].series" 
+      height="240"
+        class="mt-3" />
+    </div>
+  </div>
 </template>
