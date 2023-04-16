5f0b923ad2f7f9a4b241265faa4d2de3da388e01
redy <granredy@gmail.com>
2023-04-16 18:24:14 -0500

    Cambiado el título
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- servicios/backoffice/v1starter/src/views/dashboards/traceability/UserTabIntereses.vue
+++ servicios/backoffice/v1starter/src/views/dashboards/traceability/UserTabIntereses.vue
@@ -93,12 +93,12 @@ onMounted(async () =>
 <template>
   <VCard>
     <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-      <VCardTitle>New Technologies Data API</VCardTitle>
+      <VCardTitle>Intereses de los usuarios suscritos</VCardTitle>
 
       <template #append>
         <!-- <input type="text" id="date-picker" ref="datePicker" /> -->
           <div class="date-picker-wrapper" style="width: 273px">
-            <AppDateTimePicker id="date-picker" placeholder="Seleccionar una fecha" prepend-inner-icon="tabler-calendar"
+            <AppDateTimePicker id="date-picker" placeholder="Seleccionar un rango de fecha" prepend-inner-icon="tabler-calendar"
               density="compact" 
               @on-change="filtrarDatos"
               :config="{ 
