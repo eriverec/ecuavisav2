f51f8cb6ff54ac33e7cd0124a931d5c89123bbc5
redy <granredy@gmail.com>
2023-05-05 16:29:11 -0500

    add bloque de sugerencia de usuarios
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- rd-mi-perfil/rdMiPerfil.js
+++ rd-mi-perfil/rdMiPerfil.js
@@ -1265,6 +1265,171 @@ var modalUsuarioSugerencia = {
 
     collapse();
 
+/**********************BLOQUE MODAL DE SUGERENCIAS**********************/
+var bloqueUsuarioSugerencia = {
+      idBloque:"bloque_body_seguimiento_tema",
+      idListadoTema:"listado-temas-modal",
+      diBloqueSeguiT:"bloque_titulo_seguimiento_tema",
+      title:function(){
+        //document.querySelector('#'+this.diBloqueSeguiT).innerHTML = `Cuéntanos sobre ti`;
+      },
+      load:function(){
+        var classListaTemas = document.querySelector('#'+this.idListadoTema);
+        classListaTemas.classList.remove("isDisabled");
+        return true;
+      },
+      btn_click:function(id, title){
+        var getUser = ECUAVISA_EC.USER_data();
+        var template = document.getElementById(`b_template_${id}`);
+        var btn = document.getElementById(`b_btn_${id}`);
+        var estado = 0;
+        btn.setAttribute('disabled', true);
+        if (template.classList.contains('remove')) {
+          template.classList.remove('remove');
+          estado = 0;
+        } else {
+          template.classList.add('remove');
+          estado = 1;
+        }
+        btn.removeAttribute('disabled');
+
+        var resp = fetch("https://sugerencias-ecuavisa.vercel.app/addsugerencia", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            "userId": getUser.id,
+            "sugerenciaId": id,
+            "estado": estado
+          }),
+        }).then( (response) => response.json())
+        .then( (result) => {
+          console.log(result)
+          return true;
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+      },
+      existeSugerencia:function(sugerenciaId, data){
+        for(var i in data){
+          if(data[i].sugerenciaId == sugerenciaId && data[i].meta_existe == true){
+            return true;
+          }
+        }
+        return false;
+      },
+      initComponent:function(){
+        var classListalistDrivers = document.querySelector('.MODAL_SEGUIMIENTO_TEMA');
+        var dpagination = document.createElement("div");
+        dpagination.setAttribute("id", "bloque_body_seguimiento_tema");
+        classListalistDrivers.append(dpagination);
+      },
+      body:function(){
+        document.querySelector('#'+this.idBloque).innerHTML=`<div class="spinner-border" role="status">
+          <span class="sr-only">Loading...</span>
+        </div>`;
+        var getUser = ECUAVISA_EC.USER_data();
+        var fun = this;
+        /*FETCH*/
+        var myHeaders = new Headers();
+        var urlSugerencia = "https://sugerencias-ecuavisa.vercel.app/all?userId="+getUser.id;
+        var requestOptions = {
+          method: 'GET',
+          headers: myHeaders
+        };
+        fetch(urlSugerencia, requestOptions).then(response => {
+          return response.json();
+        }).then(jsondata => {
+          if (jsondata.resp) {
+            /*FETCH FIN*/
+            fun.temas = [ { "interes":"Sugerencias", "data":jsondata.data } ];
+            var temasSeguir = ``;
+            for(var i in fun.temas){
+              var ins = fun.temas[i];
+              temasSeguir += `<p class="mis-intereses-modal py-1 m-0" style="width:100%">
+                ${ins.interes}
+               </p>`;
+
+               for(var j in ins.data){
+                var dat = ins.data[j];
+                  dat["id"] = dat._id;
+                  dat["name"] = dat.title;
+                  if(dat.estado){
+                    temasSeguir+= `<div class="item_tema t_${dat.id}">
+                       <div class="keywords font-2 fs13">
+                          <div class="template-meta-favorite-action ${fun.existeSugerencia(dat.id, jsondata.dataSugerenciasSeguido)?'remove':''}" value="${dat.name}" id="b_template_${dat.id}" title="Seguir sugerencia" onclick="bloqueUsuarioSugerencia.btn_click('${dat.id}', '${dat.name}')" style="/* display:none; */">
+                             <button type="button" class="button_seguir btn btn-default btn-sm btn-modal-seguir" id="b_btn_${dat.id}">
+
+                                <small>${dat.name}</small>
+
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-tag" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                  <path fill-rule="evenodd" d="M2 2v4.586l7 7L13.586 9l-7-7H2zM1 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 1 6.586V2z"></path>
+                                  <path fill-rule="evenodd" d="M4.5 5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"></path>
+                                </svg>
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-tag-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                  <path fill-rule="evenodd" d="M2 1a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l4.586-4.586a1 1 0 0 0 0-1.414l-7-7A1 1 0 0 0 6.586 1H2zm4 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"></path>
+                                </svg>
+                             </button> 
+                          </div>
+                       </div>
+                   </div>`;
+                  }
+               }
+              
+            }
+
+            document.querySelector('#'+this.idBloque).innerHTML = `
+            <div class="contenido-modal">
+               <p class="parrafo-modal">
+                Para ofrecerte una mejor experiencia elige los intereses que se ajusten a ti.
+               </p>
+               <p class="mis-intereses-modal fw-bold">
+                Mis intereses
+               </p>
+               <hr>
+               <div class="listado-temas isDisabled" id="${this.idListadoTema}" style=" overflow: auto; max-height: 350px; ">
+                  ${temasSeguir}
+               </div>
+            </div>`;
+
+            fun.load();
+          }
+        });
+      },
+      existeTemaSeguimiento:function(){
+        var num = 0;
+        var highlightedItemss = document.querySelectorAll(".template-meta-favorite-action");
+        highlightedItemss.forEach((userItem) => {
+          if(userItem.classList.contains('remove')){
+            num ++;
+          }
+        });
+
+        return {
+          existe: num > 0,
+          num : num
+        };
+      },
+      init:function(){
+        var numIter = 1;
+        var ins = this;
+        ins.title();
+        ins.initComponent();
+        ins.body();
+      },
+      temas:[
+        {
+          "interes":"Noticias",
+          "data":[
+            {id:'', name:''},
+          ]
+        }
+      ]
+    }
+/**********************BLOQUE MODAL DE SUGERENCIAS**********************/
+
 // ##############BUENAS PRACTICAS##################
 if (!ECUAVISA_EC.login() && urlParamsGET.get('tk')==null){ /*Si no existe sesión lo va a redireccionar al login */
 }else{  DataEntrada();  DatosPersonales();   }
