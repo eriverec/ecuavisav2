4b6acc0641d8a4ce4c7830207e4ccc14addd9945
gholguinm <88909737+gholguinm@users.noreply.github.com>
2023-05-10 15:46:35 -0500

    ocultar tarjetas 10-5-23

--- swiper-dev/s-c-estilo-seccion.js
+++ swiper-dev/s-c-estilo-seccion.js
@@ -1,23 +1,23 @@
-setTimeout(function () {
-  // var noticiasCard = document.querySelector('.card_dest_estilo');
-  // var noticiasEstilo = document.querySelector('.card_dest_estilo .noticias');
-  // var articuloEstilo = document.querySelectorAll('.card_dest_estilo .noticias article');
+// setTimeout(function () {
+//   // var noticiasCard = document.querySelector('.card_dest_estilo');
+//   // var noticiasEstilo = document.querySelector('.card_dest_estilo .noticias');
+//   // var articuloEstilo = document.querySelectorAll('.card_dest_estilo .noticias article');
   
 
-  // noticiasCard.classList.add('swiper');
-  // noticiasEstilo.classList.add('swiper-wrapper');
+//   // noticiasCard.classList.add('swiper');
+//   // noticiasEstilo.classList.add('swiper-wrapper');
 
-  // for (const art of articuloEstilo) {
-  //   art.classList.add('swiper-slide');
-  // }
+//   // for (const art of articuloEstilo) {
+//   //   art.classList.add('swiper-slide');
+//   // }
 
-  $('.card_dest_estilo').addClass('swiper');
-  $('.card_dest_estilo .noticias').addClass('swiper-wrapper');
-  $('.card_dest_estilo .noticias .article').addClass('swiper-slide');
+//   $('.card_dest_estilo').addClass('swiper');
+//   $('.card_dest_estilo .noticias').addClass('swiper-wrapper');
+//   $('.card_dest_estilo .noticias .article').addClass('swiper-slide');
 
-  var swiperEsti = new Swiper(".card_dest_estilo", {
-    effect: "cards",
-    grabCursor: true,
-  });
-}, 500);
+//   var swiperEsti = new Swiper(".card_dest_estilo", {
+//     effect: "cards",
+//     grabCursor: true,
+//   });
+// }, 500);
 
