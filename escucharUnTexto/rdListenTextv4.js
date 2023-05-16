1289630f29dd3b8b682ad517c53c27a757e27448
Eri Vera <eriveraec@gmail.com>
2023-05-16 14:48:14 -0500

    Create rdListenTextv4.js

---
+++ escucharUnTexto/rdListenTextv4.js
@@ -0,0 +1,66 @@
+
+var getIdArticle = "5134589";
+var playButton = document.getElementById('playButton_2');
+var pauseButton = document.getElementById('pauseButton_2');
+var pauseBtn = false;
+
+function stripHtmlTags(text) {
+  return text.replace(/<[^>]+>/g, '');
+}
+// var getIdArticle = ITER.CONTEXT.articleId;
+async function getJsonArticle() {
+  try {
+    var resp = await fetch(`https://estadisticas.ecuavisa.com/sites/services/global/mirrorGetVoice.php?idarticulo=${getIdArticle}`); 
+    var obtener = await resp.json();
+    const components = obtener.article.content.component;
+    var textValues = "";
+    for (let component of components) {
+      if (component.name === 'Text') {
+        textValues += component.__text;
+      }
+    }
+    var plainText = stripHtmlTags(textValues);
+    playButton.addEventListener('click', () => {
+      if(responsiveVoice.voiceSupport()) {
+        playButton.classList.add("disabled");
+        playButton.setAttribute("disabled", true);
+
+        responsiveVoice.speak(plainText, 'Spanish Male', {
+          onend: function() {
+            pauseBtn = false;
+            pauseButton.innerHTML = "Pausar";
+            playButton.classList.remove("disabled");
+            playButton.removeAttribute("disabled");
+
+            pauseButton.classList.add("disabled");
+            pauseButton.setAttribute("disabled", true);
+          }
+        });
+
+        pauseButton.classList.remove("disabled");
+        pauseButton.removeAttribute("disabled");
+      }else{
+        console.log("El traductor no es compatible")
+      }
+    });
+
+    pauseButton.addEventListener('click', () => {
+      if(!pauseBtn){
+        pauseBtn = true;
+        pauseButton.innerHTML = "Seguir";
+        responsiveVoice.pause();
+      }else{
+        pauseBtn = false;
+        pauseButton.innerHTML = "Pausar";
+        responsiveVoice.resume();
+      }
+    });
+
+    return true;
+  } catch (error) {
+    console.log('Error:', error);
+  }
+}
+
+
+getJsonArticle();
