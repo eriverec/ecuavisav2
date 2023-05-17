ac36d25fe166d243b56b42117c05b827bcba4a7c
redy <granredy@gmail.com>
2023-05-17 16:05:11 -0500

    Update index.js
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- servicios/speechsynthesis/src/routes/index.js
+++ servicios/speechsynthesis/src/routes/index.js
@@ -10,6 +10,7 @@ import { SECRET_ACCESSKEY } from "./../../src/config.js";
 import { REGION } from "./../../src/config.js";
 import { S3Client } from "@aws-sdk/client-s3";
 
+const MAX_TEXT_LENGTH = 3000;
 export const routes = Router();
 moment.tz.setDefault('America/Guayaquil');
 
@@ -42,23 +43,61 @@ const s3 = new AWS.S3();
 // Crea una instancia del cliente Polly
 const polly = new AWS.Polly();
 
+function stripHtmlTags(text) {
+  return text.replace(/<[^>]+>/g, '');
+}
+
+function textPARTS(text){
+	const textParts = [];
+	for (let i = 0; i < text.length; i += MAX_TEXT_LENGTH) {
+	  const part = text.substr(i, MAX_TEXT_LENGTH);
+	  textParts.push(part);
+	}
+	return textParts[0];
+}
+
+async function getArticle(getIdArticle) {
+  var resp = await fetch(`https://estadisticas.ecuavisa.com/sites/services/global/mirrorGetVoice.php?idarticulo=${getIdArticle}`); 
+  var obtener = await resp.json();
+
+  if(obtener != null){
+  	const components = obtener.article.content.component;
+	  var textValues = "";
+	  for (let component of components) {
+	    if (component.name === 'Text') {
+	      textValues += component.__text;
+	    }
+	  }
+	  var plainText = stripHtmlTags(textValues);
+	  return plainText;
+  }
+
+  return "";
+};
+
 routes.get("/", async function (req, res) {
-	return res.json("Servicio de speechsynthesis: v0.01 - "+moment().format('MMMM Do YYYY, h:mm:ss a'));
+	return res.json("Servicio de speechsynthesis: v0.02 - "+moment().format('MMMM Do YYYY, h:mm:ss a'));
 });
 
 routes.get("/speechsynthesis", async function (req, res) {
 	try {
-		var { texto="" } = req.query;
-		if(texto=="" ){
-			return res.status(200).send({ resp:false,message: "Falta el texto" });
+		var { idArticle="" } = req.query;
+		if(idArticle=="" ){
+			return res.status(200).send({ resp:false,message: "Falta el idArticle" });
 		}
 		// Define el texto que quieres convertir a voz
 	  //const texto = 'Hola, esto es un ejemplo de texto a voz utilizando AWS Polly.';
 
 	  // Configura los parámetros para la solicitud de Polly
+	  var text_Article = await getArticle(idArticle);
+	  if(text_Article == ""){
+	  	return res.status(200).send({ resp:false,message: "No existe el artículo" }); 
+	  }
+	  text_Article = textPARTS(text_Article);
+
 	  const params = {
 	    OutputFormat: 'mp3',
-	    Text: texto,
+	    Text: text_Article,
 	    VoiceId: 'Mia',
 	  };
 
@@ -66,7 +105,7 @@ routes.get("/speechsynthesis", async function (req, res) {
 	  polly.synthesizeSpeech(params, (err, data) => {
 	    if (err) {
 	      //console.log('Error al obtener el audio de Polly:', err);
-	      res.status(500).send('Error al obtener el audio de Polly');
+	      res.status(500).send('Error al obtener el audio de Polly'+err);
 	    } else if (data.AudioStream instanceof Buffer) {
 	      // Guarda el archivo de audio en el bucket de S3
 	      const audioBuffer = Buffer.from(data.AudioStream);
