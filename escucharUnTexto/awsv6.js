deb7aecf4b8f14d01f7fce30cfbc2ca0a9bfaffe
Eri Vera <eriveraec@gmail.com>
2023-05-18 09:43:33 -0500

    consumir la api al dar clic

--- escucharUnTexto/awsv6.js
+++ escucharUnTexto/awsv6.js
@@ -1,98 +1,98 @@
-
 // var getIdArticle = "5134589";
 // var getIdArticle = "5154428";
 var getIdArticle = ITER.CONTEXT.articleId;
 
 // const audioElement = document.createElement('audio');
 
-// Hacer una solicitud a la ruta /audio en Express para obtener el audio en base64
-fetch(`https://aws-polly.vercel.app/speechsynthesis?idArticle=${getIdArticle}`)
-  .then(response => response.text())
-  .then(audioBase64 => {
-    // Decodificar el audio base64 en un blob
-    const audioBlob = base64toBlob(audioBase64, 'audio/mp3');
+const audioElement = document.createElement('audio');
+const playPauseButton = document.querySelector('#play-pause-btn');
+const botonPlay = document.querySelector('.boton-play');
+let isPlaying = false;
+let isLoaded = false;
 
-    // Crear una URL del blob para reproducir el audio
-    const audioUrl = URL.createObjectURL(audioBlob);
-
-    // Reproducir el audio en una etiqueta <audio>
-    const audioElement = document.createElement('audio');
-    audioElement.src = audioUrl;
-    audioElement.controls = true;
-    // document.body.appendChild(audioElement);
-
-    const selecArea = document.querySelector('.boton-play');
-
-    // const botonElement = document.createElement('button');
-    // botonElement.setAttribute('id', 'play-pause-btn');
-    // botonElement.textContent = 'Play';
-
-    const ele = `
-      <button id="play-pause-btn">
-        <div style="display: flex; align-items: end; padding: 8px 104px 8px 8px; margin: -8px -104px -8px -8px;">
-          <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
-          <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
-          <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
-          <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
-        </div>
-        <span>Play</span>
-      </button>
-    `
-
-    selecArea.innerHTML = ele;
-
-    const playPauseButton = document.querySelector('#play-pause-btn');
-    let isPlaying = false;
-
-    const playAudio = () => {
-      audioElement.play();
-      playPauseButton.innerHTML =
-        playPauseButton.innerHTML = ` 
-      <div style="display: flex; align-items: end; padding: 8px 104px 8px 8px; margin: -8px -104px -8px -8px;">
+const playAudio = () => {
+  audioElement.play();
+  playPauseButton.innerHTML = ` 
+      <div style="display: flex; align-items: end; padding: 8px 104px 8px 8px; margin: -8px -104px -2px -8px;">
         <div class="event-play event-play-irGthP-css"></div>
         <div class="event-play event-play-ieYnvbI-css"></div>
         <div class="event-play event-play-igThDMz-css"></div>
         <div class="event-play event-play-igxkfZF-css"></div>
       </div>
-      <span>Pause</span>
+      <span>Pausar</span>
       `;
-      isPlaying = true;
-    };
+  isPlaying = true;
+};
 
-    const pauseAudio = () => {
-      audioElement.pause();
-      playPauseButton.innerHTML = ` 
-        <div style="display: flex; align-items: end; padding: 8px 104px 8px 8px; margin: -8px -104px -8px -8px;">
+const pauseAudio = () => {
+  audioElement.pause();
+  playPauseButton.innerHTML = ` 
+        <div style="display: flex; align-items: end; padding: 8px 104px 8px 8px; margin: -8px -104px -2px -8px;">
           <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
           <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
           <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
           <div class="event-pause wpds-c-iyKTdE-ijwIBRZ-css"></div>
         </div>
-        <span>Play</span>
+        <span>Reproducir</span>
       `;
-      isPlaying = false;
-    };
+  isPlaying = false;
+};
 
-    const handleVisibilityChange = () => {
-      if (document.hidden && isPlaying) {
-        pauseAudio();
-      }
-    };
+const handleVisibilityChange = () => {
+  if (document.hidden && isPlaying) {
+    pauseAudio();
+  }
+};
 
-    playPauseButton.addEventListener('click', () => {
-      if (isPlaying) {
-        pauseAudio();
-      } else {
+const handleClick = () => {
+  if (isLoaded) { // Sólo carga la solicitud API una vez
+    if (isPlaying) {
+      pauseAudio();
+    } else {
+      playAudio();
+    }
+  } else {
+    playPauseButton.innerHTML = ` <span>Cargando...</span>`;
+
+    // Hacer una solicitud a la ruta /audio en Express para obtener el audio en base64
+    fetch(`https://aws-polly.vercel.app/speechsynthesis?idArticle=${getIdArticle}`)
+      .then(response => response.text())
+      .then(audioBase64 => {
+        // Decodificar el audio base64 en un blob
+        const audioBlob = base64toBlob(audioBase64, 'audio/mp3');
+
+        // Crear una URL del blob para reproducir el audio
+        const audioUrl = URL.createObjectURL(audioBlob);
+
+        // Reproducir el audio en una etiqueta <audio>
+        audioElement.src = audioUrl;
+        audioElement.controls = true;
+
+        // botonPlay.appendChild(audioElement);
+        
+        playPauseButton.style.display = 'flex';
+        isLoaded = true; // Cambia el estado a verdadero cuando se completa la solicitud satisfactoriamente
+
         playAudio();
-      }
-    });
 
-    document.addEventListener('visibilitychange', handleVisibilityChange);
+      })
+      .catch(err => {
+        console.error('Error al obtener el audio:', err);
+      });
+  }
+};
 
-  })
-  .catch(err => {
-    console.error('Error al obtener el audio:', err);
-  });
+playPauseButton.addEventListener('click', handleClick);
+
+playPauseButton.innerHTML = `
+<div style="display: flex; align-items: end; padding: 8px 104px 8px 8px; margin: -8px -104px -8px -8px;">
+  <svg style="width: 20px;" width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm8.856-3.845A1.25 1.25 0 0 0 9 9.248v5.504a1.25 1.25 0 0 0 1.856 1.093l5.757-3.189a.75.75 0 0 0 0-1.312l-5.757-3.189Z" fill="#212121"/></svg>
+</div>
+<span>Escuchar</span>
+`;
+botonPlay.appendChild(playPauseButton);
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
 
 // Función para convertir una cadena base64 en un blob
 function base64toBlob(base64, contentType) {
