e6c5e070b97c2c4293f6692262602318b5720490
Fredy Cruz <cruzfreddy_5@hotmail.com>
2023-05-23 19:07:23 -0500

    Update index.vue

--- servicios/backoffice/v1starter/src/pages/apps/user/list/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/user/list/index.vue
@@ -7,14 +7,6 @@ import Moment from 'moment';
 import { extendMoment } from 'moment-range';
 import esLocale from "moment/locale/es";
 
-import avatar1 from "@/assets/images/avatars/avatar-1.png";
-import avatar2 from "@/assets/images/avatars/avatar-2.png";
-import avatar3 from "@/assets/images/avatars/avatar-3.png";
-import avatar4 from "@/assets/images/avatars/avatar-4.png";
-import avatar5 from "@/assets/images/avatars/avatar-5.png";
-import avatar6 from "@/assets/images/avatars/avatar-6.png";
-import avatar7 from "@/assets/images/avatars/avatar-7.png";
-import avatar8 from "@/assets/images/avatars/avatar-8.png";
 
 const moment = extendMoment(Moment);
 moment.locale('es', [esLocale]);
@@ -649,7 +641,7 @@ const updateSortDesc = (sortDesc) => {
               <VBtn color="secondary" @click="reset"> Reiniciar </VBtn>
               <!-- 👉 Export button -->
               <VBtn
-                :disabled="usersFull.length < totalUsers-1"
+                
                 variant="tonal"
                 color="success"
                 prepend-icon="tabler-screen-share"
