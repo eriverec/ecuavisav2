12717ca54159df1fcfb5ba54e3384adca2e9e8e2
Eri Vera <eriveraec@gmail.com>
2023-05-29 09:05:42 -0500

    Update liveindicador.js

--- rd-live-indicador/liveindicador.js
+++ rd-live-indicador/liveindicador.js
@@ -59,8 +59,8 @@ if (day > 0 && day <= 5) {
 
   if ("05:55" <= timeOfDay && timeOfDay <= "09:00") {
     actDIV();      
-  // } else if ("10:30" <= timeOfDay && timeOfDay <= "16:30") {
-  //   actDIV();
+  } else if ("10:30" <= timeOfDay && timeOfDay <= "16:30") {
+    actDIV();
   } else if ("19:00" <= timeOfDay && timeOfDay <= "23:00") {
     actDIV();
   }  else {}
