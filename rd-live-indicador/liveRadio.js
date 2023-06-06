017aaa1641fec33f2f0c1c5054c45c75f802a398
Eri Vera <eriveraec@gmail.com>
2023-06-06 13:17:53 -0500

    live radio

--- rd-live-indicador/liveRadio.js
+++ rd-live-indicador/liveRadio.js
@@ -1,72 +1,83 @@
-const eventLun_Vie = [
-  { inicio: "05:55", fin: "06:55", titulo: "Televistazo en la comunidad" },
-  { inicio: "06:55", fin: "07:30", titulo: "Contacto Directo" },
-  { inicio: "07:30:00", fin: "09:00:00", titulo: "Televistazo en la comunidad" },
-  { inicio: "13:00:00", fin: "14:00:00", titulo: "Televistazo 13h00" },
-  { inicio: "19:00:00", fin: "20:00:00", titulo: "Televistazo 19h00" }
-];
+//obtenermos el iduser del localsotage
+const userIdXRadio = localStorage.getItem('wylexUserId') || 0;
+if (userIdXRadio == 0) {
+  console.warn("LiveRadio - no estas logueado");
+} else {
+  console.info("LiveRadio - Si estas logueado");
 
-const eventSab = [
-  { inicio: "19:00:00", fin: "19:30:00", titulo: "Televistazo 19h00" }
-];
+  const eventLun_Vie = [
+    { inicio: "05:55", fin: "06:55", titulo: "Televistazo en la comunidad" },
+    { inicio: "06:55", fin: "07:30", titulo: "Contacto Directo" },
+    { inicio: "07:30:00", fin: "09:00:00", titulo: "Televistazo en la comunidad" },
+    // { inicio: "10:30:00", fin: "13:00:00", titulo: "En Contacto" },
+    { inicio: "13:00:00", fin: "14:00:00", titulo: "Televistazo 13h00" },
+    { inicio: "19:00:00", fin: "20:00:00", titulo: "Televistazo 19h00" }
+  ];
 
-const eventDom = [
-  { inicio: "10:30:00", fin: "11:30:00", titulo: "Políticamente Correcto" },
-  { inicio: "19:00:00", fin: "20:00:00", titulo: "Televistazo 19h00" }
+  const eventSab = [
+    { inicio: "19:00:00", fin: "19:30:00", titulo: "Televistazo 19h00" }
+  ];
 
-];
+  const eventDom = [
+    { inicio: "10:30:00", fin: "11:30:00", titulo: "Políticamente Correcto" },
+    { inicio: "19:00:00", fin: "20:00:00", titulo: "Televistazo 19h00" }
 
-function mostrarProgramacion() {
-  const ahora = new Date();
-  const dia = ahora.getDay();
-  const hora = ahora.getHours().toString().padStart(2, "0") + ':' + ahora.getMinutes().toString().padStart(2, "0") + ':' + ahora.getSeconds().toString().padStart(2, "0");
-  // console.log(dia,hora)
+  ];
 
-  let programacion = [];
+  function mostrarProgramacion() {
+    const ahora = new Date();
+    const dia = ahora.getDay();
+    const hora = ahora.getHours().toString().padStart(2, "0") + ':' + ahora.getMinutes().toString().padStart(2, "0") + ':' + ahora.getSeconds().toString().padStart(2, "0");
+    // console.log(dia,hora)
 
-  // Elegir programación según día de la semana
-  switch (dia) {
-    case 0:
-      programacion = eventDom;
-      break;
-    case 6:
-      programacion = eventSab;
-      break;
-    default:
-      programacion = eventLun_Vie;
-      break;
-  }
+    let programacion = [];
 
-  let enProg = false;
-
-  // Verificar si estamos dentro del horario de programación
-  for (let i = 0; i < programacion.length; i++) {
-    const programa = programacion[i];
-    if (hora >= programa.inicio && hora <= programa.fin) {
-      enProg = true;
-      tituloPrograma = programa.titulo;
-      break;
+    // Elegir programación según día de la semana
+    switch (dia) {
+      case 0:
+        programacion = eventDom;
+        break;
+      case 6:
+        programacion = eventSab;
+        break;
+      default:
+        programacion = eventLun_Vie;
+        break;
     }
-  }
 
-  // Mostrar u ocultar elementos según corresponda
-  const selectDivRad = document.getElementById("parent_radio");
+    let enProg = false;
 
-  // let forzadoEvent = localStorage.getItem("playerForzado");
-  // console.log("forzadoEvent:", forzadoEvent)
+    // Verificar si estamos dentro del horario de programación
+    for (let i = 0; i < programacion.length; i++) {
+      const programa = programacion[i];
+      if (hora >= programa.inicio && hora <= programa.fin) {
+        enProg = true;
+        tituloPrograma = programa.titulo;
+        break;
+      }
+    }
 
-  // if (forzadoEvent != "true") {
+    // Mostrar u ocultar elementos según corresponda
+    const selectDivRad = document.getElementById("parent_radio");
+
+    // let forzadoEvent = localStorage.getItem("playerForzado");
+    // console.log("forzadoEvent:", forzadoEvent)
+
+    // if (forzadoEvent != "true") {
     if (enProg) {
-      selectDivRad.style.display= "block";
+      //aqui va el display block
+      selectDivRad.style.display = "none";
     } else {
       //aqui va el display none
-      selectDivRad.style.display= "block";
+      selectDivRad.style.display = "none";
     }
-  // } else { }
+    // } else { }
 
-  // Llamar a la función de nuevo cada medio minuto
-  setTimeout(mostrarProgramacion, 2000);
+    // Llamar a la función de nuevo cada medio minuto
+    setTimeout(mostrarProgramacion, 2000);
+  }
+
+  // Iniciar el ciclo de verificación
+  mostrarProgramacion();
+
 }
-
-// Iniciar el ciclo de verificación
-mostrarProgramacion();
