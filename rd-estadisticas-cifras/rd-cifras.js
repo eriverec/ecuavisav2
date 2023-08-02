aeec57f6411ef258b57cf2d8f0780d4f7b504c3e
Eri Vera <eriveraec@gmail.com>
2023-08-02 16:53:01 -0500

    contenido dinamico

--- rd-estadisticas-cifras/rd-cifras.js
+++ rd-estadisticas-cifras/rd-cifras.js
@@ -28,3 +28,226 @@ function Tabs() {
 }
 
 var connectTabs = new Tabs();
+
+
+function eventLigaPro() {
+  const urlsCifras = [
+    {
+      mainUrl: "/deportes/tabla-de-posiciones",
+      subUrl: {
+        name: "Posiciones",
+        agenda:
+          "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/ecuador/pages/es/agenda.html",
+        iframe:
+          "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.ecuador&lang=es_LA&page=posiciones",
+      },
+    },
+    {
+      mainUrl: "/deportes/liga-pro-serie-a-ecuador/calendario",
+      subUrl: {
+        name: "Calendario",
+        agenda:
+          "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/ecuador/pages/es/agenda.html",
+        iframe:
+          "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.ecuador&lang=es_LA&page=fixture",
+      },
+    },
+    {
+      mainUrl: "/deportes/liga-pro-serie-a-ecuador/goleadores",
+      subUrl: {
+        name: "Goleadores",
+        agenda: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/ecuador/pages/es/agenda.html",
+        iframe: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.ecuador&lang=es_LA&page=goleadores",
+      },
+    },
+    {
+      mainUrl: "/deportes/liga-pro-serie-a-ecuador/planteles",
+      subUrl: {
+        name: "Planteles",
+        agenda: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/ecuador/pages/es/agenda.html",
+        iframe: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.ecuador&lang=es_LA&page=planteles",
+      },
+    },
+  ];
+
+  const currentUrl = window.location.pathname;
+
+  // Variable para almacenar el contenido HTML
+  let htmlContent = "";
+
+  const uLigaProTabla = "/deportes/tabla-de-posiciones";
+  const uLigaProCalendario = "/deportes/liga-pro-serie-a-ecuador/calendario";
+  const uLigaProGoleadores = "/deportes/liga-pro-serie-a-ecuador/goleadores";
+  const uLigaProPlanteles = "/deportes/liga-pro-serie-a-ecuador/planteles";
+
+  // Recorrer el objeto urlsCifras
+  for (const urlObj of urlsCifras) {
+    if (currentUrl === urlObj.mainUrl) {
+      // Generar el contenido HTML usando un template string
+      htmlContent = /*html*/ `        
+          <ul class="nav nav-pills navCentral" id="myTab" role="tablist">
+              <li class="nav-item" role="presentation">
+                  <a class="nav-link active" id="ligapro-tab" href="/deportes/tabla-de-posiciones">LigaPro</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                  <a class="nav-link " id="copali-tab" href="/deportes/copa-libertadores">Copa Libertadores</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                  <a class="nav-link " id="copaSudamericana-tab" href="/deportes/copa-sudamericana" >Copa Sudamericana</a>
+              </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+              <div class="" id="ligapro" role="tabpanel" aria-labelledby="ligapro-tab">
+                  <iframe title="ifr_Posiciones" src="${urlObj.subUrl.agenda}" width="100%" height="180" scrolling="auto" style="width: 1px; min-width: 100%; *width: 100%;" class=""></iframe>                   
+                  <ul class="nav nav-pills navChilds" id="myTab" role="tablist">
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/tabla-de-posiciones" class="nav-link ${currentUrl === uLigaProTabla ? "active" : ""}" id="Posiciones-tab" >Posiciones</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/liga-pro-serie-a-ecuador/calendario" class="nav-link ${currentUrl === uLigaProCalendario ? "active" : ""} " id="Fixture-tab">Calendario</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/liga-pro-serie-a-ecuador/goleadores" class="nav-link ${currentUrl === uLigaProGoleadores ? "active" : ""} " id="Goleadores-tab" >Goleadores</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/liga-pro-serie-a-ecuador/planteles" class="nav-link ${currentUrl === uLigaProPlanteles ? "active" : ""} " id="Planteles-tab" >Planteles</a>
+                      </li>
+                  </ul>
+                  <div class="tab-content" id="myTabContent">
+                      <div class="" id="Posiciones" role="tabpanel" aria-labelledby="Posiciones-tab">
+                          <ul class="nav nav-pills navChilds s_acumulada" id="myTab" role="tablist">
+                              <li class="nav-item" role="presentation">
+                                  <a href="/deportes/tabla-de-posiciones" class="nav-link active" id="fase2-tab" >Fase 2</a>
+                              </li>
+                              <li class="nav-item" role="presentation">
+                                  <a href="/deportes/liga-pro-serie-a-ecuador/tabla-acumulada" class="nav-link " id="acumulada-tab">Acumulada</a>
+                              </li>
+                          </ul>
+                          <iframe title="ifr_Posiciones"
+                              src="${urlObj.subUrl.iframe}"
+                              width="100%" height="700" scrolling="auto" style="width: 1px; min-width: 100%; *width: 100%;"
+                              class=""></iframe>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        `;
+      break; // Detener el bucle una vez que se encuentra una coincidencia
+    }
+  }
+  // Agregar el contenido HTML al cuerpo del documento
+  
+  const skk = document.querySelector(".contenido_dinamico__ligapro");
+  if(skk){
+    skk.innerHTML = htmlContent;
+  }
+}
+
+function eventLibertadores() {
+  const urlsCifras = [
+    {
+      mainUrl: "/deportes/copa-libertadores",
+      subUrl: {
+        name: "Posiciones",
+        agenda: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/libertadores/pages/es/agenda.html",
+        iframe: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.libertadores&lang=es_LA&page=posiciones",
+      },
+    },
+    {
+      mainUrl: "/deportes/copa-libertadores/calendario",
+      subUrl: {
+        name: "Calendario",
+        agenda: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/libertadores/pages/es/agenda.html",
+        iframe: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.libertadores&lang=es_LA&page=fixture",
+      },
+    },
+    {
+      mainUrl: "/deportes/copa-libertadores/goleadores",
+      subUrl: {
+        name: "Goleadores",
+        agenda: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/libertadores/pages/es/agenda.html",
+        iframe: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.libertadores&lang=es_LA&page=goleadores",
+      },
+    },
+    {
+      mainUrl: "/deportes/copa-libertadores/planteles",
+      subUrl: {
+        name: "Planteles",
+        agenda: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/htmlCenter/data/deportes/futbol/libertadores/pages/es/agenda.html",
+        iframe: "https://estadisticas.ecuavisa.com/sites/services/datafactory/html/v3/page.html?channel=deportes.futbol.libertadores&lang=es_LA&page=planteles",
+      },
+    },
+  ];
+
+  const currentUrl = window.location.pathname;
+
+  // Variable para almacenar el contenido HTML
+  let htmlContent = "";
+
+  const uLibertadoresTabla = "/deportes/copa-libertadores";
+  const uLibertadoresCalendario = "/deportes/copa-libertadores/calendario";
+  const uLibertadoresGoleadores = "/deportes/copa-libertadores/goleadores";
+  const uLibertadoresPlanteles = "/deportes/copa-libertadores/planteles";
+
+  // Recorrer el objeto urlsCifras
+  for (const urlObj of urlsCifras) {
+    if (currentUrl === urlObj.mainUrl) {
+      // Generar el contenido HTML usando un template string
+      htmlContent = /*html*/ `        
+          <ul class="nav nav-pills navCentral" id="myTab" role="tablist">
+              <li class="nav-item" role="presentation">
+                  <a class="nav-link " id="ligapro-tab" href="/deportes/tabla-de-posiciones">LigaPro</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                  <a class="nav-link active" id="copali-tab" href="/deportes/copa-libertadores">Copa Libertadores</a>
+              </li>
+              <li class="nav-item" role="presentation">
+                  <a class="nav-link " id="copaSudamericana-tab" href="/deportes/copa-sudamericana" >Copa Sudamericana</a>
+              </li>
+          </ul>
+          <div class="tab-content" id="myTabContent">
+              <div class="" id="ligapro" role="tabpanel" aria-labelledby="ligapro-tab">
+                  <iframe title="ifr_Posiciones" src="${urlObj.subUrl.agenda}" width="100%" height="180" scrolling="auto" style="width: 1px; min-width: 100%; *width: 100%;" class=""></iframe>                   
+                  <ul class="nav nav-pills navChilds" id="myTab" role="tablist">
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/tabla-de-posiciones" class="nav-link ${currentUrl === uLibertadoresTabla ? "active" : ""}" id="Posiciones-tab" >Posiciones</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/copa-libertadores/calendario" class="nav-link ${currentUrl === uLibertadoresCalendario ? "active" : ""} " id="Fixture-tab">Calendario</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/copa-libertadores/goleadores" class="nav-link ${currentUrl === uLibertadoresGoleadores ? "active" : ""} " id="Goleadores-tab" >Goleadores</a>
+                      </li>
+                      <li class="nav-item" role="presentation">
+                          <a href="/deportes/copa-libertadores/planteles" class="nav-link ${currentUrl === uLibertadoresPlanteles ? "active" : ""} " id="Planteles-tab" >Planteles</a>
+                      </li>
+                  </ul>
+                  <div class="tab-content" id="myTabContent">
+                      <div class="" id="Posiciones" role="tabpanel" aria-labelledby="Posiciones-tab">
+                          <iframe title="ifr_Posiciones"
+                              src="${urlObj.subUrl.iframe}"
+                              width="100%" height="700" scrolling="auto" style="width: 1px; min-width: 100%; *width: 100%;"
+                              class=""></iframe>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        `;
+      break; // Detener el bucle una vez que se encuentra una coincidencia
+    }
+  }
+  // Agregar el contenido HTML al cuerpo del documento
+  const skk = document.querySelector(".contenido_dinamico__libertadores");
+  if(skk){
+    skk.innerHTML = htmlContent;
+  }
+}
+
+if (window.location.pathname === "/deportes/tabla-de-posiciones" || window.location.pathname === "/deportes/liga-pro-serie-a-ecuador/calendario" || window.location.pathname === "/deportes/liga-pro-serie-a-ecuador/goleadores" || window.location.pathname === "/deportes/liga-pro-serie-a-ecuador/planteles") {
+  eventLigaPro();
+} else if (window.location.pathname === "/deportes/copa-libertadores" || window.location.pathname === "/deportes/copa-libertadores/calendario" || window.location.pathname === "/deportes/copa-libertadores/goleadores" || window.location.pathname === "/deportes/copa-libertadores/planteles") {
+  eventLibertadores();
+} else if (window.location.pathname === "/deportes/copa-sudamericana") {
+} else {
+}
+
