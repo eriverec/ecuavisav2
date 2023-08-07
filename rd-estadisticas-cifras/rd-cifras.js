c75153ea8e0e54173989f44866f5ca17bbea2d57
Eri Vera <eriveraec@gmail.com>
2023-08-07 14:01:36 -0500

    tabs scroll automatico class active

--- rd-estadisticas-cifras/rd-cifras.js
+++ rd-estadisticas-cifras/rd-cifras.js
@@ -1,33 +1,45 @@
-'use strict';
+// 'use strict';
 
-function Tabs() {
-  var bindAll = function() {
-    var menuElements = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < menuElements.length ; i++) {
-      menuElements[i].addEventListener('click', change, false);
+// function Tabs() {
+//   var bindAll = function() {
+//     var menuElements = document.querySelectorAll('[data-tab]');
+//     for(var i = 0; i < menuElements.length ; i++) {
+//       menuElements[i].addEventListener('click', change, false);
+//     }
+//   }
+
+//   var clear = function() {
+//     var menuElements = document.querySelectorAll('[data-tab]');
+//     for(var i = 0; i < menuElements.length ; i++) {
+//       menuElements[i].classList.remove('active');
+//       var id = menuElements[i].getAttribute('data-tab');
+//       document.getElementById(id).classList.remove('active');
+//     }
+//   }
+
+//   var change = function(e) {
+//     clear();
+//     e.target.classList.add('active');
+//     var id = e.currentTarget.getAttribute('data-tab');
+//     document.getElementById(id).classList.add('active');
+//   }
+
+//   bindAll();
+// }
+
+// var connectTabs = new Tabs();
+
+
+//FUNCION PARA HACER SCROLL HORIZONTAL AUTOMATICO DE ACUERDO A LA CLASE ACTIVE
+var activeNavItem = document.querySelector(".nav-link.active");
+if (activeNavItem) {
+  var contTabs = document.querySelectorAll("#myTab");
+  if(contTabs){
+    for (const sst of contTabs){
+      sst.scrollLeft = activeNavItem.offsetLeft - (sst.offsetWidth - activeNavItem.offsetWidth) / 2;
     }
   }
-
-  var clear = function() {
-    var menuElements = document.querySelectorAll('[data-tab]');
-    for(var i = 0; i < menuElements.length ; i++) {
-      menuElements[i].classList.remove('active');
-      var id = menuElements[i].getAttribute('data-tab');
-      document.getElementById(id).classList.remove('active');
-    }
-  }
-
-  var change = function(e) {
-    clear();
-    e.target.classList.add('active');
-    var id = e.currentTarget.getAttribute('data-tab');
-    document.getElementById(id).classList.add('active');
-  }
-
-  bindAll();
 }
-
-var connectTabs = new Tabs();
 
 
 function eventLigaPro() {
