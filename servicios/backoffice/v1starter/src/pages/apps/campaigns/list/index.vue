86d7bf41ede0d0e36baed863245c9b9eb87b86e5
redy <granredy@gmail.com>
2023-08-09 18:29:36 -0500

    Consumo de api, todas las campañas
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- servicios/backoffice/v1starter/src/pages/apps/campaigns/list/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/campaigns/list/index.vue
@@ -1,8 +1,27 @@
 <script setup>
-import axios from '@axios'
 
-const currentTab = ref('tab-lista')
-const checkbox = ref(1)
+const currentTab = ref('tab-lista');
+const checkbox = ref(1);
+const dataCampaigns = ref([]);
+
+onMounted(getCampaigns)
+
+async function getCampaigns(){
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+
+  var response = await fetch(`https://ads-service.vercel.app/campaign/get/all`, requestOptions);
+  const data = await response.json();
+
+  dataCampaigns.value = data;
+}
+
 </script>
 
 <template>
@@ -41,7 +60,7 @@ const checkbox = ref(1)
                   style="justify-content: space-between;"
                 >
                   <div>
-                    <VCardTitle v-if="usuarioFielModel">
+                    <VCardTitle>
                       Listado de campañas existentes
                     </VCardTitle>
                     <VCardSubtitle> Elige la campaña sobre la que necesites información  </VCardSubtitle>
@@ -53,61 +72,32 @@ const checkbox = ref(1)
                   
                 <VList lines="two">
                   <template
-                    v-for="(element, index) in datos"
+                    v-for="(element, index) in dataCampaigns"
                     :key="index"
                   >
                     <VListItem
-                      v-if="element.nameModule"
+                      v-if="element.campaignTitle"
                       border
                     >
-                      <!-- solo traemos módulos -->
-                      <!--
-                        <template #prepend>
-                        <VIcon :icon="user.prependIcon" class="me-3" />
-                        </template> 
-                      -->
 
                       <VListItemTitle>
-                        <span>{{ element.urlactual }}</span>
-                        <span v-if="element.nameModule">Módulo de: {{ element.nameModule }}</span>
+                        <span>{{ element.position }}</span>
                       </VListItemTitle>
                       <VListItemSubtitle
                         class="mt-1"
                         color="info"
                       >
-                        <!-- <span class="text-xs text-disabled">Item N° {{ index + 1 }}: {{ element.description }}</span> -->
                       </VListItemSubtitle>
-                      <template #append>
-                        <VSwitch
-                          v-model="element.configuracionModuloSugerencias"
-                          :label="element.configuracionModuloSugerencias ? 'Activo' : 'Inactivo'"
-                        />
-                      </template>
+                      
                     </VListItem>
                   </template>
                 </VList>
-                <VBtn
-                  class="mt-5"
-                  color="success"
-                  @click="clicka"
-                >
-                  Aplicar Cambios en Ecuavisa.com<VIcon
-                    end
-                    icon="tabler-cloud-upload"
-                  />
-                </VBtn>
                 <!-- fin lista usuarios -->
               </VWindowItem>
 
-              <VWindowItem value="tab-agregar">
-                <!-- <p>Próximamente</p> -->
-              </VWindowItem>
+              
 
-              <!--
-                <VWindowItem v-for="item in 3" :key="item" >
-                {{ tabItemContent }}
-                </VWindowItem> 
-              -->
+              
             </VWindow>
           </VCardText>
         </VCard>
@@ -116,130 +106,7 @@ const checkbox = ref(1)
   </section>
 </template>
 
-<script>
-import Moment from 'moment'
-import { extendMoment } from 'moment-range'
-import esLocale from "moment/locale/es"
 
-const moment = extendMoment(Moment)
-
-moment.locale('es', [esLocale])
-
-export default {
-  data() {
-    return {
-      usuarioFielModel:500,
-      listUsuarioFieles:[
-        { title:"20", value:20 },
-        { title:"30", value:30 },
-        { title:"50", value:50 },
-        { title:"100", value:100 },
-        { title:"300", value:300 },
-        { title:"400", value:400 },
-        { title:"500", value:500 },
-        { title:"700", value:700 },
-      ],
-      datos: [],
-      suggestion: {
-        title: "",
-        description: "",
-        estado: true,
-      },
-      estado: "",
-      maxRegistros:500,
-      currentTab: 'tab-lista',
-    }
-  },
-  watch: {
-    // async usuarioFielModel(newVal, oldVal) {
-    //   this.maxRegistros = newVal;
-    // this.obtenerDatos();
-    // }
-    
-  },
-  async mounted() {
-    this.authorizedCheck()
-    this.obtenerDatos()
-    await this.accionBackoffice()
-  },
-  methods: {
-    async clicka(){
-      console.log(JSON.stringify(this.datos))
-      var myHeaders = new Headers()
-      myHeaders.append("Content-Type", "application/json")
-      var raw = JSON.stringify(this.datos)
-      var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow',
-      }
-      await fetch('https://estadisticas.ecuavisa.com/sites/services/global/index.php',requestOptions)
-        .then(response =>{			
-        }).catch(error => console.log('error', error))
-      await this.obtenerDatos()
-
-      //console.log('presionaste el click');
-      //console.log(JSON.stringify(this.datos));
-
-    },
-    async obtenerDatos() {
-      const respuesta = await fetch(`https://estadisticas.ecuavisa.com/sites/services/global/datareader.php`)
-      const datos = await respuesta.json()
-
-      // const registros = datos; //datos.data;
-      const sumatoriaPorUsuario = {}
-
-      this.datos = datos // eljson de datos lo hacemos el nuevo valor del app this.datos
-    },
-
-    async handleSubmit() {
-      try {
-        const response = await axios.post(
-          "https://sugerencias-ecuavisa.vercel.app/add",
-          this.suggestion,
-        )
-
-        window.location.reload()
-
-        // this.$router.push("/apps/sugerencias/list");
-      } catch (error) {
-        console.error(error)
-      }
-    },
-
-    //journal de usuarios del backoffice
-    async accionBackoffice (){
-      let dateNow = moment().format("DD/MM/YYYY HH:mm:ss").toString()
-      let userData = JSON.parse(localStorage.getItem('userData'))
-      if(userData.email !== 'admin@demo.com' ){
-        var myHeaders = new Headers()
-        myHeaders.append("Content-Type", "application/json")
-        var log = JSON.stringify({
-          "usuario": userData.email,   
-          "pagina": "ecuavisa.com-modulos",
-          "fecha": dateNow,
-        })
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: log,
-          redirect: 'follow',
-        }
-        await fetch(`https://servicio-logs.vercel.app/accion`, requestOptions)
-          .then(response =>{			
-          }).catch(error => console.log('error', error))
-      }
-    },
-    authorizedCheck (){
-      let rol = localStorage.getItem('role')
-      if(rol !== 'administrador' && rol !== 'webmaster'){
-        this.$router.push({ path: '/pages/errors/not-authorized' })
-      }
-    },
-  },
-}
-</script>
 
 <style scoped>  
 .v-card.v-theme--dark .iframe-dark {
