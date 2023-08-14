e91db53ec5733dee7b916f807a02fa0b199adaaf
Gaby <gholguinm@gmail.com>
2023-08-13 22:15:33 -0500

    revers

--- devCloudflare/envivo2023/envivoPlayerV2.js
+++ devCloudflare/envivo2023/envivoPlayerV2.js
@@ -23,11 +23,8 @@ const programacionSabado = [
 ];
 
 const programacionDomingo = [
-  { inicio: "10:30:00", fin: "11:30:00", titulo: "Políticamente Correcto" },
-  { inicio: "19:00:00", fin: "22:00:00", titulo: "Debate Presidencial" },
-  
-  // { inicio: "19:00:00", fin: "20:00:00", titulo: "Televistazo 19h00" },
-  // { inicio: "20:00:00", fin: "21:00:00", titulo: "Políticamente Correcto" },
+  { inicio: "19:00:00", fin: "20:00:00", titulo: "Televistazo 19h00" },
+  { inicio: "20:00:00", fin: "20:50:00", titulo: "Políticamente Correcto" },
 ];
 
 
