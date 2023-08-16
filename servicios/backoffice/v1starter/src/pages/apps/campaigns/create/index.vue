184472130111b33d058bc45cb5d3dd2c159380cb
redy <granredy@gmail.com>
2023-08-16 17:22:28 -0500

    Update index.vue

--- servicios/backoffice/v1starter/src/pages/apps/campaigns/create/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/campaigns/create/index.vue
@@ -2,25 +2,74 @@
 import {FormWizard,TabContent} from "vue3-form-wizard";
 import 'vue3-form-wizard/dist/style.css'
 const currentTab = ref('tab-lista');
-const checkbox = ref(1);
+const checkbox = ref(false);
+const loadingWizard = ref(false);
 const dataCampaigns = ref([]);
+// const FormWizard = ref(false);
+// const TabContent = ref(false);
+
+const firstName = ref('')
+const email = ref('')
+const mobile = ref()
+const password = ref()
+const languages = ref([])
+const criterio = ref([])
+const posicion = ref([])
+
+const languageList = [
+  'Local',
+  'html'
+]
+
+const criterioList = [
+  'Geolocalización'
+]
+
+const posicionList = [
+  'RDTop1',
+  'RDTop2',
+  'RDTop3',
+  'RDRDFloating',
+]
+
 
 onMounted(getCampaigns)
 
 async function getCampaigns(){
   var myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
-
   var requestOptions = {
     method: 'GET',
     headers: myHeaders,
     redirect: 'follow'
   };
-
   var response = await fetch(`https://ads-service.vercel.app/campaign/get/all`, requestOptions);
   const data = await response.json();
-
   dataCampaigns.value = data.data;
+
+}
+
+const consentimiento = ref(false);
+
+async function onComplete() {
+  if (checkbox.value) {
+    alert("Formulario completado");
+  } else {
+    alert("Debes aceptar los términos y condiciones para continuar.");
+  }
+}
+
+async function handleValidation(isValid, tabIndex) {
+  console.log('Tab: '+tabIndex+ ' valid: '+isValid)
+}
+
+async function setLoading(value) {
+  loadingWizard.value = value
+}
+
+async function validateAsync() {
+
+  return false
 }
 
 </script>
@@ -32,7 +81,7 @@ async function getCampaigns(){
         class="mt-6"
         cols="12"
         md="12"
-        lg="12"
+        lg="6"
       >
         <VTabs
           v-model="currentTab"
@@ -62,25 +111,133 @@ async function getCampaigns(){
                 >
                   <div>
                     <VCardTitle>
-                      Listado de campañas existentes
+                      Crear campañas
                     </VCardTitle>
-                    <VCardSubtitle> Elige la campaña sobre la que necesites información  </VCardSubtitle>
+                    <VCardSubtitle> 
+                      Elige la campaña sobre la que necesites información  
+                    </VCardSubtitle>
                   </div>
                 </div>
 
                 
                 <!-- inicio lista de Módulos -->
                   
-                <form-wizard @on-complete="onComplete">
-                  <tab-content title="Personal details">
-                    My first tab content
+                <form-wizard 
+                  @on-complete="onComplete" 
+                  @on-loading="setLoading"
+                  color="#7367F0" 
+                  @on-validate="handleValidation"
+                  validate-on-back="true"
+                  nextButtonText="Siguiente"
+                >
+                  <tab-content title="Detalles de la campaña" :before-change="validateAsync">
+                   
+                    <VRow>
+                        <VCol cols="12">
+                          <VRow no-gutters>
+                            <!-- 👉 First Name -->
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <label for="firstName">Nombre de la campaña</label>
+                            </VCol>
+
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <VTextField
+                                id="nombreCampania"
+                                v-model="firstName"
+                                placeholder="Nombre de la campaña"
+                                persistent-placeholder
+                              />
+                            </VCol>
+                          </VRow>
+                        </VCol>
+
+                        <VCol cols="6">
+                          <VRow no-gutters>
+                            <!-- 👉 Email -->
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <label for="email">Tipo de contenido</label>
+                            </VCol>
+
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <VSelect
+                                v-model="languages"
+                                :items="languageList"
+                                chips
+                                clearable
+                                label="Tipo de contenido"
+                              />
+                            </VCol>
+                          </VRow>
+                        </VCol>
+
+                        <VCol cols="6">
+                          <VRow no-gutters>
+                            <!-- 👉 Email -->
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <label for="email">Tipo de criterio</label>
+                            </VCol>
+
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <VSelect
+                                v-model="criterio"
+                                :items="criterioList"
+                                chips
+                                clearable
+                              />
+                            </VCol>
+                          </VRow>
+                        </VCol>
+                        <VCol cols="12">
+                          <VRow no-gutters>
+                            <!-- 👉 Email -->
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <label for="email">Posición</label>
+                            </VCol>
+
+                            <VCol
+                              cols="12"
+                              md="12"
+                            >
+                              <VSelect
+                                v-model="posicion"
+                                :items="posicionList"
+                                chips
+                                clearable
+                              />
+                            </VCol>
+                          </VRow>
+                        </VCol>
+                      </VRow>
                   </tab-content>
-                  <tab-content title="Additional Info" >
+                  <tab-content title="Visualización"  :before-change="validateAsync">
                     My second tab content
                   </tab-content>
-                  <tab-content title="Last step" >
+                  <tab-content title="Last step"  :before-change="validateAsync">
                     Yuhuuu! This seems pretty damn simple
                   </tab-content>
+
+                <div class="loader" v-if="loadingWizard"></div>
                 </form-wizard>
                 <!-- fin lista usuarios -->
               </VWindowItem>
@@ -113,5 +270,47 @@ async function getCampaigns(){
 
 .v-card.v-theme--light .iframe-light {
   display: block;
+}
+/* This is a css loader. It's not related to vue-form-wizard */
+.loader,
+.loader:after {
+  border-radius: 50%;
+  width: 10em;
+  height: 10em;
+}
+.loader {
+  margin: 60px auto;
+  font-size: 10px;
+  position: relative;
+  text-indent: -9999em;
+  border-top: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-right: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-bottom: 1.1em solid rgba(255, 255, 255, 0.2);
+  border-left: 1.1em solid #e74c3c;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation: load8 1.1s infinite linear;
+  animation: load8 1.1s infinite linear;
+}
+@-webkit-keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes load8 {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
 }
 </style>
