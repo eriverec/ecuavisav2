7f09fb3c5827e73e0a4c8200c70a44be2cc89cfe
redy <granredy@gmail.com>
2023-08-16 16:40:01 -0500

    Update bo.js

--- jun2023/botpress/bo.js
+++ jun2023/botpress/bo.js
@@ -2,3 +2,16 @@ const jd = document.querySelector(".txt__");
 if(jd){
   jd.style.color = "red";
 }
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/49542283/Ecuavisa/Registro_Floating', [1, 1],'floating_ad').addService(googletag.pubads());
+    googletag.pubads().setTargeting('EC_Seccion','');
+    googletag.pubads().setTargeting('EC_Subseccion',''); 
+    googletag.pubads().setTargeting('EC_Tipo','');
+    googletag.pubads().setTargeting('EC_Articulo','');
+    googletag.pubads().setTargeting('EC_backoffice','all-Users-gye');
+    googletag.pubads().setTargeting('EC_Demo',dfp_demo);
+    googletag.pubads().collapseEmptyDivs();
+    googletag.pubads().setCentering(true);
+    googletag.enableServices();
+    });
