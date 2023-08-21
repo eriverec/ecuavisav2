acc6178d007cf459e2a779acdc8b14725330b1b3
redy <granredy@gmail.com>
2023-08-21 17:44:05 -0500

    Merge branch 'main' of https://github.com/eriverec/ecuavisav2

--- servicios/backoffice/v1starter/src/pages/apps/campaigns/create/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/campaigns/create/index.vue
@@ -32,6 +32,32 @@ const selectItemsList = ref([{ title:'Otro', value: 'Otro' },{ title:'100', valu
 const minValue = ref(1); // Valor mínimo permitido
 const maxValue = ref(100); // Valor máximo permitido
 
+const selectItemDispositivos = ref([]);
+const selectItemsListDispositivos = ref([
+  { title:'Todos', value: '0', avatar:"mdi-cellphone-link" },
+  { title:'Escritorio', value: 'desktop', avatar:"mdi-laptop-chromebook" },
+  { title:'Móvil', value: 'movil', avatar:"mdi-cellphone-android" },
+  ]);
+
+const selectItemNavegador = ref([]);
+const selectItemsListNavegador = ref([
+  { title:'Todos', value: '0', avatar:"" },
+  { title:'Chrome', value: 'Chrome', avatar:"" },
+  { title:'Safari', value: 'Safari', avatar:"" },
+  { title:'Firefox', value: 'Firefox', avatar:""},
+  { title:'Otro', value: 'Otro', avatar:"" } ,
+  ]);
+
+const selectItemSO = ref([]);
+const selectItemsListSO = ref([
+  { title:'Todos', value: '0', avatar:"", navegador: [] },
+  { title:'Windows', value: 'Windows', avatar:"tabler-brand-windows", navegador: [{ title:'Chrome', value: 'Chrome' },{ title:'Firefox', value: 'Firefox' }] },
+  { title:'Mac OS', value: 'Mac OS', avatar:"tabler-brand-apple", navegador: [{ title:'Safari', value: 'Safari' },{ title:'Chrome', value: 'Chrome' }] },
+  { title:'Android', value: 'Android', avatar:"tabler-brand-android", navegador: [{ title:'Chrome', value: 'Chrome' },{ title:'Firefox', value: 'Firefox' }] },
+  { title:'Linux', value: 'Linux', avatar:"mdi-linux", navegador: [{ title:'Chrome', value: 'Chrome' }] } ,
+  { title:'Otro', value: 'Otro', avatar:"", navegador: [{ title:'Chrome', value: 'Chrome' }] } ,
+  ]);
+
 const numeroRules = [
   (v) => !!v || 'El número es requerido', // Verifica que no esté vacío
   (v) => /^\d+$/.test(v) || 'Ingrese solo números', // Verifica que solo sean números
@@ -49,7 +75,11 @@ const languageList = [{
   value:'script'
 }]
 
-const criterioList = [{ title:'Geolocalización', value:'trazabilidads' }];//, { title:'Metadatos', value:'metadato' }
+const criterioList = [
+  { title:'Geolocalización', value:'trazabilidads' },
+  { title:'Dispositivos', value:'dispositivos' },
+  // { title:'Navegador', value:'navegador' },
+];//, { title:'Metadatos', value:'metadato' }
 
 const posicionList = [
   'floating_ad',
@@ -90,23 +120,30 @@ async function getCountries(){
   dataCountry.value = data;
   loadingPanel.value=false;
 }
-
 async function getUsuarios(){
-  var ciudad = selectedItemCiudad.value;
-  var pais = selectedItem.value;
+  var ciudad = (selectedItemCiudad.value).length > 0 ? selectedItemCiudad.value : -1;
+  var pais = (selectedItem.value).length > 0 ? selectedItem.value : -1;
 
-  if(ciudad != '' && ciudad != null && pais != '' && pais != null){
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-    var response = await fetch(`https://ads-service.vercel.app/campaign/get/user/total/${pais}/${ciudad}`, requestOptions);
-    const data = await response.json();
-    dataUsuarios.value = data;
-  }
+  var so_temp = selectItemSO.value || null;
+  var dispositivo_temp = selectItemDispositivos.value || null;
+  var navegador_temp = selectItemNavegador.value || null;
+
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  // console.log(pais || "-1")
+  // alert(pais.length)
+  var response = await fetch(`https://ads-service.vercel.app/campaign/get/user/total/${pais}/${ciudad}?${ new URLSearchParams({ 
+    so: so_temp, 
+    dispositivo: dispositivo_temp,
+    navegador: navegador_temp
+  }) }`, requestOptions);
+  const data = await response.json();
+  dataUsuarios.value = data;
 }
 
 const consentimiento = ref(false);
@@ -138,16 +175,32 @@ async function onComplete() {
   var urlImagen_1 = linkImageEscritorio.value || "";
   var urlImagen_2 = linkImageMobile.value || "";
   var paises_temp = selectedItem.value;
+
+  var ciudad = (selectedItemCiudad.value).length > 0 ? selectedItemCiudad.value : -1;
+  var pais = (selectedItem.value).length > 0 ? selectedItem.value : -1;
+
+  var so_temp = selectItemSO.value || null;
+  var dispositivo_temp = selectItemDispositivos.value || null;
+  var navegador_temp = selectItemNavegador.value || null;
+
+
   var ciudades_temp = selectedItemCiudad.value;
   var participantes_temp = selectItemParticipantes.value;
   var otroValor_temp = numeroOtroUsuarios.value;
+
+  // var so_temp = selectItemSO.value;
+  // var dispositivo_temp = selectItemDispositivos.value;
+  // var navegador_temp = selectItemNavegador.value;
 
   var jsonEnviar = {
         "campaignTitle": name,
         "type": tipoContenido,
         "criterial": {
-            "country": paises_temp,
-            "city": ciudades_temp.join(',')
+            "country": pais,
+            "city": ciudad.join(',') || -1,
+            "so": so_temp.join(',') || null,
+            "dispositivo": dispositivo_temp.join(',') || null,
+            "navegador": navegador_temp.join(',') || null
         },
         "coleccion": cri.join(','),
         "position": po,
@@ -441,6 +494,26 @@ function generateRandomIntegers(min, max, count) {
   return randomIntegers;
 }
 
+async function generarOtrosValores(){
+  maxValue.value = dataUsuarios.value.total;
+  minValue.value = 1;
+  if(dataUsuarios.value.total < 100){
+    selectItemsList.value = [{ title:'Todos', value: 'Todos' },{ title:'Otro valor', value: 'Otro' }];
+  }else{
+    var numeros = generateRandomIntegers(100, dataUsuarios.value.total, 3);
+    var items = [];
+    items.push({ title:'Otro valor', value: 'Otro' });
+    if(dataUsuarios.value.total > 110){
+      for(var i in numeros){
+        items.push({ title:numeros[i], value: numeros[i] });
+      }
+    }
+    items.push({ title:'Todos', value: 'Todos' });
+    selectItemsList.value = items;
+  }
+
+  return true;
+}
 
 watch(async () => selectedItemCiudad.value,async  (newValue, oldValue) => {
   // console.log('Nuevo valor seleccionado:', newValue);
@@ -449,22 +522,53 @@ watch(async () => selectedItemCiudad.value,async  (newValue, oldValue) => {
     loadingPanel.value=true;
     await getUsuarios();
     loadingPanel.value=false;
-    maxValue.value = dataUsuarios.value.total;
-    minValue.value = 1;
-    if(dataUsuarios.value.total < 100){
-      selectItemsList.value = [{ title:'Todos', value: 'Todos' },{ title:'Otro valor', value: 'Otro' }];
-    }else{
-      var numeros = generateRandomIntegers(100, dataUsuarios.value.total, 3);
-      var items = [];
-      items.push({ title:'Otro valor', value: 'Otro' });
-      if(dataUsuarios.value.total > 110){
-        for(var i in numeros){
-          items.push({ title:numeros[i], value: numeros[i] });
-        }
-      }
-      items.push({ title:'Todos', value: 'Todos' });
-      selectItemsList.value = items;
-    }
+    await generarOtrosValores();
+  }else{
+    dataUsuarios.value = {};
+  }
+
+  // selectItemsList.value = [100, 200, 1000, "Otro"];
+});
+
+watch(async () => selectItemDispositivos.value,async  (newValue, oldValue) => {
+  // console.log('Nuevo valor seleccionado:', newValue);
+  // console.log('Valor anterior:', oldValue);
+  if(selectItemDispositivos.value != null){
+    loadingPanel.value=true;
+    await getUsuarios();
+    loadingPanel.value=false;
+    await generarOtrosValores();
+  }else{
+    dataUsuarios.value = {};
+  }
+
+  // selectItemsList.value = [100, 200, 1000, "Otro"];
+});
+
+watch(async () => selectItemSO.value,async  (newValue, oldValue) => {
+  // console.log('Nuevo valor seleccionado:', newValue);
+  // console.log('Valor anterior:', oldValue);
+  if(selectItemSO.value != null){
+    loadingPanel.value=true;
+    await getUsuarios();
+    loadingPanel.value=false;
+    await generarOtrosValores();
+  }else{
+    dataUsuarios.value = {};
+  }
+
+  // selectItemsList.value = [100, 200, 1000, "Otro"];
+});
+
+
+watch(async () => selectItemNavegador.value,async  (newValue, oldValue) => {
+  // console.log('Nuevo valor seleccionado:', newValue);
+  // console.log('Valor anterior:', oldValue);
+  if(selectItemNavegador.value != null){
+    loadingPanel.value=true;
+    await getUsuarios();
+    loadingPanel.value=false;
+    await generarOtrosValores();
   }else{
     dataUsuarios.value = {};
   }
@@ -593,7 +697,7 @@ watch(async () => selectedItemCiudad.value,async  (newValue, oldValue) => {
                               cols="12"
                               md="12"
                             >
-                              <label for="email">Criterio</label>
+                              <label for="email">Criterio de búsqueda</label>
                             </VCol>
 
                             <VCol
@@ -785,51 +889,160 @@ watch(async () => selectedItemCiudad.value,async  (newValue, oldValue) => {
                                 </VCol>
                               </VRow>
                             </VCol>
-                            <VCol cols="12">
-                              <VRow no-gutters>
-                                <!-- 👉 Email -->
-                                <VCol
-                                  cols="12"
-                                  md="12"
-                                >
-                                  <label for="email">Países</label>
-                                </VCol>
+                            <VCol cols="12" :class="criterio.includes('trazabilidads')?'':'d-none'">
+                              <VRow no-gutters >
+                                <VCol cols="6">
+                                  <VRow no-gutters>
+                                    <!-- 👉 Email -->
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <label for="email">Países</label>
+                                    </VCol>
 
-                                <VCol
-                                  cols="12"
-                                  md="12"
-                                >
-                                  <VSelect
-                                    v-model="selectedItem"
-                                    :items="countryList"
-                                    
-                                    chips
-                                    clearable
-                                  />
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <VSelect
+                                        v-model="selectedItem"
+                                        :items="countryList"
+                                        class="pr-1"
+                                        chips
+                                        clearable
+                                      />
+                                    </VCol>
+                                  </VRow>
+                                </VCol>
+                                <VCol cols="6">
+                                  <VRow no-gutters>
+                                    <!-- 👉 Email -->
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <label for="email">Ciudades</label>
+                                    </VCol>
+
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <VSelect
+                                        :items="cityList"
+                                        v-model="selectedItemCiudad"
+                                        multiple
+                                        chips
+                                        clearable
+                                        class="pl-1"
+                                      />
+                                    </VCol>
+                                  </VRow>
                                 </VCol>
                               </VRow>
                             </VCol>
-                            <VCol cols="12">
-                              <VRow no-gutters>
-                                <!-- 👉 Email -->
-                                <VCol
-                                  cols="12"
-                                  md="12"
-                                >
-                                  <label for="email">Ciudades</label>
-                                </VCol>
+                            <VCol cols="12" :class="criterio.includes('dispositivos')?'':'d-none'">
+                              <VRow no-gutters >
+                                <VCol cols="12">
+                                  <VRow no-gutters>
+                                    <!-- 👉 Email -->
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <label for="email">Dispositivos</label>
+                                    </VCol>
 
-                                <VCol
-                                  cols="12"
-                                  md="12"
-                                >
-                                  <VSelect
-                                    :items="cityList"
-                                    v-model="selectedItemCiudad"
-                                    multiple
-                                    chips
-                                    clearable
-                                  />
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <VSelect
+                                        v-model="selectItemDispositivos"
+                                        :items="selectItemsListDispositivos"
+                                        item-title="title"
+                                        item-value="value"
+                                        class="pr-1"
+                                        
+                                        multiple
+                                        clearable
+                                      >
+                                        <template #selection="{ item }">
+                                          <VChip>
+                                            <VAvatar>
+                                              <VIcon color="" :icon="item.raw.avatar" />
+                                            </VAvatar>
+                                            <span>{{ item.title }}</span>
+                                          </VChip>
+                                        </template>
+                                      </VSelect>
+                                    </VCol>
+                                  </VRow>
+                                </VCol>
+                                <VCol cols="6 pt-2">
+                                  <VRow no-gutters>
+                                    <!-- 👉 Email -->
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <label for="navegador">Sistema operativo</label>
+                                    </VCol>
+
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <VSelect
+                                        v-model="selectItemSO"
+                                        :items="selectItemsListSO"
+                                        item-title="title"
+                                        item-value="value"
+                                        class="pr-1"
+                                        
+                                        multiple
+                                        clearable
+                                      >
+                                        <template #selection="{ item }">
+                                          <VChip>
+                                            <VAvatar>
+                                              <VIcon color="" :icon="item.raw.avatar" />
+                                            </VAvatar>
+                                            <span>{{ item.title }}</span>
+                                          </VChip>
+                                        </template>
+                                      </VSelect>
+                                    </VCol>
+                                  </VRow>
+                                </VCol>
+                                <VCol cols="6 pt-2">
+                                  <VRow no-gutters>
+                                    <!-- 👉 Email -->
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <label for="navegador">Navegador</label>
+                                    </VCol>
+
+                                    <VCol
+                                      cols="12"
+                                      md="12"
+                                    >
+                                      <VSelect
+                                        v-model="selectItemNavegador"
+                                        :items="selectItemsListNavegador"
+                                        item-title="title"
+                                        item-value="value"
+                                        class="pr-1"
+                                        chips
+                                        multiple
+                                        clearable
+                                      />
+                                      
+                                    </VCol>
+                                  </VRow>
                                 </VCol>
                               </VRow>
                             </VCol>
