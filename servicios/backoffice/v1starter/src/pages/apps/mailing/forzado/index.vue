0790c9ba8698939b216f76051aafe1e5bb1c28d0
Eri Vera <eriveraec@gmail.com>
2023-09-12 15:15:06 -0500

    modulo forzado v3

--- servicios/backoffice/v1starter/src/pages/apps/mailing/forzado/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/mailing/forzado/index.vue
@@ -30,9 +30,9 @@
             <div class="px-4">
               <VCardText class="align-center px-0 pb-0 pt-2">
                 <form @submit.prevent="createCampaign">
-                  <VRow>
+                  <VRow class="al-center">
 
-                    <VCol cols="12" sm="8" md="8">
+                    <VCol cols="12" sm="6" md="6">
                       <VCol cols="12" sm="12" md="12">
                         <VTextField prepend-inner-icon="tabler-clipboard-text" v-model="newsletterName"
                           label="Nombre del Newsletter" autocomplete="off" />
@@ -51,30 +51,31 @@
                       </VCol>
 
                       <VCol cols="12" sm="12" md="12">
-                        <VAutocomplete prepend-inner-icon="tabler-template" @change="fetchTemplateBody" v-model="selectedTemplate"
-                          :items="dataTemplateList" label="Lista de plantillas del sendpulse" />
+                        <VAutocomplete prepend-inner-icon="tabler-template"
+                          v-model="selectedTemplate" :items="dataTemplateList"
+                          label="Lista de plantillas del sendpulse" />
                       </VCol>
 
-                      
-                      <div class="bloque_editar_template">
-                        <VCardTitle>Campos para editar la nueva campaña </VCardTitle>
+
+                      <div class="bloque_editar_template" v-if="selectedTemplate">
+                        <VCardTitle>Campos para editar el template </VCardTitle>
                         <VCol cols="12" sm="12" md="12">
-                          <VTextField prepend-inner-icon="tabler-list" v-model="subject" label="Imagen del articulo"
+                          <VTextField prepend-inner-icon="tabler-list" v-model="imgenArticulo" label="Imagen del articulo"
                             persistent-hint autocomplete="off" />
                         </VCol>
-  
+
                         <VCol cols="12" sm="12" md="12">
-                          <VTextField prepend-inner-icon="tabler-list" v-model="subject" label="Url del articulo"
+                          <VTextField prepend-inner-icon="tabler-list" v-model="urlArticulo" label="Url del articulo"
                             persistent-hint autocomplete="off" />
                         </VCol>
-  
+
                         <VCol cols="12" sm="12" md="12">
-                          <VTextField prepend-inner-icon="tabler-list" v-model="subject" label="Titulo del nota"
+                          <VTextField prepend-inner-icon="tabler-list" v-model="tituloArticulo" label="Titulo del articulo"
                             persistent-hint autocomplete="off" />
                         </VCol>
-  
+
                         <VCol cols="12" sm="12" md="12">
-                          <VTextField prepend-inner-icon="tabler-list" v-model="subject" label="Subtitulo del nota"
+                          <VTextarea v-model="subtituloArticulo" label="Subtitulo del articulo"
                             persistent-hint autocomplete="off" />
                         </VCol>
 
@@ -88,17 +89,17 @@
                       </VCol>
                     </VCol>
 
-                    <VCol cols="12" sm="4" md="4">
+                    <VCol cols="12" sm="6" md="6" class="fondo-gris">
                       <VCol cols="12" sm="12" md="12">
-                        <div v-if="selectedTemplatePreview">
+                        <!-- <div v-if="selectedTemplatePreview">
                           <img class="imgPreview" :src="selectedTemplatePreview" alt="Vista Previa del Template" />
-                        </div>
-
+                        </div> -->
                         <div v-if="htmlBody">
                           <p v-html="htmlBody"></p>
                         </div>
-
-        
+                        <div v-else>
+                          <p class="text-center">Debe de seleccionar una plantilla</p>
+                        </div>
                       </VCol>
                     </VCol>
                   </VRow>
@@ -126,12 +127,16 @@ const templates = ref([]);
 const dataBookUserList = ref([]);
 const dataList = ref([]);
 const dataTemplateList = ref([]);
-const htmlBody = ref(atob(''))
+const htmlBody = ref('');
+const imgenArticulo = ref("https://www.ecuavisa.com/binrepository/1200x842/0c121/1200d600/none/11705/CCLO/2023082070755-cuenca-elecciones-antic_1138756_20230911181429.jpg");
+const tituloArticulo = ref("Lorem ipsum dolor sit amet consectetur adipisicing elit. Non animi suscipit nam adipisci.");
+const urlArticulo = ref("https://www.ecuavisa.com");
+const subtituloArticulo = ref("Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur qui adipisci ut, veniam ad nesciunt. Illum dolore voluptatibus eligendi repellat iure. Sint aliquam repellendus vitae quidem similique temporibus explicabo aspernatur.");
 
 // Arreglo con las opciones para el campo VAutocomplete
 // const dataBoletinList = ['Diario', 'Opinión'];
 
-watch(selectedTemplate, async(value)=>{
+watch(selectedTemplate, async (value) => {
   if (value) {
     try {
       const response = await axios.get(`https://api.sendpulse.com/template/${value}`, {
@@ -139,16 +144,124 @@ watch(selectedTemplate, async(value)=>{
           Authorization: `Bearer ${sendPulseAccessToken}`, // Agregamos el encabezado de autorización
         },
       });
-      htmlBody.value = (response.data.body);
-      console.log(htmlBody.value );
-
+      function base64ToBytes(base64) {
+        const binString = atob(base64);
+        return Uint8Array.from(binString, (m) => m.codePointAt(0));
+      }
+      htmlBody.value = new TextDecoder().decode(base64ToBytes(response.data.body));
+      updateImageSrc();
+      updateTitulo();
+      updateUrl();
+      updateSubtitulo();
+      updateFecha();
       // return templateBody; // Devuelve el cuerpo del template
     } catch (error) {
       console.error('Error al obtener el cuerpo del template', error);
     }
   }
   // alert(value)
-})
+});
+
+watch(imgenArticulo, () => {
+  updateImageSrc();
+});
+
+watch(tituloArticulo, () => {
+  updateTitulo();
+});
+
+watch(urlArticulo, () => {
+  updateUrl();
+});
+
+watch(subtituloArticulo, () => {
+  updateSubtitulo();
+});
+
+function updateImageSrc() {
+  // Buscar la clase "back_img" dentro de htmlBody.value
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlBody.value, 'text/html');
+  const imgElement = doc.querySelector('.back_img');
+
+  if (imgElement) {
+    imgElement.src = imgenArticulo.value;
+    htmlBody.value = doc.body.innerHTML;
+  }
+}
+function updateTitulo() {
+  // Buscar la clase "back_titulo" dentro de htmlBody.value
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlBody.value, 'text/html');
+  const tituloElement = doc.querySelector('.back_titulo');
+  if (tituloElement) {
+    tituloElement.textContent = tituloArticulo.value;
+    // Actualizar el contenido de htmlBody.value con el nuevo HTML modificado
+    htmlBody.value = doc.body.innerHTML;
+  }
+}
+
+function updateSubtitulo() {
+  // Buscar la clase "back_titulo" dentro de htmlBody.value
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlBody.value, 'text/html');
+  const subtituloElement = doc.querySelector('.back_body');
+  if (subtituloElement) {
+    subtituloElement.textContent = subtituloArticulo.value;
+    // Actualizar el contenido de htmlBody.value con el nuevo HTML modificado
+    htmlBody.value = doc.body.innerHTML;
+  }
+}
+function updateUrl() {
+  // Buscar la clase "back_titulo_url" dentro de htmlBody.value
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlBody.value, 'text/html');
+  const urlElement = doc.querySelector('.back_titulo_url');
+  const urlVerMasElement = doc.querySelector('.back_url_vermas');
+
+  if (urlElement) {
+    urlElement.href = urlArticulo.value;
+    htmlBody.value = doc.body.innerHTML;
+  }
+  if(urlVerMasElement){
+    urlVerMasElement.href = urlArticulo.value;
+    htmlBody.value = doc.body.innerHTML;
+  }
+}
+
+function obtenerFechaConFormato() {
+  const diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+  const meses = [
+    "enero", "febrero", "marzo", "abril", "mayo", "junio",
+    "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+  ];
+
+  const fechaActual = new Date();
+  const diaSemana = diasSemana[fechaActual.getDay()];
+  const dia = fechaActual.getDate().toString().padStart(2, "0");
+  const mes = meses[fechaActual.getMonth()];
+  const año = fechaActual.getFullYear();
+
+  return `${diaSemana}, ${dia} de ${mes} de ${año}`;
+}
+
+
+function updateFecha() {
+  // Obtener la fecha actual
+  // const fechaActual = new Date().toLocaleDateString();
+  const fechaActual = obtenerFechaConFormato();
+
+  // Buscar la clase "fechaDelDia" dentro de htmlBody.value
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(htmlBody.value, 'text/html');
+  const fechaElement = doc.querySelector('.fechaDelDia');
+  if (fechaElement) {
+    fechaElement.textContent = fechaActual;
+    // Actualizar el contenido de htmlBody.value con el nuevo HTML modificado
+    htmlBody.value = doc.body.innerHTML;
+  }
+}
+
 
 const dataBoletinList = [
   { text: 'Diario', value: '3499' },
@@ -209,7 +322,7 @@ onMounted(async () => {
     userLists.value = userListsResponse.data;
 
     for (var i in userListsResponse.data) {
-      dataBookUserList.value.push({ title: `${userListsResponse.data[i].name} - ${userListsResponse.data[i].id}`, value: userListsResponse.data[i].id })
+      dataBookUserList.value.push({ title: `${userListsResponse.data[i].name}`, value: userListsResponse.data[i].id })
     }
     dataBookUserList.value.sort((a, b) => a.title.localeCompare(b.title));
 
@@ -245,13 +358,18 @@ const createCampaign = () => {
     return;
   }
 
+  function bytesToBase64(bytes) {
+    const binString = Array.from(bytes, (x) => String.fromCodePoint(x)).join("");
+    return btoa(binString);
+  }
+
   const campaignData = {
     sender_name: nameRemitente.value,
     sender_email: "ecuavisainforma@ecuavisa.com",
     name: newsletterName.value,
     subject: subject.value,
     list_id: selectedUserList.value,
-    template_id: selectedTemplate.value,
+    body: bytesToBase64(new TextEncoder().encode(htmlBody.value))
     // Otros datos necesarios para crear la campaña
   };
 
@@ -266,13 +384,12 @@ const createCampaign = () => {
     .then((response) => {
       // Maneja la respuesta de la API después de crear la campaña
       console.log('Campaña creada con éxito', response.data);
-      if (response.data) {
-        configSnackbar.value = {
-          message: "Enviado con exito",
-          type: "success",
-          model: true
-        };
-      }
+      configSnackbar.value = {
+        message: "Enviado con exito",
+        type: "success",
+        model: true
+      };
+      
     })
     .catch((error) => {
       console.error('Error al crear la campaña', error);
