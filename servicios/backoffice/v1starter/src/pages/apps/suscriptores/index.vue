09d1c3e9a04f010bb559b25076ed0e1a7aba1d22
Eri Vera <eriveraec@gmail.com>
2023-09-22 15:36:26 -0500

    tabs icon

--- servicios/backoffice/v1starter/src/pages/apps/suscriptores/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/suscriptores/index.vue
@@ -3,11 +3,26 @@
 		<VRow>
 			<VCol cols="12">
 				<div class="tab-titles parentTabs">
-					<div class="" @click="selectTab(0)" :class="{ active: selectedTab === 0 }">Cobros con Tarjeta de crédito</div>
-					<div @click="selectTab(1)" :class="{ active: selectedTab === 1 }">Cobros por paquete</div>
-					<div @click="selectTab(2)" :class="{ active: selectedTab === 2 }">Alta de suscriptores</div>
-					<div @click="selectTab(3)" :class="{ active: selectedTab === 3 }">Baja de suscriptores por paquete</div>
-					<div @click="selectTab(4)" :class="{ active: selectedTab === 4 }">Baja de suscriptores por motivo</div>
+					<div class="" @click="selectTab(0)" :class="{ active: selectedTab === 0 }">
+						<VIcon :size="35" icon="tabler-credit-card" />
+						<span>Cobros con Tarjeta de crédito</span>
+					</div>
+					<div @click="selectTab(1)" :class="{ active: selectedTab === 1 }">
+						<VIcon :size="35" icon="tabler-box" />
+						<span>Cobros por paquete</span>
+					</div>
+					<div @click="selectTab(2)" :class="{ active: selectedTab === 2 }">
+						<VIcon :size="35" icon="tabler-user-plus" />
+						<span>Alta de suscriptores</span>
+					</div>
+					<div @click="selectTab(3)" :class="{ active: selectedTab === 3 }">
+						<VIcon :size="35" icon="tabler-user-minus" />
+						<span>Baja de suscriptores por paquete</span>
+					</div>
+					<div @click="selectTab(4)" :class="{ active: selectedTab === 4 }">
+						<VIcon :size="35" icon="tabler-user-question" />
+						<span>Baja de suscriptores por motivo</span>
+					</div>
 
 				</div>
 				<VCard>
@@ -46,82 +61,92 @@ import BajaSuscriptores from '@/pages/apps/suscriptores/tabs/baja-suscriptores.v
 import BajaSuscriptoresMotivo from '@/pages/apps/suscriptores/tabs/baja-suscriptores-motivo.vue';
 const selectedTab = ref(0);
 const selectTab = (index) => {
-  selectedTab.value = index;
+	selectedTab.value = index;
 };
 </script>
 
 <style scoped>
 .tab-slider {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  height: 2px; /* Altura del slider */
-  background-color: rgb(var(--v-theme-primary)); /* Color del slider */
-  width: 0; /* Inicialmente el ancho es cero */
-  transition: width 0.3s; /* Agregar transición para suavizar el deslizamiento */
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	height: 2px;
+	/* Altura del slider */
+	background-color: rgb(var(--v-theme-primary));
+	/* Color del slider */
+	width: 0;
+	/* Inicialmente el ancho es cero */
+	transition: width 0.3s;
+	/* Agregar transición para suavizar el deslizamiento */
 }
+
 .parentTabs {
-  overflow: auto;
-  /* white-space: nowrap; */
-  display: flex;
-  align-items: flex-start;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  padding-bottom: 15px;
+	overflow: auto;
+	/* white-space: nowrap; */
+	display: flex;
+	align-items: flex-start;
+	align-items: center;
+	justify-content: space-between;
+	width: 100%;
+	padding-bottom: 15px;
 }
 
 .parentTabs::-webkit-scrollbar {
-  height: 10px;
+	height: 10px;
 }
 
 .parentTabs::-webkit-scrollbar:hover {
-  height: 10px;
-  padding-top: 10px;
+	height: 10px;
+	padding-top: 10px;
 }
 
 .parentTabs::-webkit-scrollbar-track {
-  -webkit-box-shadow: inset 0 0 0px rgba(0, 0, 0, 0);
+	-webkit-box-shadow: inset 0 0 0px rgba(0, 0, 0, 0);
 }
 
 .parentTabs::-webkit-scrollbar-thumb {
-  height: 6px;
-  background-color: rgba(var(--v-theme-on-background), var(--v-medium-emphasis-opacity));
-  border-radius: 6px;
+	height: 6px;
+	background-color: rgba(var(--v-theme-on-background), var(--v-medium-emphasis-opacity));
+	border-radius: 6px;
 }
 
 .parentTabs::-webkit-scrollbar-thumb:hover {
-  background-color: rgb(var(--v-theme-primary));
-  height: 10px;
+	background-color: rgb(var(--v-theme-primary));
+	height: 10px;
 
 }
+
 .tab-titles div.active {
-  background-color: rgb(var(--v-theme-primary));
-  color: rgb(var(--v-theme-on-primary)) !important;
-  border-radius: 0.25rem !important;
+	background-color: rgb(var(--v-theme-primary));
+	color: rgb(var(--v-theme-on-primary)) !important;
+	border-radius: 0.25rem !important;
 }
 
 .tab-titles {
-  display: flex;
+	display: flex;
 }
 
 .tab-titles div {
-  cursor: pointer;
-  /* background-color: lightgray; */
-  margin-right: 8px;
-  height: var(--v-tabs-height);
-  padding: 10px 20px;
-  text-transform: uppercase;
-  font-size: .775rem;
-  --v-btn-size: .875rem;
-  --v-btn-height: 38px;
-  font-weight: 500;
-  letter-spacing: .0892857143em;
-  line-height: normal;
+	cursor: pointer;
+	/* background-color: lightgray; */
+	margin-right: 8px;
+	height: var(--v-tabs-height);
+	padding: 10px 10px;
+	text-transform: uppercase;
+	font-size: .775rem;
+	--v-btn-size: .875rem;
+	--v-btn-height: 38px;
+	font-weight: 500;
+	letter-spacing: .0892857143em;
+	line-height: normal;
+
+	/* */
+	display: flex;
+	align-items: center;
+	gap: 10px;
 }
 
 .tab-content {
-  margin-top: 16px;
+	margin-top: 16px;
 }
-
 </style>
