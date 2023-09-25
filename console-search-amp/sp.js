0180b32fe472276417819d28a1303ccacaa5ad7d
Eri Vera <eriveraec@gmail.com>
2023-09-25 14:38:49 -0500

    depuracion de carpetas y archivos

--- console-search-amp/sp.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('button');
-  btn.addEventListener('click', () => {
-    document.body.textContent = 'Hello World!';
-  });
-
-  var iFrame = document.getElementById("ya").src;
-
-
-  console.log(iFrame);
