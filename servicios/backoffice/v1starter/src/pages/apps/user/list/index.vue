a52a114b175930fcaa2631f9f79edb2b1b477619
Eri Vera <eriveraec@gmail.com>
2023-09-26 13:01:09 -0500

    agregar totales de de web y app

--- servicios/backoffice/v1starter/src/pages/apps/user/list/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/user/list/index.vue
@@ -21,6 +21,11 @@ const totalPage = ref(1);
 const totalUsers = ref(0);
 const totalAppUsers = ref(0);
 
+const totalDevicesUser = ref(0);
+const totalDevicesEmail = ref(0);
+const totalDevicesFacebook = ref(0);
+const totalDevicesGoogle = ref(0);
+
 const totalFacebook = ref(0);
 const totalGoogle = ref(0);
 const totalEmail = ref(0);
@@ -126,6 +131,11 @@ const countUsers = () => {
       totalAppGoogle.value = response.data.totalAppGoogle;
 
       let totalApp = totalAppUsers.value;
+
+      totalDevicesUser.value = (response.data.fullUsers) + (totalApp);
+      totalDevicesEmail.value = (response.data.totalEmail) + (response.data.totalAppEmail);
+      totalDevicesFacebook.value = (response.data.totalFacebook) + (response.data.totalAppFacebook);
+      totalDevicesGoogle.value = (response.data.totalGoogle) + (response.data.totalGoogle);
 
       pE.value = (totalEmail.value * 100) / total;
       percentEmail.value = Math.round((pE.value + Number.EPSILON) * 100) / 100;
@@ -315,6 +325,7 @@ const userListMeta = [
     percentage: null,
     percentageApp:null, 
     subtitle: totalAppUsers,
+    total: totalDevicesUser
   },
   {
     icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 33.39 14" width="3rem" height="3rem"><g id="Capa_2" data-name="Capa 2"><g id="logo"><path d="M14.19,12.16a1.66,1.66,0,0,0-1.66-1.66H1.66a1.66,1.66,0,0,0,0,3.32H12.53A1.66,1.66,0,0,0,14.19,12.16Z" style="fill:#7367f0"/><path d="M32.52.19a1.68,1.68,0,0,0-2.25.68L25.09,9.14,20.23.77A1.72,1.72,0,0,0,18.77.08H7.82a1.66,1.66,0,0,0,0,3.32h9.53a1,1,0,0,1,.78.36l5.39,9.3a1.71,1.71,0,0,0,.71.75,1.66,1.66,0,0,0,2.25-.68L33.19,2.44A1.67,1.67,0,0,0,32.52.19Z" style="fill:#7367f0"/><path d="M19.25,13.83a1.65,1.65,0,0,0,.79-2.2s-2.38-4-3.24-5.74a1.12,1.12,0,0,0-1-.63H4.48a1.66,1.66,0,0,0,0,3.32H14a.65.65,0,0,1,.58.29C15.41,10.55,17,13,17,13.05A1.65,1.65,0,0,0,19.25,13.83Z" style="fill:#7367f0"/></g></g></svg>',
@@ -324,6 +335,8 @@ const userListMeta = [
     percentage: percentEmail,
     percentageApp: percentAppEmail, 
     subtitle: totalAppEmail,
+    total: totalDevicesEmail
+
   },
   {
     icon: '<svg width="2rem" height="2rem" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M18 9C18 4.02944 13.9706 0 9 0C4.02944 0 0 4.02944 0 9C0 13.4922 3.29117 17.2155 7.59375 17.8906V11.6016H5.30859V9H7.59375V7.01719C7.59375 4.76156 8.9374 3.51562 10.9932 3.51562C11.9779 3.51562 13.0078 3.69141 13.0078 3.69141V5.90625H11.8729C10.7549 5.90625 10.4062 6.6 10.4062 7.31175V9H12.9023L12.5033 11.6016H10.4062V17.8906C14.7088 17.2155 18 13.4922 18 9Z" fill="#1877F2"/></svg>',
@@ -333,6 +346,8 @@ const userListMeta = [
     percentage: percentFacebook,
     percentageApp: percentAppFacebook, 
     subtitle: totalAppFacebook,
+    total: totalDevicesFacebook
+
   },
   {
     icon: '<svg aria-hidden="true" class="native svg-icon iconGoogle" width="2rem" height="2rem" viewBox="0 0 18 18"><path d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 0 0 2.38-5.88c0-.57-.05-.66-.15-1.18Z" fill="#4285F4"></path><path d="M8.98 17c2.16 0 3.97-.72 5.3-1.94l-2.6-2a4.8 4.8 0 0 1-7.18-2.54H1.83v2.07A8 8 0 0 0 8.98 17Z" fill="#34A853"></path><path d="M4.5 10.52a4.8 4.8 0 0 1 0-3.04V5.41H1.83a8 8 0 0 0 0 7.18l2.67-2.07Z" fill="#FBBC05"></path><path d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 0 0 1.83 5.4L4.5 7.49a4.77 4.77 0 0 1 4.48-3.3Z" fill="#EA4335"></path></svg>',
@@ -342,6 +357,8 @@ const userListMeta = [
     percentage: percentGoogle,
     percentageApp: percentAppGoogle, 
     subtitle: totalAppGoogle,
+    total: totalDevicesGoogle
+
   },
 ];
 
@@ -808,6 +825,9 @@ const resolveFechaSelected = (fechas) => {
               <div class="d-flex align-center gap-2 my-1">
                 <VChip label color="warning">App</VChip> {{ meta.subtitle }} <span class="text-warning" :hidden="meta.percentageApp ? false : true " >({{ meta.percentageApp }}%) </span>
               </div>
+              <div class="d-flex align-center gap-2 my-1">
+                  <VChip label>Total</VChip> {{ meta.total }}
+                </div>
             </div>
 
             <VAvatar
