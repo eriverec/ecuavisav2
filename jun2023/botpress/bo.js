89bc44d9c44a9962f368be3ae6b6f9758efaf887
Eri Vera <eriveraec@gmail.com>
2023-10-18 17:41:29 -0500

    bojs

--- jun2023/botpress/bo.js
+++ jun2023/botpress/bo.js
@@ -1,44 +1,4 @@
-const jd = document.querySelector(".txt__");
-if(jd){
-  jd.style.color = "red";
-}
 function newAdSpace(){
   console.log("itsOk")
 }
 newAdSpace();
-// var scriptNewAdSpace = document.createElement("script");
-// scriptNewAdSpace.innerHTML = `
-// // agregamos el maping unico del nuevo floating
-// // ad floating de campaña
-//  _html3 = document.createElement('div'); // creamos el elemento
-// // agregamos el maping unico del nuevo floating
-// window.googletag = window.googletag || {cmd: []};
-// googletag.cmd.push(function() {
-//   googletag.defineSlot('/49542283/Ecuavisa/Registro_Floating', [1, 1],'floating_ad').addService(googletag.pubads());
-//   googletag.pubads().setTargeting('EC_Seccion','');
-//   googletag.pubads().setTargeting('EC_Subseccion',''); 
-//   googletag.pubads().setTargeting('EC_Tipo','');
-//   googletag.pubads().setTargeting('EC_Articulo','');
-//   googletag.pubads().setTargeting('EC_backoffice','all-Users-gye');
-//   // googletag.pubads().setTargeting('EC_Demo',dfp_demo);
-//   googletag.pubads().collapseEmptyDivs();
-//   googletag.pubads().setCentering(true);
-//   googletag.enableServices();
-// });
-// // agregamos el contenido y atributos al elemento creado
-// _html3.innerHTML = '<div id="floating_ad" data-ads="floating_ad"> <script> googletag.cmd.push(function() { googletag.display("floating_ad"); }); </script></div>';
-// document.body.appendChild(_html3); // agregamos al body el espacio
-// console.log("ad ejecutado con exito") // si el espaciocorre todo ok
-
-// setTimeout(() => {
-//   // console.log(runAdContainer("floating_ad"));// Corremos Floating
-// }, "1000");
-
-// console.log("ad ejecutado con exito") // si el espaciocorre todo ok
-
-// `;
-// setTimeout(() => {
-//   document.head.appendChild(scriptNewAdSpace); //agregamos mapping al head tag
-//   console.log('se creó el nuevo mapping de este espacio publicitario');
-//   // console.log(runAdContainer("floating_ad"));// Corremos Floating
-// }, "1000");
