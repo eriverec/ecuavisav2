8ba0460ea5205393df1014e82272f6b9f30a62fb
Eri Vera <eriveraec@gmail.com>
2023-10-23 11:50:36 -0500

    refresh services envivo - comentado 23/10/2023

--- devCloudflare/envivoManager/emanager.js
+++ devCloudflare/envivoManager/emanager.js
@@ -302,7 +302,7 @@ function eventoEnvivoManager() {
         console.error("Error al obtener los datos:", error);
       });
 
-    setTimeout(fetchHorarioEnvivo, 120000); //cada 2 minutos se actualiza
+    // setTimeout(fetchHorarioEnvivo, 120000); //comentado 23/octubre/2023 - 11:50AM
   }
 
   // Llamar a la función para obtener y procesar los datos inicialmente
