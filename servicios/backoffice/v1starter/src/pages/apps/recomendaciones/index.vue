a9e9a094f609432989576193a54db64714454da2
redy <granredy@gmail.com>
2023-10-30 16:40:52 -0500

    Recomendada
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- servicios/backoffice/v1starter/src/pages/apps/recomendaciones/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/recomendaciones/index.vue
@@ -1,6 +1,48 @@
 <script setup>
-import ChartRecomendaciones from "@/views/charts/apex-chart/ChartRecomendaciones.vue";
-import ChartRecSubsecccion from "@/views/charts/apex-chart/ChartRecSubsecccion.vue";
+import ChartRecomendaciones from "@/views/charts/ecuavisa/ChartRecomendaciones.vue";
+// import ChartRecSubsecccion from "@/views/charts/apex-chart/ChartRecSubsecccion.vue";
+
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
+import esLocale from "moment/locale/es";
+
+const moment = extendMoment(Moment);
+moment.locale('es', [esLocale]);
+
+const fechaIngresada = ref('');
+const fechaIni = ref('');
+const fechaFin = ref('');
+
+async function obtenerPorFechaMeta(selectedDates) {
+  try {
+    if (selectedDates.length > 1) {
+      // currentPage.value = 1;
+      let fechaI = moment(selectedDates[0]).format('YYYY-MM-DD');
+      let fechaF = moment(selectedDates[1]).format('YYYY-MM-DD');
+      fechaIni.value = fechaI;
+      fechaFin.value = fechaF;
+      // await obtenerFechaDispositivos(fechaI, fechaF);
+      //panelGrafico.classList.remove("disabled");                       
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const initData = () => {
+  let fechai = moment().subtract(7, 'days').format("YYYY-MM-DD").toString();
+  let fechaf = moment().format("YYYY-MM-DD").toString();
+  fechaIni.value = fechai;
+  fechaFin.value = fechaf;
+  fechaIngresada.value = fechai + ' a ' + fechaf;
+  // console.log(fechaIngresada.value)
+}
+
+onMounted(async() => {
+  initData();
+});
+
+
 </script>
 
 <style type="text/css">
@@ -15,29 +57,37 @@ import ChartRecSubsecccion from "@/views/charts/apex-chart/ChartRecSubsecccion.v
 </style>
 <template>
   <VRow>
-    <VCol cols="12">
-      <VCard class="mt-3">
-        <VCardText class=" flex-wrap justify-space-between gap-4">
-          <VCardItem class="pt-0 pb-0">
+    <VCol cols="12" md="6">
+      <VCard>
+        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
             <VCardTitle>Recomendaciones por navegación</VCardTitle>
             <VCardSubtitle>Categoría de dispositivos de los últimos 7 días</VCardSubtitle>
-          </VCardItem>
-          <VDivider />
 
-          <ChartRecomendaciones />
+            <template #append>
+              <div class="date-picker-wrapper" style="width: 250px;">
+                <AppDateTimePicker 
+                  prepend-inner-icon="tabler-calendar" 
+                  density="compact" 
+                  v-model="fechaIngresada" 
+                  @on-change="obtenerPorFechaMeta"
+                  show-current="true" :config="{
+                    position: 'auto right',
+                    mode: 'range',
+                    altFormat: 'F j, Y',
+                    dateFormat: 'Y-m-d',
+                    maxDate: new Date(),
+                    reactive: true
+                  }" />
 
+              </div>
+            </template>
+        </VCardItem>
+
+        <VCardText>
+          <ChartRecomendaciones :fechaIni="fechaIni" :fechaFin="fechaFin" />
         </VCardText>
       </VCard>
     </VCol>
-
-
+    
   </VRow>
 </template>
-<script>
-import Moment from 'moment';
-import { extendMoment } from 'moment-range';
-import esLocale from "moment/locale/es";
-const moment = extendMoment(Moment);
-moment.locale('es', [esLocale]);
-
-</script>
