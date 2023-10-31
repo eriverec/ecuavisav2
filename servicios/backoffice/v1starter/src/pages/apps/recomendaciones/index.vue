5e5262fd895df4f59043534afe05d8ca8bc1fa04
Eri Vera <eriveraec@gmail.com>
2023-10-31 14:29:37 -0500

    fecha por criterio y el nuevo evento clic export

--- servicios/backoffice/v1starter/src/pages/apps/recomendaciones/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/recomendaciones/index.vue
@@ -2,6 +2,8 @@
 import ChartRecomendaciones from "@/views/charts/ecuavisa/ChartRecomendaciones.vue";
 import ChartRecSubsecciones from "@/views/charts/ecuavisa/ChartRecSubsecciones.vue";
 import ChartRecMeta from "@/views/charts/ecuavisa/ChartRecMeta.vue";
+import ChartRecPais from "@/views/charts/ecuavisa/ChartRecPais.vue";
+
 // import ChartRecSubsecccion from "@/views/charts/apex-chart/ChartRecSubsecccion.vue";
 
 import Moment from 'moment';
@@ -12,105 +14,203 @@ const moment = extendMoment(Moment);
 moment.locale('es', [esLocale]);
 
 const fechaIngresada = ref('');
-const fechaIngresadaSubseccion = ref('');
-const fechaIngresadaMetadato = ref('');
+const fechaIngresadaSubseccion = ref('Hoy');
+const fechaIngresadaMetadato = ref('Hoy');
+const fechaIngresadaPais = ref('Hoy');
+const selectedfechaIniFin = ref('Hoy');
 
 const fechaIni = ref('');
-const fechaFin = ref('');
+const fechaFin = ref(moment().add(-1, 'days').format("YYYY-MM-DD"));
 
 const fechaIniSub = ref('');
-const fechaFinSub = ref('');
+const fechaFinSub = ref(moment().add(-1, 'days').format("YYYY-MM-DD"));
 
 const fechaIniMeta = ref('');
-const fechaFinMeta = ref('');
-const modelItemsSeccion = ref({ title:'Todos', value:'0' });
+const fechaFinMeta = ref(moment().add(-1, 'days').format("YYYY-MM-DD"));
 
-async function obtenerPorFechaMeta(selectedDates) {
-  try {
-    if (selectedDates.length > 1) {
-      // currentPage.value = 1;
-      let fechaI = moment(selectedDates[0]).format('YYYY-MM-DD');
-      let fechaF = moment(selectedDates[1]).format('YYYY-MM-DD');
-      fechaIni.value = fechaI;
-      fechaFin.value = fechaF;
-      // await obtenerFechaDispositivos(fechaI, fechaF);
-      //panelGrafico.classList.remove("disabled");                       
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+const fechaIniPais = ref('');
+const fechaFinPais = ref(moment().add(-1, 'days').format("YYYY-MM-DD"));
 
-async function obtenerPorSubsecciones(selectedDates) {
-  try {
-    if (selectedDates.length > 1) {
-      // currentPage.value = 1;
-      let fechaI = moment(selectedDates[0]).format('YYYY-MM-DD');
-      let fechaF = moment(selectedDates[1]).format('YYYY-MM-DD');
-      fechaIniSub.value = fechaI;
-      fechaFinSub.value = fechaF;
-      // await obtenerFechaDispositivos(fechaI, fechaF);
-      //panelGrafico.classList.remove("disabled");                       
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+const modelItemsSeccion = ref({ title: 'Todos', value: '0' });
+const modelItemsSeccionPais = ref({ title: 'Todos', value: '0' });
 
-async function obtenerPorMeta(selectedDates) {
-  try {
-    if (selectedDates.length > 1) {
-      // currentPage.value = 1;
-      let fechaI = moment(selectedDates[0]).format('YYYY-MM-DD');
-      let fechaF = moment(selectedDates[1]).format('YYYY-MM-DD');
-      fechaIniMeta.value = fechaI;
-      fechaFinMeta.value = fechaF;
-      // await obtenerFechaDispositivos(fechaI, fechaF);
-      //panelGrafico.classList.remove("disabled");                       
-    }
-  } catch (error) {
-    console.error(error);
-  }
-}
+const fechaIniFinList = [
+  'Hoy', 'Hace una semana', '15 días atrás', '1 mes atrás'
+];
 
 const initData = () => {
-  let fechai = moment().subtract(7, 'days').format("YYYY-MM-DD").toString();
-  let fechaf = moment().format("YYYY-MM-DD").toString();
+  let fechai = moment().add(-1, 'days').format("YYYY-MM-DD");
+  let fechaf = moment().add(-1, 'days').format("YYYY-MM-DD");
   fechaIni.value = fechai;
   fechaFin.value = fechaf;
   fechaIngresada.value = fechai + ' a ' + fechaf;
-  // console.log(fechaIngresada.value)
 }
 
 const initDataSub = () => {
-  let fechai = moment().subtract(3, 'days').format("YYYY-MM-DD").toString();
-  let fechaf = moment().format("YYYY-MM-DD").toString();
+  let fechai = moment().add(-1, 'days').format("YYYY-MM-DD");
+  let fechaf = moment().add(-1, 'days').format("YYYY-MM-DD");
   fechaIniSub.value = fechai;
   fechaFinSub.value = fechaf;
-  fechaIngresadaSubseccion.value = fechai + ' a ' + fechaf;
+  fechaIngresada.value = fechai + ' a ' + fechaf;
 }
 
 const initDataMeta = () => {
-  let fechai = moment().subtract(3, 'days').format("YYYY-MM-DD").toString();
-  let fechaf = moment().format("YYYY-MM-DD").toString();
+  let fechai = moment().add(-1, 'days').format("YYYY-MM-DD");
+  let fechaf = moment().add(-1, 'days').format("YYYY-MM-DD");
   fechaIniMeta.value = fechai;
   fechaFinMeta.value = fechaf;
-  fechaIngresadaMetadato.value = fechai + ' a ' + fechaf;
+  fechaIngresada.value = fechai + ' a ' + fechaf;
+}
+
+const initDataPais = () => {
+  let fechai = moment().add(-1, 'days').format("YYYY-MM-DD");
+  let fechaf = moment().add(-1, 'days').format("YYYY-MM-DD");
+  fechaIniPais.value = fechai;
+  fechaFinPais.value = fechaf;
+  fechaIngresada.value = fechai + ' a ' + fechaf;
 }
 
 onMounted(async () => {
   initData();
   initDataSub();
   initDataMeta();
+  initDataPais();
 });
 const items = [
-  { title:'Todos', value:'0' },
-  { title:'Noticias', value:'noticias' },
-  { title:'Estadio', value:'estadio' },
-  { title:'Entretenimiento', value:'entretenimiento' },
-  { title:'Mundo', value:'mundo' },
+  { title: 'Todos', value: '0' },
+  { title: 'Noticias', value: 'noticias' },
+  { title: 'Estadio', value: 'estadio' },
+  { title: 'Entretenimiento', value: 'entretenimiento' },
+  { title: 'Mundo', value: 'mundo' },
   // { title:'Estilo', value:'estilo' }
 ]
+
+
+watch(async () => selectedfechaIniFin.value, async () => {
+  let selectedCombo = selectedfechaIniFin.value
+  // console.log("selectedCombo:",selectedCombo);
+  if (selectedCombo === 'Hoy') {
+    fechaIni.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+    fechaFin.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+    // console.log(fechaIni.value +'--a--'+ fechaFin.value )
+  }
+  if (selectedCombo === 'Hace una semana') {
+    fechaIni.value = moment().add(-7, 'days').format("YYYY-MM-DD");
+    fechaFin.value = moment().format("YYYY-MM-DD");
+    // console.log(fechaIni.value +'--a--'+ fechaFin.value )
+  }
+  if (selectedCombo === '15 días atrás') {
+    fechaIni.value = moment().add(-15, 'days').format("YYYY-MM-DD");
+    fechaFin.value = moment().format("YYYY-MM-DD");
+    // console.log(fechaIni.value +'--a--'+ fechaFin.value )
+  }
+  if (selectedCombo === '1 mes atrás') {
+    fechaIni.value = moment().add(-30, 'days').format("YYYY-MM-DD");
+    fechaFin.value = moment().format("YYYY-MM-DD");
+    // console.log(fechaIni.value +'--a--'+ fechaFin.value )
+  }
+});
+
+watch(async () => fechaIngresadaMetadato.value, async () => {
+  let selectedCombo = fechaIngresadaMetadato.value
+  if (selectedCombo === 'Hoy') {
+    fechaIniMeta.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+    fechaFinMeta.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+  }
+  if (selectedCombo === 'Hace una semana') {
+    fechaIniMeta.value = moment().add(-7, 'days').format("YYYY-MM-DD");
+    fechaFinMeta.value = moment().format("YYYY-MM-DD");
+  }
+  if (selectedCombo === '15 días atrás') {
+    fechaIniMeta.value = moment().add(-15, 'days').format("YYYY-MM-DD");
+    fechaFinMeta.value = moment().format("YYYY-MM-DD");
+  }
+  if (selectedCombo === '1 mes atrás') {
+    fechaIniMeta.value = moment().add(-30, 'days').format("YYYY-MM-DD");
+    fechaFinMeta.value = moment().format("YYYY-MM-DD");
+  }
+});
+
+watch(async () => fechaIngresadaSubseccion.value, async () => {
+  let selectedCombo = fechaIngresadaSubseccion.value
+  if (selectedCombo === 'Hoy') {
+    fechaIniSub.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+    fechaFinSub.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+  }
+  if (selectedCombo === 'Hace una semana') {
+    fechaIniSub.value = moment().add(-7, 'days').format("YYYY-MM-DD");
+    fechaFinSub.value = moment().format("YYYY-MM-DD");
+  }
+  if (selectedCombo === '15 días atrás') {
+    fechaIniSub.value = moment().add(-15, 'days').format("YYYY-MM-DD");
+    fechaFinSub.value = moment().format("YYYY-MM-DD");
+  }
+  if (selectedCombo === '1 mes atrás') {
+    fechaIniSub.value = moment().add(-30, 'days').format("YYYY-MM-DD");
+    fechaFinSub.value = moment().format("YYYY-MM-DD");
+  }
+});
+
+watch(async () => fechaIngresadaPais.value, async () => {
+  let selectedCombo = fechaIngresadaPais.value
+  if (selectedCombo === 'Hoy') {
+    fechaIniPais.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+    fechaFinPais.value = moment().add(-1, 'days').format("YYYY-MM-DD");
+  }
+  if (selectedCombo === 'Hace una semana') {
+    fechaIniPais.value = moment().add(-7, 'days').format("YYYY-MM-DD");
+    fechaFinPais.value = moment().format("YYYY-MM-DD");
+  }
+  if (selectedCombo === '15 días atrás') {
+    fechaIniPais.value = moment().add(-15, 'days').format("YYYY-MM-DD");
+    fechaFinPais.value = moment().format("YYYY-MM-DD");
+  }
+  if (selectedCombo === '1 mes atrás') {
+    fechaIniPais.value = moment().add(-30, 'days').format("YYYY-MM-DD");
+    fechaFinPais.value = moment().format("YYYY-MM-DD");
+  }
+});
+
+
+const clickSeccion = () => {
+  const dataToSend = {
+    fecha: selectedfechaIniFin.value,
+    criterio: 'seccion',
+    datei: fechaIni.value,
+    datef: fechaFin.value
+  };
+  console.log(dataToSend);
+};
+
+const clickSub = () => {
+  const dataToSend = {
+    fecha: fechaIngresadaSubseccion.value,
+    criterio: 'subseccion',
+    datei: fechaIniSub.value,
+    datef: fechaFinSub.value
+  };
+  console.log(dataToSend);
+};
+
+const clickMeta = () => {
+  const dataToSend = {
+    fecha: fechaIngresadaMetadato.value,
+    criterio: 'metadatos',
+    datei: fechaIniMeta.value,
+    datef: fechaFinMeta.value
+  };
+  console.log(dataToSend);
+};
+
+const clickPais = () => {
+  const dataToSend = {
+    fecha: fechaIngresadaPais.value,
+    criterio: 'pais',
+    datei: fechaIniPais.value,
+    datef: fechaFinPais.value
+  };
+  console.log(dataToSend);
+};
 
 </script>
 
@@ -123,12 +223,18 @@ const items = [
 .clickable {
   cursor: pointer;
 }
+
+.header_card_item{
+  display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+}
 </style>
 <template>
   <VRow>
     <VCol cols="12" md="6">
       <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
+        <VCardItem class="header_card_item">
           <div class="d-flex">
             <div class="descripcion">
               <VCardTitle>Las 5 secciones que más navegan <br>los usuarios</VCardTitle>
@@ -137,19 +243,20 @@ const items = [
           </div>
 
           <template #append>
-            <div class="date-picker-wrapper" style="width: 250px;">
-              <AppDateTimePicker prepend-inner-icon="tabler-calendar" density="compact" v-model="fechaIngresada"
-                @on-change="obtenerPorFechaMeta" show-current="true" :config="{
-                  position: 'auto right',
-                  mode: 'range',
-                  altFormat: 'F j, Y',
-                  dateFormat: 'Y-m-d',
-                  maxDate: new Date(),
-                  reactive: true
-                }" />
+            <div class="date-picker-wrapper" >
+              <VBtn icon color="success" variant="tonal" @click="clickSeccion">
+                <VIcon size="22" icon="tabler-download" />
+              </VBtn>
 
             </div>
           </template>
+        </VCardItem>
+
+        <VCardItem class="pt-0">
+          <div class="date-picker-wrapper" style="width: 250px;">
+            <VCombobox v-model="selectedfechaIniFin" :items="fechaIniFinList" variant="outlined" label="" persistent-hint
+              hide-selected hint="" />
+          </div>
         </VCardItem>
 
         <VCardText>
@@ -160,7 +267,7 @@ const items = [
 
     <VCol cols="12" md="6">
       <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
+        <VCardItem class="header_card_item">
           <div class="d-flex">
             <div class="descripcion">
               <VCardTitle>Las 5 subsecciones que <br> más navegan los usuarios</VCardTitle>
@@ -169,20 +276,21 @@ const items = [
           </div>
 
           <template #append>
-            <div class="date-picker-wrapper" style="width: 250px;">
-              <AppDateTimePicker prepend-inner-icon="tabler-calendar" density="compact" v-model="fechaIngresadaSubseccion"
-                @on-change="obtenerPorSubsecciones" show-current="true" :config="{
-                  position: 'auto right',
-                  mode: 'range',
-                  altFormat: 'F j, Y',
-                  dateFormat: 'Y-m-d',
-                  maxDate: new Date(),
-                  reactive: true
-                }" />
+            <div class="date-picker-wrapper">
+              <VBtn icon color="success" variant="tonal" @click="clickSub">
+                <VIcon size="22" icon="tabler-download" />
+              </VBtn>
 
             </div>
 
           </template>
+        </VCardItem>
+
+        <VCardItem class="pt-0">
+          <div class="date-picker-wrapper" style="width: 250px;">
+            <VCombobox v-model="fechaIngresadaSubseccion" :items="fechaIniFinList" variant="outlined" label=""
+                persistent-hint hide-selected hint="" />
+          </div>
         </VCardItem>
 
         <VCardText>
@@ -193,33 +301,70 @@ const items = [
 
     <VCol cols="12" md="6">
       <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
+        <VCardItem class="header_card_item">
           <div class="d-flex">
             <div class="descripcion">
-              <VCardTitle>Los 5 metadatos que más navegan <br>los usuarios</VCardTitle>
+              <VCardTitle>Los metadatos que más navegan <br>los usuarios</VCardTitle>
               <VCardSubtitle>Datos desde: {{ fechaIni }} hasta {{ fechaFin }}</VCardSubtitle>
             </div>
           </div>
 
           <template #append>
-            <div class="date-picker-wrapper" style="width: 250px;">
-              <AppDateTimePicker prepend-inner-icon="tabler-calendar" density="compact" v-model="fechaIngresadaMetadato"
-                @on-change="obtenerPorMeta" show-current="true" :config="{
-                  position: 'auto right',
-                  mode: 'range',
-                  altFormat: 'F j, Y',
-                  dateFormat: 'Y-m-d',
-                  maxDate: new Date(),
-                  reactive: true
-                }" />
+            <div class="date-picker-wrapper">
+              <VBtn icon color="success" variant="tonal" @click="clickMeta">
+                <VIcon size="22" icon="tabler-download" />
+              </VBtn>
+            </div>
+          </template>
+        </VCardItem>
+
+        <VCardItem class="pt-0">
+          <div class="date-picker-wrapper" style="width: 250px;">
+            <VCombobox v-model="fechaIngresadaMetadato" :items="fechaIniFinList" variant="outlined" label=""
+                persistent-hint hide-selected hint="" />
               <VSelect class="mt-3" :items="items" label="Secciones" v-model="modelItemsSeccion" />
+
+          </div>
+        </VCardItem>
+
+        <VCardText>
+          <ChartRecMeta :fechaIniMeta="fechaIniMeta" :fechaFinMeta="fechaFinMeta"
+            :modelItemsSeccion="modelItemsSeccion" />
+        </VCardText>
+      </VCard>
+    </VCol>
+
+    <VCol cols="12" md="6">
+      <VCard>
+        <VCardItem class="header_card_item">
+          <div class="d-flex">
+            <div class="descripcion">
+              <VCardTitle>Los paises que más navegan los usuarios</VCardTitle>
+              <VCardSubtitle>Datos desde: {{ fechaIni }} hasta {{ fechaFin }}</VCardSubtitle>
+            </div>
+          </div>
+
+          <template #append>
+            <div class="date-picker-wrapper">
+              <VBtn icon color="success" variant="tonal" @click="clickPais">
+                <VIcon size="22" icon="tabler-download" />
+              </VBtn>
 
             </div>
           </template>
         </VCardItem>
 
+        <VCardItem class="pt-0">
+          <div class="date-picker-wrapper" style="width: 250px;">
+            <VCombobox v-model="fechaIngresadaPais" :items="fechaIniFinList" variant="outlined" label="" persistent-hint
+                hide-selected hint="" />
+              <VSelect class="mt-3" :items="items" label="Secciones" v-model="modelItemsSeccionPais" />
+          </div>
+        </VCardItem>
+
         <VCardText>
-          <ChartRecMeta :fechaIniMeta="fechaIniMeta" :fechaFinMeta="fechaFinMeta" :modelItemsSeccion="modelItemsSeccion" />
+          <ChartRecPais :fechaIniPais="fechaIniPais" :fechaFinPais="fechaFinPais"
+            :modelItemsSeccionPais="modelItemsSeccionPais" />
         </VCardText>
       </VCard>
     </VCol>
---
+++ servicios/backoffice/v1starter/src/views/charts/ecuavisa/ChartRecPais.vue
@@ -0,0 +1,143 @@
+<script setup>
+  import VueApexCharts from 'vue3-apexcharts';
+  import { hexToRgb } from '@layouts/utils';
+  import { useTheme } from 'vuetify';
+
+  import Moment from 'moment';
+  import { extendMoment } from 'moment-range';
+  import esLocale from "moment/locale/es";
+
+  const moment = extendMoment(Moment);
+  moment.locale('es', [esLocale]);
+  const isLoading = ref(false);
+  const uniqueKey = ref(0);
+
+  const props = defineProps({
+    fechaIniPais: String,
+    fechaFinPais: String,
+    modelItemsSeccionPais: String,
+  });
+
+  const dataChart = ref([]);
+
+  const isMobile = window.innerWidth <= 768;
+  const dateNowF = ref(moment().format("DD/MM/YYYY HH:mm:ss").toString());
+
+  // 👉 Colors variables
+  const colorVariables = themeColors => {
+    const themeSecondaryTextColor = `rgba(${hexToRgb(themeColors.colors['on-surface'])},${themeColors.variables['medium-emphasis-opacity']})`
+    const themeDisabledTextColor = `rgba(${hexToRgb(themeColors.colors['on-surface'])},${themeColors.variables['disabled-opacity']})`
+    const themeBorderColor = `rgba(${hexToRgb(String(themeColors.variables['border-color']))},${themeColors.variables['border-opacity']})`
+    const themePrimaryTextColor = `rgba(${hexToRgb(themeColors.colors['on-surface'])},${themeColors.variables['high-emphasis-opacity']})`
+    
+    return { themeSecondaryTextColor, themeDisabledTextColor, themeBorderColor, themePrimaryTextColor }
+  }
+
+  const vuetifyTheme = useTheme();
+
+  const { themeBorderColor, themeDisabledTextColor } = colorVariables(vuetifyTheme.current.value);
+
+  const resolveData = computed(() => {
+
+    let dataRaw = Array.from(dataChart.value);
+    const seriesFormat = {
+        name: 'Interacciones de usuarios',
+        data: []
+    };
+
+    const categoriesRaw = [];
+    for (let i in dataRaw) {
+        let num = parseInt(dataRaw[i].total);
+        seriesFormat.data.push(num);
+        categoriesRaw.push(dataRaw[i].name);   
+    }
+
+    const options= {
+        chart: {
+          parentHeightOffset: 0,
+          toolbar: { show: false },
+          height: (seriesFormat.data.length > 0 && seriesFormat.data.length < 6)?400:700
+        },
+        dataLabels: { 
+          enabled: true
+        },
+        colors: ['#ff4949'],   
+        plotOptions: {
+          bar: {
+            borderRadius: 0,
+            barHeight: '40%',
+            horizontal: true,
+            startingShape: 'rounded',
+          },
+        },
+        grid: {
+          borderColor: themeBorderColor,
+          xaxis: {
+            lines: { show: true },
+          },
+          padding: {
+            top: -10,
+          },
+        },
+        yaxis: {
+          labels: {
+            style: { colors: themeDisabledTextColor },
+          },
+        },
+        xaxis: {
+          axisBorder: { show: false },
+          axisTicks: { color: themeBorderColor },
+          categories: categoriesRaw,
+          labels: {
+            style: { colors: themeDisabledTextColor },
+          },
+        },
+        minHeight: 300,
+      }
+
+
+    return {series: [seriesFormat], options: options, intereses: categoriesRaw};
+  });
+
+  async function getChart(fechaIniPais, fechaFinPais, seccion) {
+    var link = `https://servicio-de-actividad.vercel.app/grafico/metadato/getpais/10?fechai=${fechaIniPais}&fechaf=${fechaFinPais}`;
+
+    if(seccion.value != 0 && seccion != 0){
+      link += `&seccion=${seccion}`;
+    }
+
+    await fetch(link)
+      .then(response => response.json())
+      .then(data => {
+        if(data.resp){
+          dataChart.value = data.data;
+        }
+      }).catch(error => { 
+        // isDialogVisibleChart2.value = false;
+        return error;
+      });
+  }
+
+  watch(props, async (newProps, oldProps) => {
+    // Imprime las nuevas fechas
+    isLoading.value = true;
+    await getChart(newProps.fechaIniPais, newProps.fechaFinPais, newProps.modelItemsSeccionPais);
+    isLoading.value = false;
+      // console.log(resolveData.value.options, resolveData.value.series)
+    // Realiza cualquier acción adicional que desees aquí, por ejemplo, actualizar el componente ChartRecomendaciones
+  });
+
+
+</script>
+<template>
+  <VRow>
+    <VCol cols="12" md="12" :class="isLoading?'disabled':''">
+        <VueApexCharts
+          :key="uniqueKey"
+          type="bar"
+          :options="resolveData.options"
+          :series="resolveData.series"
+        />
+    </VCol>
+  </VRow>
+</template>
