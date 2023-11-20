cdc0be7ec158f6718c5a624e58eeba2b0914af6d
Eri Vera <eriveraec@gmail.com>
2023-11-20 17:50:27 -0500

    Update fa2023.js

--- devCloudflare/finano2023/fa2023.js
+++ devCloudflare/finano2023/fa2023.js
@@ -1,7 +1,8 @@
 // Agregar la clase "active" a los dos primeros elementos
-var reveals = document.querySelectorAll(".bloque_list_finano .noticias article");
-reveals[0].classList.add("active");
-reveals[1].classList.add("active");
+var initialReveals = document.querySelectorAll(".bloque_list_finano .noticias article");
+for (var i = 0; i < 2; i++) {
+  initialReveals[i].classList.add("active");
+}
 
 function reveal() {
   var reveals = document.querySelectorAll(".bloque_list_finano .noticias article");
