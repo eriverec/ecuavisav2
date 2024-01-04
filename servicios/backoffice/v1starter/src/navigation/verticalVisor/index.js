abcaaedb23b14c45350436c6c333834139fbbf6a
redy <granredy@gmail.com>
2024-01-04 17:32:12 -0500

    visor fixes

--- servicios/backoffice/v1starter/src/navigation/verticalVisor/index.js
+++ servicios/backoffice/v1starter/src/navigation/verticalVisor/index.js
@@ -4,30 +4,21 @@ export default [
       to: { name: 'index' },
       icon: { icon: 'tabler-smart-home' },
     },
-    
     {
-      title: 'Concursos',
-      icon: { icon: 'tabler-balloon' },
-      // to: { name: 'apps-concursos' },
-      children: [
-        { title: 'Click click boom', to: 'apps-concursos' },
-        { title: 'Gavilanes', to: 'apps-concursos-gavilanes' },
-      //   // { title: 'View', to: { name: 'apps-user-view-id', params: { id: 21 } } },
-      ],
-    },
+      title: 'NewsLetter',
+      to: { name: 'apps-mailing-tabs-edit' },
+      icon: { icon: 'tabler-mail' },
+    },    
+    // {
+    //   title: 'Concursos',
+    //   icon: { icon: 'tabler-balloon' },
+    //   // to: { name: 'apps-concursos' },
+    //   children: [
+    //     { title: 'Click click boom', to: 'apps-concursos' },
+    //     { title: 'Gavilanes', to: 'apps-concursos-gavilanes' },
+    //   ],
+    // },
     
-    {
-      title: 'Trazabilidad',
-      icon: { icon: 'tabler-database' },
-      // to: { name: 'apps-trazabilidad-list' },
-      children: [
-        // { title: 'Lista de Usuarios', to: 'apps-trazabilidad-list' },
-        // { title: 'View', to: { name: 'apps-user-view-id', params: { id: 21 } } },
-        // modulos por salir 
-        // { title: 'Dispositivos', to: 'apps-dispositivos' },
-        // { title: 'Visitas', to: 'apps-visitas' }
-      ],
-    },
   
   ]
   
