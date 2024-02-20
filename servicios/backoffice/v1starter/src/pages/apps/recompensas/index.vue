f53d41ea633601e79b8628b24cd7db963a3687a1
Eri Vera <eriveraec@gmail.com>
2024-02-20 15:42:03 -0500

    recompensas v3 creditos erivera

--- servicios/backoffice/v1starter/src/pages/apps/recompensas/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/recompensas/index.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
     <VCard title="Recompesas">
-      <VCardSubtitle> Busca los usuarios por nombres, apellidos y emails </VCardSubtitle>
+      <!-- <VCardSubtitle> Busca los usuarios por nombres, apellidos y emails </VCardSubtitle> -->
       <VCardText>
         <div class="d-flex gap-4">
-          <VTextField v-model="searchTerm" @keyup.enter="search" style="max-width: 400px;" label="Buscar..."  />
-          <VBtn @click="search" color="primary">
+          <VTextField v-model="searchTerm" @keyup.enter="startSearch" style="max-width: 400px;" label="Buscar..." />
+          <VBtn @click="startSearch" color="primary">
             Buscar
           </VBtn>
         </div>
@@ -13,41 +13,69 @@
       </VCardText>
     </VCard>
 
-    <VCard class="mt-4" title="Resultado">
-      <VCardText>
+    <VRow>
+      <VCol cols="7">
+        <VCard class="mt-4" title="Resultado de la búsqueda">
+          <VCardText>
 
-        <div v-if="loading">Cargando...</div>
+            <div v-if="loading" style="height: 400px;" class="d-flex align-center justify-center ">Cargando...</div>
+            <div v-else-if="searchResults.length > 0">
+              <VTable style="width: 100%;" class="text-no-wrap tableNavegacion mb-5" hover="true">
+                <thead>
+                  <tr>
+                    <th scope="col">Usuario</th>
+                    <th scope="col">Correo</th>
+                  </tr>
+                </thead>
 
-        <VTable v-else-if="searchResults.length > 0" style="width: 100%;" class="text-no-wrap tableNavegacion mb-5"
-          hover="true">
-          <thead>
-            <tr>
-              <th scope="col">Usuario</th>
-              <th scope="col">Correo</th>
-            </tr>
-          </thead>
+                <tbody>
+                  <tr v-for="user in searchResults" :key="user._id" class="" @click="handleUserClick(user.wylexId)">
+                    <td>
+                      {{ (user.last_name + ' ' + user.first_name).length > 25 ? (user.last_name + ' ' +
+                        user.first_name).substring(0,
+                          25) + "..." : (user.last_name + ' ' + user.first_name) }}
+                    </td>
+                    <td class="text-medium-emphasis">
+                      {{ user.email }}
+                    </td>
 
-          <tbody>
-            <tr v-for="user in searchResults" :key="user._id" class="">
-              <td>
-                {{ (user.last_name + ' ' + user.first_name).length > 25 ? (user.last_name + ' ' + user.first_name).substring(0,
-                  25) + "..." : (user.last_name + ' ' + user.first_name) }}
-              </td>
-              <td class="text-medium-emphasis">
-                {{ user.email }}
-              </td>
+                  </tr>
 
-            </tr>
-
-          </tbody>
-        </VTable>
-        <div v-else>
-          <td colspan="3">No hay datos</td>
-        </div>
+                </tbody>
 
 
-      </VCardText>
-    </VCard>
+              </VTable>
+              <VPagination v-if="total > limit" v-model="page" size="small" :total-visible="5" :length="total"
+                @update:model-value="updatePage" />
+
+            </div>
+            <div v-else>No hay datos</div>
+
+
+
+          </VCardText>
+        </VCard>
+
+      </VCol>
+      <VCol cols="5">
+        <VCard class="mt-4" title="Información detalla">
+          <VCardText>
+            <div v-if="cargandoUser" class="d-flex align-center justify-center ">Cargando...</div>
+            <div v-else-if="selectedUserDetails">
+              <ul>
+                <li v-for="(item, index) in selectedUserDetails.data" :key="index">
+                  <p>{{ item.title }}</p>
+                  <!-- <p>{{ item.section }}</p> -->
+                </li>
+              </ul>
+            </div>
+            <div v-else>No se encontro información</div>
+          </VCardText>
+        </VCard>
+
+      </VCol>
+    </VRow>
+
   </section>
 </template>
 
@@ -57,16 +85,33 @@ import { ref } from 'vue';
 const searchTerm = ref('');
 const searchResults = ref([]);
 const loading = ref(false);
+const cargandoUser = ref(false);
+
+
+const page = ref(1);
+const limit = ref(10); // Esto debería coincidir con el límite establecido por la API
+const total = ref(0);
+const selectedUserDetails = ref(null);
+// const totalPages = computed(() => Math.ceil(total.value / limit.value));
+
+const startSearch = () => {
+  page.value = 1; // Reinicia la página a 1 al realizar una nueva búsqueda
+  search();
+};
 
 const search = async () => {
   loading.value = true;
+  // searchResults.value = [];
   try {
-    const response = await fetch(`https://ads-service.vercel.app/busqueda/user/?s=${searchTerm.value}`);
+    // const response = await fetch(`https://ads-service.vercel.app/busqueda/user/?s=${searchTerm.value}`);
+    const response = await fetch(`https://ads-service.vercel.app/busqueda/user/?s=${searchTerm.value}&page=${page.value}&limit=${limit.value}`);
     const data = await response.json();
     if (data.resp) {
       searchResults.value = data.data;
+      total.value = data.total;
     } else {
       searchResults.value = [];
+      total.value = 0;
     }
   } catch (error) {
     console.error('Error al realizar la búsqueda:', error);
@@ -75,4 +120,28 @@ const search = async () => {
   }
 };
 
+const updatePage = (newPage) => {
+  page.value = newPage;
+  search();
+};
+
+const handleUserClick = async (wylexId) => {
+  cargandoUser.value = true;
+
+  try {
+    const response = await fetch(`https://servicio-de-actividad.vercel.app/recomendadasv2/${wylexId}`);
+    const data = await response.json();
+    if (data.resp && data.data.length > 0) {
+      selectedUserDetails.value = data;
+    } else {
+      selectedUserDetails.value = null;
+    }
+  } catch (error) {
+    console.error('Error al obtener detalles del usuario:', error);
+    selectedUserDetails.value = null;
+  } finally {
+    cargandoUser.value = false;
+
+  }
+};
 </script>
