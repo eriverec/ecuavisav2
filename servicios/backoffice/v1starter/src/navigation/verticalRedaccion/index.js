b5bdc89e1e6d98b0934c584cdadd1df6bbfc237a
AVergaraR <88627261+fredcrz@users.noreply.github.com>
2024-03-18 15:06:16 -0500

    Update index.js

--- servicios/backoffice/v1starter/src/navigation/verticalRedaccion/index.js
+++ servicios/backoffice/v1starter/src/navigation/verticalRedaccion/index.js
@@ -1,16 +1,9 @@
 export default [
     
-    {
-    title: 'Newsletter',
-    icon: { icon: 'tabler-mail' },
-    children: [
-      { title: 'Editar Newsletter', to: 'apps-mailing-tabs-edit' },
-      { title: 'Forzar Newsletter', to: 'apps-mailing-forzado' }
-
-      // { title: 'Ads Ubicaciones', to: 'apps-miecuavisa-ads-ubicaciones' },
-      // { title: 'Eventos', to: 'apps-configuracion-add' },
-      //{ title: 'Player Forzado', to: 'apps-configuracion-player' }
-    ]
+  {
+    title: 'Dashboard',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
   }
   
   ]
