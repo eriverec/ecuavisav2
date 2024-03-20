0c076481ccf5ae3376c853d13d0cf40be755b118
redy <granredy@gmail.com>
2024-03-20 17:22:09 -0500

    Update index.js

--- servicios/backoffice/v1starter/src/navigation/verticalRedaccion/index.js
+++ servicios/backoffice/v1starter/src/navigation/verticalRedaccion/index.js
@@ -1,10 +1,37 @@
 export default [
     
-  {
-    title: 'Dashboard',
-    to: { name: 'index' },
-    icon: { icon: 'tabler-smart-home' },
-  }
+    {
+    title: 'Newsletter',
+    icon: { icon: 'tabler-mail' },
+    children: [
+      { title: 'Editar Newsletter', to: 'apps-mailing-tabs-edit' },
+      { title: 'Forzar Newsletter', to: 'apps-mailing-forzado' }
+
+      // { title: 'Ads Ubicaciones', to: 'apps-miecuavisa-ads-ubicaciones' },
+      // { title: 'Eventos', to: 'apps-configuracion-add' },
+      //{ title: 'Player Forzado', to: 'apps-configuracion-player' }
+    ]
+  },
   
-  ]
+  {
+    title: 'Trazabilidad',
+    icon: { icon: 'tabler-database' },
+    children: [
+      // { title: 'Lista de Usuarios', to: 'apps-trazabilidad-list' },
+      // { title: 'Visitas', to: 'apps-visitas-list' },
+      { title: 'Permanencia', to: 'apps-trazabilidad-permanencia' },
+      // { title: 'Metadatos', to: 'apps-metadatos' },
+      // { title: 'Dispositivos', to: 'apps-dispositivos' },
+      // { title: 'Recomendaciones', to: 'apps-recomendaciones' },
+
+      // modulos por salir
+      //{ title: 'Trazabilidad usuarios', to: 'apps-trazabilidad-users' },
+      //{ title: 'Trazabilidad usuarios', to: 'apps-trazabilidad-ubicaciones' },
+      // { title: 'Trazabilidad usuarios', to: 'apps-notas-recomendadas'}, //apps/notas-recomendadas
+
+    ],
+  },
+  
+]
+  
   
