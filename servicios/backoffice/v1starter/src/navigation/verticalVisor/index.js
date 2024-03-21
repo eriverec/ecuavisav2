2402ad6bd0ae1ba825223a9cd4df638e0e355962
redy <granredy@gmail.com>
2024-03-21 15:28:53 -0500

    fix menu

--- servicios/backoffice/v1starter/src/navigation/verticalVisor/index.js
+++ servicios/backoffice/v1starter/src/navigation/verticalVisor/index.js
@@ -8,7 +8,26 @@ export default [
       title: 'NewsLetter',
       to: { name: 'apps-mailing-tabs-edit' },
       icon: { icon: 'tabler-mail' },
-    },    
+    },
+  
+  {
+    title: 'Trazabilidad',
+    icon: { icon: 'tabler-database' },
+    children: [
+      // { title: 'Lista de Usuarios', to: 'apps-trazabilidad-list' },
+      // { title: 'Visitas', to: 'apps-visitas-list' },
+      { title: 'Permanencia', to: 'apps-trazabilidad-permanencia' },
+      // { title: 'Metadatos', to: 'apps-metadatos' },
+      // { title: 'Dispositivos', to: 'apps-dispositivos' },
+      // { title: 'Recomendaciones', to: 'apps-recomendaciones' },
+
+      // modulos por salir
+      //{ title: 'Trazabilidad usuarios', to: 'apps-trazabilidad-users' },
+      //{ title: 'Trazabilidad usuarios', to: 'apps-trazabilidad-ubicaciones' },
+      // { title: 'Trazabilidad usuarios', to: 'apps-notas-recomendadas'}, //apps/notas-recomendadas
+
+    ],
+  },    
     // {
     //   title: 'Concursos',
     //   icon: { icon: 'tabler-balloon' },
