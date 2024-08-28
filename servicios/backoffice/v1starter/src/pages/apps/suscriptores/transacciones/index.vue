1f3b0e07ae8613d959efce96692203254d254ece
Eri Vera <eriveraec@gmail.com>
2024-08-28 12:32:23 -0500

    ajustes y modal details

--- servicios/backoffice/v1starter/src/pages/apps/suscriptores/transacciones/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/suscriptores/transacciones/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { avatarText } from '@core/utils/formatters'
 import { ref, watch, computed, onMounted } from 'vue'
+import { format } from 'date-fns'
 import { extendMoment } from 'moment-range'
 import Moment from 'moment-timezone'
 import esLocale from "moment/locale/es"
@@ -8,6 +9,12 @@ import esLocale from "moment/locale/es"
 const moment = extendMoment(Moment)
 moment.locale('es', [esLocale])
 moment.tz.setDefault('America/Guayaquil')
+
+const meses = {
+  'January': 'enero', 'February': 'febrero', 'March': 'marzo', 'April': 'abril',
+  'May': 'mayo', 'June': 'junio', 'July': 'julio', 'August': 'agosto',
+  'September': 'septiembre', 'October': 'octubre', 'November': 'noviembre', 'December': 'diciembre'
+}
 
 // Referencias reactivas
 const transactions = ref([])
@@ -17,6 +24,13 @@ const totalPage = ref(0)
 const rowPerPage = ref(10)
 const loadingTransactions = ref(false)
 const searchQuery = ref('')
+
+// Agregar estas nuevas referencias reactivas
+const selectedTransaction = ref(null)
+const isModalOpen = ref(false)
+
+
+
 
 // Función para obtener transacciones
 async function getTransactions(page, limit) {
@@ -96,6 +110,27 @@ const paginatedTransactions = computed(() => {
   return filteredTransactions.value.slice(start, end)
 })
 
+// Agregar esta nueva función para abrir el modal
+function openModal(transaction) {
+  selectedTransaction.value = transaction
+  isModalOpen.value = true
+}
+
+// Función auxiliar para formatear la fecha
+function formatDate(dateString) {
+  if (!dateString) return 'N/A'
+  return format(new Date(dateString), 'yyyy-MM-dd HH:mm:ss')
+}
+
+// Función auxiliar para formatear la fecha
+function formatPaymentDate(dateString) {
+  if (!dateString) return 'Fecha no disponible'
+  const fecha = moment(dateString)
+  const mesEnIngles = fecha.format('MMMM')
+  const mesEnEspanol = meses[mesEnIngles]
+  return `Pagado el ${fecha.format('D [de] ')}${mesEnEspanol}${fecha.format(' [de] YYYY')}`
+}
+
 // Lifecycle hooks
 onMounted(() => {
   getTransactions(1, 100)  // Inicialmente cargamos 100 transacciones
@@ -136,6 +171,8 @@ onMounted(() => {
                 <th scope="col">Paquete</th>
                 <th scope="col">Fecha de Pago</th>
                 <th scope="col">Estado</th>
+                <th scope="col">Acción</th>
+
 
               </tr>
             </thead>
@@ -146,12 +183,20 @@ onMounted(() => {
                 <td>{{ (Array.isArray(item.user) ? item.user[0]?.email : item.user?.email) || 'N/A' }}</td>
                 <td>{{ item.transaction?.id || 'N/A' }}</td>
                 <td>{{ item.paquete?.nombre || 'N/A' }}</td>
-                <td>{{ item.transaction?.payment_date ? moment(item.transaction.payment_date).format('YYYY-MM-DD HH:mm:ss') : 'N/A' }}</td>
+                <td>{{ item.transaction?.payment_date ? moment(item.transaction.payment_date).format('DD-MM-YYYY') : 'N/A' }}
+                </td>
                 <td>
                   <VChip :color="item.estado === 'Realizado' ? 'success' : 'error'">
                     {{ item.estado }}
                   </VChip>
                 </td>
+
+                <td>
+                  <VBtn @click="openModal(item)" variant="plain" color="info" size="small">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-credit-card-pay"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" /><path d="M3 10h18" /><path d="M16 19h6" /><path d="M19 16l3 3l-3 3" /><path d="M7.005 15h.005" /><path d="M11 15h2" /></svg>
+                  </VBtn>
+                </td>
+                
               </tr>
             </tbody>
             <tfoot v-show="!paginatedTransactions.length && !loadingTransactions">
@@ -171,6 +216,85 @@ onMounted(() => {
             <VPagination v-model="currentPage" size="small" :total-visible="5" :length="totalPage" />
           </VCardText>
         </VCard>
+
+        <!-- Modal para mostrar los detalles de la transacción -->
+        <VDialog v-model="isModalOpen" max-width="500px">
+          <VCard v-if="selectedTransaction">
+            <!-- <VCardTitle>Detalles de la transacción</VCardTitle> -->
+            <VCardText>
+              <VList>
+                <VListItem>
+                  <div class="d-flex justify-space-between align-center">
+                    <div >
+                      <VListItemTitle>Detalles de la transacción</VListItemTitle>
+                      <h2 class="text-5xl mb-2 mt-2">${{ selectedTransaction.transaction?.amount || 'N/A' }}</h2>
+                      <VListItemSubtitle>{{ formatPaymentDate(selectedTransaction.transaction?.payment_date) }}</VListItemSubtitle>
+                    </div>
+                    <div>
+                      <svg style="width: 120px ;" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" /></svg>
+  
+                    </div>
+
+                  </div>
+                </VListItem>
+                <div height="1" class="mt-3 mb-3" style="border:0;border-collapse:collapse;margin:0;padding:0;height:1px;font-size:1px;background-color:rgb(235 235 235 / 20%);line-height:1px">&nbsp;</div>
+                <VListItem>
+                  <div class="d-flex justify-space-between align-center">
+                    <VListItemTitle>Estado</VListItemTitle>
+                    <VListItemSubtitle>
+                      <VChip :color="selectedTransaction.estado === 'Realizado' ? 'success' : 'error'">
+                        {{ selectedTransaction.estado }}
+                      </VChip>
+                    </VListItemSubtitle>
+
+                  </div>
+                </VListItem>
+                <VListItem >
+                  <div class="d-flex justify-space-between align-center">
+                    <VListItemTitle>ID</VListItemTitle>
+                    <VListItemSubtitle>{{ selectedTransaction.transaction?.id || 'N/A' }}</VListItemSubtitle>
+                  </div>
+                  
+                </VListItem>
+             
+                <VListItem>
+                  <div class="d-flex justify-space-between align-center">
+                    <VListItemTitle>Descripción del Producto</VListItemTitle>
+                    <VListItemSubtitle>{{ selectedTransaction.paquete?.nombre || 'N/A' }}</VListItemSubtitle>
+                  </div>
+                </VListItem>
+                <div v-if="selectedTransaction.user">
+                  <VListItem>
+                    <div class="d-flex justify-space-between align-center">
+                      <VListItemTitle>Nombres</VListItemTitle>
+                      <VListItemSubtitle> {{ selectedTransaction.user?.first_name }}  {{ selectedTransaction.user?.last_name }}</VListItemSubtitle>
+  
+                    </div>
+                  </VListItem>
+                  <VListItem>
+                    <div class="d-flex justify-space-between align-center">
+                      <VListItemTitle>Email</VListItemTitle>
+                      <VListItemSubtitle> {{ selectedTransaction.user?.email }}</VListItemSubtitle>
+  
+                    </div>
+                  </VListItem>
+
+                </div>
+              </VList>
+           
+             
+            </VCardText>
+            <VCardActions>
+              <VSpacer></VSpacer>
+              <VBtn color="blue darken-1" text @click="isModalOpen = false">Cerrar</VBtn>
+            </VCardActions>
+          </VCard>
+        </VDialog>
+
+
+
+
+
       </VCol>
     </VRow>
   </section>
