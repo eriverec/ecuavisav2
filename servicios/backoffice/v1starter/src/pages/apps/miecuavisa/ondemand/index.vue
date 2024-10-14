1f4922a67c1163b8e03c23ec0dd16317c4de1e8a
Eri Vera <eriveraec@gmail.com>
2024-10-14 12:07:11 -0500

    ajustes en modal ondemand

--- servicios/backoffice/v1starter/src/pages/apps/miecuavisa/ondemand/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/miecuavisa/ondemand/index.vue
@@ -1,47 +1,61 @@
 <template>
   <div>
-    <VSnackbar
-          :timeout="2000"
-          v-model="isSave"
-          color="success"
-          transition="scale-transition"
-          location="top end"
-        >
-          Datos actualizados correctamente
-        </VSnackbar>
-    
-      <VCard class="mt-5 card" title="Modal Omdemand" subtitle="Ecuavisa">
-        <VCardText>
-          <VRow class="ml-2 mr-2 mb-2">
-            <VCol cols="12" md="6" lg="6">
-              <VRow id="modal">
-                <VCol cols="12">
-                  <VSwitch v-model="estado" :label="capitalizedLabel(estado)" />
-                </VCol>
-                <VCol cols="12">
-                  <VTextarea  label="Contenido del modal" type="text" v-model="contenido" />
-                </VCol>
-                <VCol cols="12">
-                  <VTextarea  label="URL" type="text" v-model="url" />
-                </VCol>
-                <VCol cols="12">
-                  <VBtn @click="updateData" color="success" variant="tonal">
-                    Guardar
-                  </VBtn>
-                </VCol>
-              </VRow>
-            </VCol>
-    
-            <VCol cols="12" md="6" lg="6"></VCol>
-    
-          </VRow>
-    
-        </VCardText>
-      </VCard>
+    <VSnackbar :timeout="2000" v-model="isSave" color="success" transition="scale-transition" location="top end">
+      Datos actualizados correctamente
+    </VSnackbar>
+
+    <VCard class="mt-5 card" title="Modal Omdemand" subtitle="Ecuavisa">
+      <VCardText>
+        <VRow class="ml-2 mr-2 mb-2">
+          <VCol cols="12" md="6" lg="6">
+            <VRow id="modal">
+              <VCol cols="12">
+                <VSwitch v-model="estado" :label="capitalizedLabel(estado)" />
+              </VCol>
+              <VCol cols="12">
+                <VTextarea label="Contenido del modal" type="text" v-model="contenido" />
+              </VCol>
+              <VCol cols="12">
+                  <div class="demo-space-x">
+                    <VCombobox
+                      class="listUrls"
+                      v-model="urls"
+                      multiple
+                      chips
+                      :items="items"
+                      variant="outlined"
+                      label="URLS"
+                    />
+
+
+                  </div>
+
+        
+              </VCol>
+              <VCol cols="12">
+                <VBtn @click="updateData" color="success" variant="tonal">
+                  Guardar
+                </VBtn>
+              </VCol>
+            </VRow>
+          </VCol>
+
+          <VCol cols="12" md="6" lg="6"></VCol>
+
+        </VRow>
+
+      </VCardText>
+    </VCard>
 
   </div>
 
 </template>
+<style>
+.listUrls .v-chip{
+  white-space: normal; 
+  height: auto;
+}
+</style>
 
 <script setup>
 import { ref, onMounted } from 'vue';
@@ -49,7 +63,8 @@ import { ref, onMounted } from 'vue';
 // Variables reactivas
 const estado = ref(false);
 const contenido = ref('');
-const url = ref('');
+// const urlInput = ref('');
+const urls = ref([]);
 const isSave = ref(false);
 
 // Función para obtener los datos del JSON
@@ -59,7 +74,8 @@ const fetchData = async () => {
     const data = await response.json();
     estado.value = data.data.estado === "true";
     contenido.value = data.data.contenido;
-    url.value = data.data.url;
+    urls.value = data.data.url || [];
+    // urlInput.value = data.data.url.join(', ');
   } catch (error) {
     console.error('Error fetching data:', error);
   }
@@ -70,12 +86,18 @@ onMounted(fetchData);
 
 // Función para actualizar los datos
 const updateData = async () => {
+  //const urls = urlInput.value.split(/[\s,]+/).filter(url => url.length > 0); // Convertir la cadena en un array de URLs
+
+  const urlsSinComas = urls.value // Convertir la cadena en un array de URLs
+
+
+
   const newData = {
     key: "modalondemand",
     data: {
       estado: estado.value ? "true" : "false",
       contenido: contenido.value,
-      url: url.value
+      url: urlsSinComas
     }
   };
 
