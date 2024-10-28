a76bc0f6c908d978e3e83e7ff17e94deb814b9ec
BrendaC04 <brendapilozo@gmail.com>
2024-10-28 16:15:16 -0500

    parametros en vivo

---
+++ servicios/backoffice/v1starter/src/pages/apps/configuracion/parametros-envivo/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <VCard class="mt-5" title="Configuración de Redirección En Vivo">
+      <VCardText>
+        <form @submit.prevent="updateParameters">
+          <VRow>
+            <VCol cols="12" md="6">
+              <label for="num_redireccion">Número de intentos permitidos:</label>
+              <VTextField
+                v-model="numRedireccion"
+                type="number"
+ 
+                id="num_redireccion"
+                name="num_redireccion"
+                min="0"
+                required
+              />
+            </VCol>
+            
+            <VCol cols="12" md="6">
+              <label for="tiempo">Tiempo en segundos antes de redirección:</label>
+              <VTextField
+                v-model="tiempoSegundos"
+                type="number"
+  
+                id="tiempo"
+                name="tiempo"
+                min="0"
+                required
+              />
+            </VCol>
+          </VRow>
+
+          <VBtn class="my-3" type="submit" color="success" variant="tonal">
+            Actualizar Parámetros
+          </VBtn>
+        </form>
+      </VCardText>
+    </VCard>
+
+    <VSnackbar
+      v-model="configSnackbar.model"
+      location="top end"
+      variant="flat"
+      :timeout="configSnackbar.timeout || 2000"
+      :color="configSnackbar.type"
+    >
+      {{ configSnackbar.message }}
+    </VSnackbar>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+// Variables
+const numRedireccion = ref('');
+const tiempoSegundos = ref('');
+
+const apiUrl = 'https://estadisticas.ecuavisa.com/sites/gestor/Tools/en_vivo_num_redireccion/ajax/ajax_envivo.php';
+
+const configSnackbar = ref({
+  message: "Datos guardados",
+  type: "success",
+  model: false,
+  timeout: 2000
+});
+
+// Obtener los parámetros actuales
+const getParameters = async () => {
+  try {
+    const response = await axios.get(apiUrl, {
+      params: { action: 'get_envivo' }
+    });
+    if (response.data.resp) {
+      numRedireccion.value = response.data.data.num_redireccion;
+      tiempoSegundos.value = response.data.data.tiempo_segundos;
+    }
+  } catch (error) {
+    console.error('Error al obtener los parámetros:', error);
+    configSnackbar.value = {
+      message: "Error al obtener los datos",
+      type: "error",
+      model: true
+    };
+  }
+};
+
+// Actualizar los parámetros
+const updateParameters = async () => {
+  try {
+    const response = await axios.post(apiUrl, {
+      id: "1",
+      action: "edit_envivo",
+      tiempo_segundos: parseInt(tiempoSegundos.value),
+      num_redireccion: parseInt(numRedireccion.value)
+    });
+
+    if (response.data.resp) {
+      configSnackbar.value = {
+        message: "Parámetros actualizados correctamente",
+        type: "success",
+        model: true
+      };
+    } else {
+      configSnackbar.value = {
+        message: "Error al actualizar los parámetros",
+        type: "error",
+        model: true
+      };
+    }
+  } catch (error) {
+    console.error('Error al actualizar los parámetros:', error);
+    configSnackbar.value = {
+      message: "Error en la actualización",
+      type: "error",
+      model: true
+    };
+  }
+};
+
+// Obtener los parámetros iniciales al montar el componente
+onMounted(() => {
+  getParameters();
+});
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+button {
+  width: fit-content;
+}
+</style>
--- servicios/backoffice/v1starter/src/pages/apps/settings/menu/PrincipalTab.vue
+++ servicios/backoffice/v1starter/src/pages/apps/settings/menu/PrincipalTab.vue
@@ -1,3 +1,9 @@
+<style scoped>
+.text-h6 {
+  font-size: 1.05rem !important;
+}
+</style>
+
 <template>
 							<VWindowItem value="tab-Principal">
 								<div class=" py-5 gap-4 align-items-center">   
@@ -8,7 +14,7 @@
 								<VRow>
 									<VCol cols="12" lg="10" class="mx-auto my-8 align-items-center" >
 										<VRow>
-											<VCol cols="12" md="4" >
+											<VCol cols="12" md="3" >
 												<VCard  flat  border class=" cursor-pointer"  @click="$router.push({ name: 'apps-configuracion-radio' })" >
 													<VCardText class="text-center">
 														<VAvatar rounded size="70" color="primary" variant="tonal" class="p-5 mb-2" >
@@ -24,7 +30,7 @@
 													</VCardText>
 												</VCard>
 											</VCol>
-											<VCol cols="12" md="4" >
+											<VCol cols="12" md="3" >
 												<VCard  flat  border class=" cursor-pointer"   @click="$router.push({ name: 'apps-configuracion-envivo' })" >
 													<VCardText class="text-center">
 														<VAvatar rounded size="70" color="primary" variant="tonal" class="p-5 mb-2" >
@@ -40,7 +46,7 @@
 													</VCardText>
 												</VCard>
 											</VCol>
-											<VCol cols="12" md="4" >
+											<VCol cols="12" md="3" >
 												<VCard  flat  border class=" cursor-pointer"  @click="$router.push({ name: 'apps-configuracion-envivo-quito' })" >
 													<VCardText class="text-center">
 														<VAvatar rounded size="70" color="primary" variant="tonal" class="p-5 mb-2" >
@@ -51,6 +57,22 @@
 														</h6>
 														<!-- <p>mas info</p> -->
 														<VBtn size="small" variant="tonal" :to="{  name: 'apps-settings-menu' }" >
+															Configurar
+														</VBtn>
+													</VCardText>
+												</VCard>
+											</VCol>
+											<VCol cols="12" md="3" >
+												<VCard  flat  border class=" cursor-pointer"  @click="$router.push({ name: 'apps-configuracion-parametros-envivo' })" >
+													<VCardText class="text-center">
+														<VAvatar rounded size="70" color="primary" variant="tonal" class="p-5 mb-2" >
+															<VIcon icon="mdi-cogs"  size="45" />
+														</VAvatar>
+														<h6 class="text-h6 my-3">
+															Parámetros En Vivo
+														</h6>
+														<!-- <p>mas info</p> -->
+														<VBtn size="small" variant="tonal" :to="{  name: 'apps-configuracion-parametros-envivo' }" >
 															Configurar
 														</VBtn>
 													</VCardText>
