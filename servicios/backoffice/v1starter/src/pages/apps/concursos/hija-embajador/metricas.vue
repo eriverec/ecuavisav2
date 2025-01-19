376faf4aced04a56921cf8b8250e730a76e437f1
granredy <granredy@gmail.com>
2025-01-19 15:31:07 -0500

    add métricas 2
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- servicios/backoffice/v1starter/src/pages/apps/concursos/hija-embajador/metricas.vue
+++ servicios/backoffice/v1starter/src/pages/apps/concursos/hija-embajador/metricas.vue
@@ -1,5 +1,6 @@
 <script setup>
   import metrica_hija_embajador_agrupados_x_dias from '@/views/apps/concursos/metrica_hija_embajador_agrupados_x_dias.vue';
+import metrica_hija_embajador_agrupados_x_horas from '@/views/apps/concursos/metrica_hija_embajador_agrupados_x_horas.vue';
 
 </script>
 
@@ -9,6 +10,10 @@
 
     <VCol cols="12" md="12">
       <metrica_hija_embajador_agrupados_x_dias />
+    </VCol>
+
+    <VCol cols="12" md="12" class="d-none">
+      <metrica_hija_embajador_agrupados_x_horas />
     </VCol>
 
   </VRow>
