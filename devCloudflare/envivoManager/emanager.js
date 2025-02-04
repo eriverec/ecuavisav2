fbf146347a81e6fc496156ada13a309867944776
Eri Vera <eriveraec@gmail.com>
2025-02-04 14:58:41 -0500

    Update emanager.js

--- devCloudflare/envivoManager/emanager.js
+++ devCloudflare/envivoManager/emanager.js
@@ -1301,7 +1301,7 @@ function paginaEnvivo() {
     `<div id="boton_eventos">
       <div id="cont-botones">
         <a href="/envivo" class="btn-gye activo">En Vivo</a>
-        <a href="/envivo/rueda-de-prensas" class="btn-quito">Rueda de prensa</a>
+        <a href="/envivo/rueda-de-prensa" class="btn-quito">Rueda de prensa</a>
       </div>
     </div>`
   );
@@ -1312,7 +1312,7 @@ function paginaEventos() {
   <div id="boton_eventos">
       <div id="cont-botones">
         <a href="/envivo" class="btn-gye ">En Vivo</a>
-        <a href="/envivo/rueda-de-prensas" class="btn-quito activo">Rueda de prensa</a>
+        <a href="/envivo/rueda-de-prensa" class="btn-quito activo">Rueda de prensa</a>
       </div>
     </div>
   <div id="programacion_eventos" style="margin: 20px 0;">
@@ -1324,16 +1324,17 @@ function paginaEventos() {
 
 }
 
-
 setTimeout(() => {
-  
   const rutaActual = window.location.pathname;
-  if (rutaActual === '/envivo/rueda-de-prensas') {
+  if (rutaActual === '/envivo/rueda-de-prensa') {
     paginaEventos();
+    console.log("paginaEventos");
   } else if (rutaActual === '/envivo') {
     // paginaEnvivo();
-  } else {
+    console.log("paginaEnvivo");
 
+  } else {
+    console.log("nada de nada");
   }
 
 }, 1000);
