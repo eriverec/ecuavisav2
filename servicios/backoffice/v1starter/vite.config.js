02bd14d3f871744b256bf7f6d4efc2fe6d299eb3
Carlos Rojano <granredy@gmail.com>
2025-02-04 10:16:13 -0500

    Update vite.config.js
    Co-Authored-By: Carlos Rojano <22602943+carlos-rl@users.noreply.github.com>

--- servicios/backoffice/v1starter/vite.config.js
+++ servicios/backoffice/v1starter/vite.config.js
@@ -1,16 +1,20 @@
-import { fileURLToPath } from 'url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import DefineOptions from 'unplugin-vue-define-options/vite'
-import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
+import { defineConfig, loadEnv } from 'vite'
 import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  // Cargar variables de entorno
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+  base: (env.BASE_URL || '/'),
   plugins: [
     vue(),
     vueJsx(),
@@ -59,4 +63,5 @@ export default defineConfig({
       './src/**/*.vue',
     ],
   },
-})
+}
+});
