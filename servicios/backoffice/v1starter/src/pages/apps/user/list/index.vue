eb6fa158d866b012ef160f6298ae6210f998560b
BrendaC04 <brendapilozo@gmail.com>
2025-02-19 15:52:55 -0500

    Update index.vue

--- servicios/backoffice/v1starter/src/pages/apps/user/list/index.vue
+++ servicios/backoffice/v1starter/src/pages/apps/user/list/index.vue
@@ -1256,7 +1256,7 @@ const resolveFechaSelected = (fechas) => {
 
             <div class="d-flex align-init flex-wrap gap-2">
               <!-- 👉 Search  -->
-              <div style="width: 10rem">
+              <div class="d-none" style="width: 10rem">
                 <VTextField
                   v-model="searchQuery"
                   placeholder="Buscar..."
@@ -1264,12 +1264,12 @@ const resolveFechaSelected = (fechas) => {
                 />
               </div>
               <!-- 👉 Search button -->
-              <VBtn prepend-icon="tabler-search" @click="searchUsers"/>
+              <VBtn class="d-none" prepend-icon="tabler-search" @click="searchUsers"/>
               
               <!-- 👉 Reset button -->
-              <VBtn color="secondary" @click="reset"> Reiniciar </VBtn>
+              <VBtn class="d-none" color="secondary" @click="reset"> Reiniciar </VBtn>
               <!-- 👉 Export button -->
-              <div class="d-flex flex-wrap flex-column">
+              <div class="d-flex flex-wrap flex-column d-none">
                 <VBtn               
                   variant="tonal"
                   color="success"
@@ -1277,6 +1277,7 @@ const resolveFechaSelected = (fechas) => {
                   @click="downloadFull"
                   :loading="isFullLoading"
                   :disabled="disabledBtnExportAll"
+                  class="d-none"
                 >
                   <span>Exportar Todo</span>
                   <VTooltip 
@@ -1301,10 +1302,11 @@ const resolveFechaSelected = (fechas) => {
                 prepend-icon="tabler-screen-share"
                 @click="downloadSection"
                 :disabled="disabledBtnExportSection"
+                class="d-none"
               >
                 Exportar sección
               </VBtn>
-              <div class="d-flex flex-wrap flex-column">
+              <div class="d-flex flex-wrap flex-column d-none">
                 <VBtn
                   variant="tonal"
                   color="success"
@@ -1312,6 +1314,7 @@ const resolveFechaSelected = (fechas) => {
                   @click="downloadSearch"
                   :loading="isSearchLoading"
                   :disabled="disabledBtnExportSearch"
+                  class="d-none"
                 >
                   Exportar búsqueda
                 </VBtn>
