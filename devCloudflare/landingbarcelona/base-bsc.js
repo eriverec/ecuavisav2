e73725e6053d124ff5967bc3d8a0f7db3324adeb
Eri Vera <eriveraec@gmail.com>
2025-08-08 19:32:06 -0500

    actualizado

--- devCloudflare/landingbarcelona/base-bsc.js
+++ devCloudflare/landingbarcelona/base-bsc.js
@@ -10,52 +10,52 @@ function iniciarContenidoSocio() {
 
         const contertHTML = `
      <section class="bsc-portada">
-    <img class="bsc-portada" width="100%"  src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/portada.webp" alt="ecuavisa">
+    <img class="bsc-portada" width="100%"  src="https://ecuavisav2.pages.dev/landingbarcelona/assets/portada.webp" alt="ecuavisa">
 
-    <img class="bsc-beneficios" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/beneficiossc.webp" alt="ecuavisa">
+    <img class="bsc-beneficios" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/beneficiossc.webp" alt="ecuavisa">
     
   </section>
 
   <div class="parent-bsc container">
 
     <section class="bsc-brochure">
-      <img width="100%" class="img-brochure" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/BROCHURE1.webp" alt="ecuavisa">
-      <img width="100%" class="img-brochure-2" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/BROCHURE2.webp" alt="ecuavisa">
+      <img width="100%" class="img-brochure" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/BROCHURE1.webp" alt="ecuavisa">
+      <img width="100%" class="img-brochure-2" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/BROCHURE2.webp" alt="ecuavisa">
   
     </section>
   
   
     <section class="bsc-tarj-socio">
-      <img width="100%" class="img-brochure" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/cen-amarillo.webp" alt="ecuavisa">
-      <img width="100%" class="img-brochure" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/cen-bronce.webp" alt="ecuavisa">
+      <img width="100%" class="img-brochure" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/cen-amarillo.webp" alt="ecuavisa">
+      <img width="100%" class="img-brochure" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/cen-bronce.webp" alt="ecuavisa">
   
-      <img width="100%" class="img-brochure" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/cen-plata.webp" alt="ecuavisa">
-      <img width="100%" class="img-brochure" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/cen-oro.webp" alt="ecuavisa">
+      <img width="100%" class="img-brochure" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/cen-plata.webp" alt="ecuavisa">
+      <img width="100%" class="img-brochure" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/cen-oro.webp" alt="ecuavisa">
   
   
   
     </section>
 
     <section class="bsc-final">
-      <img width="100%" class="img-per-mobile" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/felipe-mobile.webp" alt="ecuavisa">
+      <img width="100%" class="img-per-mobile" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/felipe-mobile.webp" alt="ecuavisa">
 
       <div class="bscf-izq">
-        <img width="100%" class="img-top" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/comomeopuedo.webp" alt="ecuavisa">
-        <img width="100%" class="img-center" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/visitabsc.webp" alt="ecuavisa">
+        <img width="100%" class="img-top" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/comomeopuedo.webp" alt="ecuavisa">
+        <img width="100%" class="img-center" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/visitabsc.webp" alt="ecuavisa">
         <a href="https://sociosbsc.com.ec/" target="_blank">
-          <img width="100%" class="img-bottom" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/codigoqr.webp" alt="ecuavisa">
+          <img width="100%" class="img-bottom" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/codigoqr.webp" alt="ecuavisa">
         </a>
 
       </div>
   
-      <img width="100%" class="img-per-desk" src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/personaje.webp" alt="ecuavisa">
+      <img width="100%" class="img-per-desk" src="https://ecuavisav2.pages.dev/landingbarcelona/assets/personaje.webp" alt="ecuavisa">
   
   
   
     </section>
 
     <section class="bsc-footer">
-      <img src="https://cdn-ecv-2025.pages.dev/landingbarcelona/assets/ecuavisa-barcelona.webp" width="200px" alt="ecuavisa">
+      <img src="https://ecuavisav2.pages.dev/landingbarcelona/assets/ecuavisa-barcelona.webp" width="200px" alt="ecuavisa">
     </section>
   </div>
 
